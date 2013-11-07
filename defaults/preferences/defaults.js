2b3f2a975ae2b57713845cfd63adddb34f7ccde9
friflaj <friflaj@redminebacklogs.net>
2013-11-07 19:56:24 +0100

    Sync extension preferences

--- defaults/preferences/defaults.js
+++ defaults/preferences/defaults.js
@@ -1,3 +1,6 @@
 pref("extensions.zotero-better-bibtex.getCollections", true);
 pref("extensions.zotero-better-bibtex.citeCommand", 'cite');
 pref("extensions.zotero-better-bibtex.citeKeyFormat", '[auth][year]');
+pref("services.sync.prefs.sync.extensions.zotero-better-bibtex.getCollections", true);
+pref("services.sync.prefs.sync.extensions.zotero-better-bibtex.citeCommand", true);
+pref("services.sync.prefs.sync.extensions.zotero-better-bibtex.citeKeyFormat", true);
