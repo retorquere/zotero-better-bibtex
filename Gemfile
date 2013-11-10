79c5d1a282894642c5e09ff0af1027e11a1b804f
friflaj <friflaj@ajellito.com>
2013-11-10 18:32:26 +0100

    prepare for pull-export

---
+++ Gemfile
@@ -0,0 +1,9 @@
+#ruby=1.9
+#ruby-gemset=zotero
+
+source 'https://rubygems.org'
+
+gem 'facets'
+gem 'trollop'
+gem 'rake'
+gem 'nokogiri'
--- chrome/content/zotero-better-bibtex/zotero-better-bibtex.js
+++ chrome/content/zotero-better-bibtex/zotero-better-bibtex.js
@@ -1,11 +1,50 @@
 Zotero.BetterBibTex = {
   prefs: Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService).getBranch("extensions.zotero-better-bibtex."),
+  embeddedKeyRE: /bibtex:\s*([^\s\r\n]+)/,
+  translators: {},
 
   init: function () {
     Zotero.BetterBibTex.safeLoad('BetterBibLaTex.js');
     Zotero.BetterBibTex.safeLoad('BetterCiteTex.js');
     Zotero.BetterBibTex.safeLoad('BetterBibTex.js');
     Zotero.Translators.init();
+  },
+
+  export: function(translator, collectionkey) {
+    var lkh = Zotero.Collections.parseLibraryKeyHash(collectionkey);
+    if (!lkh) { return collectionkey + ' not found'; }
+
+    var col = Zotero.Collections.getByLibraryAndKey(lkh.libraryID, lkh.key);
+    if (!col) { return collectionkey + ' not found'; }
+
+    var translator = Zotero.BetterBibTex.translators[translator.toLowerCase()];
+    if (!translator) { return 'No translator' + translator; }
+
+    var item;
+    var items = col.getChildren(true, false, 'item');
+    items = [item.id for (item of items)];
+    items = Zotero.Items.get(items);
+
+    var translation = new Zotero.Translate.Export();
+    translation.setItems(items);
+    translation.setTranslator(translator);
+
+    var status = {finished: false};
+
+    translation.setHandler("done", function(obj, success) {
+      status.success = success;
+      status.finished = true;
+      if (success) { status.data = obj.string; }
+    });
+    translation.translate();
+
+    while (!status.finished) {}
+
+    if (status.success) {
+      return status.data;
+    } else {
+      return 'export failed';
+    }
   },
 
   safeLoad: function(translator) {
@@ -46,6 +85,8 @@ Zotero.BetterBibTex = {
       console.log('Loading ' + translator + ' failed: ' + err);
       return;
     }
+
+    Zotero.BetterBibTex.translators[header.label.toLowerCase()] = header.translatorID;
 
     var override;
     for (section of ['configOptions', 'displayOptions']) {
