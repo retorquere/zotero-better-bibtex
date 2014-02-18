3f7fe634ae2fa7a46e51257bcfcef857116bcaf6
friflaj <friflaj@ajellito.com>
2014-02-18 14:31:55 +0100

    prep for export filename formatting

--- defaults/preferences/defaults.js
+++ defaults/preferences/defaults.js
@@ -1,8 +1,12 @@
 pref("extensions.zotero.translators.better-bibtex.citeCommand", 'cite');
 pref("extensions.zotero.translators.better-bibtex.citeKeyFormat", '[auth][year]');
+pref("extensions.zotero.translators.better-bibtex.attachmentFormat", 'files/[id]/[filename]');
+pref("extensions.zotero.translators.better-bibtex.snapshotFormat", 'files/[id]/[filename]');
 pref("extensions.zotero.translators.better-bibtex.unicode", '');
 pref("extensions.zotero.translators.better-bibtex.fancyURLs", false);
 
 pref("services.sync.prefs.sync.extensions.zotero.translators.better-bibtex.citeCommand", true);
 pref("services.sync.prefs.sync.extensions.zotero.translators.better-bibtex.citeKeyFormat", true);
+pref("services.sync.prefs.sync.extensions.zotero.translators.better-bibtex.attachmentFormat", true);
+pref("services.sync.prefs.sync.extensions.zotero.translators.better-bibtex.snapshotFormat", true);
 pref("services.sync.prefs.sync.extensions.zotero.translators.better-bibtex.unicode", true);
