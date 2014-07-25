58cb59bed3cb98dd21ac4c8b7507cfa0e6a5d8da
friflaj <friflaj@ajellito.com>
2014-07-25 10:41:43 +0200

    e-1 passes

--- chrome/content/zotero-better-bibtex/dict.js
+++ chrome/content/zotero-better-bibtex/dict.js
@@ -5,16 +5,38 @@ if (!Dict) {
   Dict = function(init){
     var obj = Object.create(null);
 
-    function forEach(callback) {
-      Object.keys(this).forEach(function(prop) {
-        callback.call(null, prop, obj[prop]);
-      });
-    }
-    Object.defineProperty(obj, 'forEach', { enumerable: false, configurable: false, writable: false, value: forEach.bind(obj) });
+    /*
+    Object.defineProperty(obj, 'values', {
+      enumerable: false,
+      configurable: false,
+      writable: false,
+      value: function () {
+        return Object.keys(this).map(function(key) { return this[key]; });
+      }.bind(obj)
+    });
+    Object.defineProperty(obj, 'forEach', {
+      enumerable: false,
+      configurable: false,
+      writable: false,
+      value: function (callback) {
+        Object.keys(this).forEach(function(prop) {
+          callback.call(null, prop, obj[prop]);
+        });
+      }.bind(obj)
+    });
+    */
 
     Object.keys(init || {}).forEach(function(prop) {
       obj[prop] = init[prop];
     });
     return obj;
+  };
+  Dict.forEach = function (obj, callback) {
+    Object.keys(obj).forEach(function(prop) {
+      callback.call(null, prop, obj[prop]);
+    });
+  };
+  Dict.values = function () {
+    return Object.keys(this).map(function(key) { return this[key]; });
   };
 }
