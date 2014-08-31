b0f07d869d6e3179f331c5718b8380ce9df49076
friflaj <friflaj@ajellito.com>
2014-08-31 16:33:10 +0200

    prepping for #70

--- chrome/content/zotero-better-bibtex/auto-export.js
+++ chrome/content/zotero-better-bibtex/auto-export.js
@@ -4,3 +4,15 @@ var Exporter = new function() {
 onmessage = function(event) {
   postMessage(Exporter[req.method].apply(null, req.params));
 };
+
+// https://developer.mozilla.org/en/docs/Storage
+// openDatabaseSync
+var db=openDatabase('myDB', '', 'my first database', 2 * 1024 * 1024);
+db.transaction(function(tx){
+   tx.executeSql('CREATE TABLE IF NOT EXISTS foo (id unique, text)',[],function(tx,results){
+      self.postMessage("passed");
+   },function(_trans,_error){self.postMessage(_error.message)});
+   tx.executeSql('INSERT INTO foo (id, text) VALUES (1, "synergies")',[],function(tx,results){
+      self.postMessage("passed");
+   },function(_trans,_error){self.postMessage(_error.message)});
+});
