4a002f75961e1018c44ea6ac1d2740f3d7ff19d4
friflaj <friflaj@ajellito.com>
2014-09-02 10:14:41 +0200

    .

--- chrome/content/zotero-better-bibtex/auto-export.js
+++ chrome/content/zotero-better-bibtex/auto-export.js
@@ -5,8 +5,10 @@ onmessage = function(event) {
   postMessage(Exporter[req.method].apply(null, req.params));
 };
 
+// remember to drop cache on *any* preference change
 // https://developer.mozilla.org/en/docs/Storage
 // openDatabaseSync
+// https://developer.mozilla.org/en-US/docs/Web/API/ChromeWorker
 var db=openDatabase('myDB', '', 'my first database', 2 * 1024 * 1024);
 db.transaction(function(tx){
    tx.executeSql('CREATE TABLE IF NOT EXISTS foo (id unique, text)',[],function(tx,results){
