06f0ed38a9610a5072b6b706d38d74640b559bf6
retorquere <Emiliano.Heyns@iris-advies.com>
2015-04-28 16:26:01 +0200

    release: zotero-better-bibtex-0.9.12.xpi

--- circle.sh
+++ circle.sh
@@ -26,6 +26,8 @@ case $1 in
     if [ "$CHECKIN" = "$RELEASE" ] ; then
       git submodule update --init
       cd www; git checkout master; git pull
+      bundle update
+      gem cleanup
       git config --global user.name "$CIRCLE_USERNAME"
       git config --global user.email "$CIRCLE_USERNAME@$CIRCLE_PROJECT_USERNAME.github.com"
       git config --global push.default matching
