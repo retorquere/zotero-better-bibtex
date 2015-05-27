748b5825dc1d59098faed6129f37c96768953681
retorquere <emiliano.heyns@iris-advies.com>
2015-05-27 21:49:09 +0200

    only orchestrate for release builds

--- .travis.sh
+++ .travis.sh
@@ -3,15 +3,15 @@
 set -e
 set -u
 
-STATUS=`travis_parallel_sentinel script`
-if [ "$STATUS" = "deploy" ] ; then
-  rake
-  XPI=`ls *.xpi`
-  RELEASE="$TRAVIS_COMMIT release: $XPI"
-  CHECKIN=`git log -n 1 --pretty=oneline`
-  echo "checkin: $CHECKIN"
-  echo "release: $RELEASE"
-  if [ "$CHECKIN" = "$RELEASE" ] ; then
+rake
+XPI=`ls *.xpi`
+RELEASE="$TRAVIS_COMMIT release: $XPI"
+CHECKIN=`git log -n 1 --pretty=oneline`
+echo "checkin: $CHECKIN"
+echo "release: $RELEASE"
+if [ "$CHECKIN" = "$RELEASE" ] ; then
+  STATUS=`travis_parallel_sentinel script`
+  if [ "$STATUS" = "deploy" ] ; then
     sed -i.bak -e 's/git@github.com:/https:\/\/github.com\//' .gitmodules
     sed -i.bak -e 's/git@github.com:/https:\/\/github.com\//' .git/modules/www/config
     echo "https://${GITHUB_TOKEN}:@github.com" > /tmp/credentials
@@ -23,7 +23,7 @@ if [ "$STATUS" = "deploy" ] ; then
     git config --global user.email "retorquere@ZotPlus.github.com"
     git config --global push.default matching
     bundle exec rake deploy
-  else
-    echo 'not a tagged release'
   fi
+else
+  echo 'not a tagged release'
 fi
