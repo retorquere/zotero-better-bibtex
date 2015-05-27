928bfe43bd088fdd79a0c5b8fc0d7934e7ce181b
retorquere <emilano.heyns@iris-advies.com>
2015-05-27 10:55:47 +0200

    not deploying via circle anymore

--- circle.sh
+++ circle.sh
@@ -34,22 +34,22 @@ case $1 in
     ;;
 
   deploy)
-    bundle exec rake
-    XPI=`ls *.xpi`
-    RELEASE="$CIRCLE_SHA1 release: $XPI"
-    CHECKIN=`git log -n 1 --pretty=oneline`
-    echo "checkin: $CHECKIN"
-    echo "release: $RELEASE"
-    if [ "$CHECKIN" = "$RELEASE" ] ; then
-      git submodule update --init
-      (cd www && git checkout master && git pull)
-      git config --global user.name "$CIRCLE_USERNAME"
-      git config --global user.email "$CIRCLE_USERNAME@$CIRCLE_PROJECT_USERNAME.github.com"
-      git config --global push.default matching
-      bundle exec rake deploy
-    else
+    #bundle exec rake
+    #XPI=`ls *.xpi`
+    #RELEASE="$CIRCLE_SHA1 release: $XPI"
+    #CHECKIN=`git log -n 1 --pretty=oneline`
+    #echo "checkin: $CHECKIN"
+    #echo "release: $RELEASE"
+    #if [ "$CHECKIN" = "$RELEASE" ] ; then
+    #  git submodule update --init
+    #  (cd www && git checkout master && git pull)
+    #  git config --global user.name "$CIRCLE_USERNAME"
+    #  git config --global user.email "$CIRCLE_USERNAME@$CIRCLE_PROJECT_USERNAME.github.com"
+    #  git config --global push.default matching
+    #  bundle exec rake deploy
+    #else
       echo 'not a tagged release'
-    fi
+    #fi
     ;;
 
   *)
