3d18a05156ad5d0e5e73419f407527d437a545cd
retorquere <emilano.heyns@iris-advies.com>
2015-05-27 10:45:03 +0200

    sad to go, but I can't trust circle builds anymore

--- .travis.sh
+++ .travis.sh
@@ -8,6 +8,24 @@ if [ "$BUILD_LEADER" = "YES" ]; then
   if [ "$BUILD_AGGREGATE_STATUS" = "others_$OTHERS" ]; then
     if [ "$OTHERS" = "succeeded" ]; then
       echo "All Succeeded! PUBLISHING..."
+
+      bundle exec rake
+      XPI=`ls *.xpi`
+      RELEASE="$TRAVIS_COMMIT release: $XPI"
+      CHECKIN=`git log -n 1 --pretty=oneline`
+      echo "checkin: $CHECKIN"
+      echo "release: $RELEASE"
+      if [ "$CHECKIN" = "$RELEASE" ] ; then
+        git submodule update --init
+        (cd www && git checkout master && git pull)
+        git config --global user.name "retorquere"
+        git config --global user.email "retorquere@ZotPlus.github.com"
+        git config --global push.default matching
+        bundle exec rake deploy
+      else
+        echo 'not a tagged release'
+      fi
+
     else
       echo "All Failed"
     fi
