60f9e74ee74935b09cac3ca093b53a79404ade47
retorquere <emiliano.heyns@iris-advies.com>
2015-09-10 09:37:54 +0200

    Revert "cleaner code"
    This reverts commit b1b81e231cfbdb3128da103d0580379aaf99b65d.

--- lib/unicode_table.rb
+++ lib/unicode_table.rb
@@ -33,7 +33,7 @@ class UnicodeConverter
     cs = File.expand_path(cs)
     open(cs, 'w'){|f|
       f.puts "LaTeX = {} unless LaTeX"
-      f.puts "LaTeX.toLaTeX ="
+      f.puts "LaTeX.toLaTeX = { unicode: Object.create(null), ascii: Object.create(null) }"
 
       unicode = {math: '', text: ''}
       done = {}
@@ -41,12 +41,11 @@ class UnicodeConverter
         next unless (charcode >= 0x20 && charcode <= 0x7E) || charcode == 0x00A0 || latex.latex == ' ' || charcode == ' '.ord # an ascii character that needs translation? Probably a TeX special character
         next if done[charcode]
         done[charcode] = true
-        unicode[latex.math ? :math : :text] << "      #{char(charcode)}: #{latex.latex[0].inspect}\n"
+        unicode[latex.math ? :math : :text] << "  #{char(charcode)}: #{latex.latex[0].inspect}\n"
       }
-      f.puts "  unicode:"
-      f.puts "    math:"
+      f.puts "LaTeX.toLaTeX.unicode.math ="
       f.puts unicode[:math]
-      f.puts "    text:"
+      f.puts "LaTeX.toLaTeX.unicode.text ="
       f.puts unicode[:text]
 
       ascii = {math: '', text: ''}
@@ -54,23 +53,22 @@ class UnicodeConverter
       @chars.sort.map{|charcode, latex|
         next if done[charcode]
         done[charcode] = true
-        ascii[latex.math ? :math : :text] << "      #{char(charcode)}: #{latex.latex[0].inspect}\n"
+        ascii[latex.math ? :math : :text] << "  #{char(charcode)}: #{latex.latex[0].inspect}\n"
       }
-      f.puts "  ascii:"
-      f.puts "    math:"
+      f.puts "LaTeX.toLaTeX.ascii.math ="
       f.puts ascii[:math]
-      f.puts "    text:"
+      f.puts "LaTeX.toLaTeX.ascii.text ="
       f.puts ascii[:text]
 
       done = {}
-      f.puts "  toUnicode:"
+      f.puts "LaTeX.toUnicode ="
       @chars.sort.map{|charcode, latex|
         latex.latex.each{|ltx|
           next if ltx =~ /^[a-z]+$/i || ltx.strip == ''
           next if charcode < 256 && ltx == charcode.chr
           next if done[ltx]
           done[ltx] = true
-          f.puts "    #{ltx.inspect}: #{char(charcode)}"
+          f.puts "  #{ltx.inspect}: #{char(charcode)}"
         }
       }
     }
