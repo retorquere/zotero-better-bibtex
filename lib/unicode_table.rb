f6229e6a76d42659d7fdfa4ceeaf874da68a8280
retorquere <emiliano.heyns@iris-advies.com>
2015-09-10 08:41:11 +0200

    stable maps, better diffs

--- lib/unicode_table.rb
+++ lib/unicode_table.rb
@@ -14,6 +14,18 @@ class UnicodeConverter
     read('http://www.w3.org/Math/characters/unicode.xml')
   end
 
+  def char(charcode)
+    return "'\\\\'" if charcode == '\\'.ord
+
+    return "'#{charcode.chr}'" if charcode >= 0x20 && charcode <= 0x7E
+
+    return "'\\u#{charcode.to_s(16).upcase.rjust(4, '0')}'" if charcode < 0x10000
+
+    codepoint = charcode - 0x10000
+    codepoint = [(codepoint >> 10) + 0xD800, (@lowmask & codepoint) + 0xDC00].collect{|n| n.to_s(16).upcase.rjust(4, '0')}
+    return "'" + codepoint.collect{|cp| "\\u" + cp}.join('') + "'"
+  end
+
   def save(cs)
     fixup
     expand
@@ -25,12 +37,11 @@ class UnicodeConverter
 
       unicode = {math: '', text: ''}
       done = {}
-      @chars.each_pair{|char, latex|
-        next unless char =~ /^[\x20-\x7E]$/ || latex.latex == ' ' || char == ' ' || char == '\u00A0' # an ascii character that needs translation? Probably a TeX special character
-        char = "\\\\" if char == '\\'
-        next if done[char]
-        done[char] = true
-        unicode[latex.math ? :math : :text] << "  '#{char}': #{latex.latex[0].inspect}\n"
+      @chars.sort.map{|charcode, latex|
+        next unless (charcode >= 0x20 && charcode <= 0x7E) || charcode == 0x00A0 || latex.latex == ' ' || charcode == ' '.ord # an ascii character that needs translation? Probably a TeX special character
+        next if done[charcode]
+        done[charcode] = true
+        unicode[latex.math ? :math : :text] << "  #{char(charcode)}: #{latex.latex[0].inspect}\n"
       }
       f.puts "LaTeX.toLaTeX.unicode.math ="
       f.puts unicode[:math]
@@ -39,17 +50,10 @@ class UnicodeConverter
 
       ascii = {math: '', text: ''}
       done = {}
-      @chars.each_pair{|char, latex|
-        if char == '\\'
-          char = "'\\\\'"
-        elsif char =~ /^[\x20-\x7E]$/
-          char = "'#{char}'"
-        else
-          char = latex.char
-        end
-        next if done[char]
-        done[char] = true
-        ascii[latex.math ? :math : :text] << "  #{char}: #{latex.latex[0].inspect}\n"
+      @chars.sort.map{|charcode, latex|
+        next if done[charcode]
+        done[charcode] = true
+        ascii[latex.math ? :math : :text] << "  #{char(charcode)}: #{latex.latex[0].inspect}\n"
       }
       f.puts "LaTeX.toLaTeX.ascii.math ="
       f.puts ascii[:math]
@@ -58,42 +62,35 @@ class UnicodeConverter
 
       done = {}
       f.puts "LaTeX.toUnicode ="
-      @chars.each_pair{|char, latex|
-        if char == '\\'
-          char = "'\\\\'"
-        elsif char =~ /^[\x20-\x7E]$/
-          char = "'#{char}'"
-        else
-          char = latex.char
-        end
+      @chars.sort.map{|charcode, latex|
         latex.latex.each{|ltx|
           next if ltx =~ /^[a-z]+$/i || ltx.strip == ''
-          next if ltx == char.sub(/^'/, '').sub(/'$/, '')
+          next if charcode < 256 && ltx == charcode.chr
           next if done[ltx]
           done[ltx] = true
-          f.puts "  #{ltx.inspect}: #{char}"
+          f.puts "  #{ltx.inspect}: #{char(charcode)}"
         }
       }
     }
   end
 
   def fixup
-    @chars['&'.inspect] = OpenStruct.new({latex: "\\&", char: "'&'", math: false})
-    @chars['\uFFFD'] = OpenStruct.new({latex: "\\dbend", char: "'\\uFFFD'", math: false})
-    @chars["\\"] = OpenStruct.new({latex: "\\backslash", char: "'\\\\'", math: true})
+    @chars['&'.ord] = OpenStruct.new({latex: "\\&", math: false})
+    @chars[0xFFFD] = OpenStruct.new({latex: "\\dbend", math: false})
+    @chars[0x00A0] = OpenStruct.new({latex: ' ', math: false})
+    @chars["\\".ord] = OpenStruct.new({latex: "\\backslash", math: true})
 
     # biber doesn't like it when I escape closing square brackets #245.1, so only opening bracket
-    @chars['['] = OpenStruct.new({latex: '{[}', char: "'['", math: false})
+    @chars['['.ord] = OpenStruct.new({latex: '{[}', math: false})
 
     # TODO: replace '}' and '{' with textbrace(left|right) once the bug mentioned in
     # http://tex.stackexchange.com/questions/230750/open-brace-in-bibtex-fields/230754#comment545453_230754
     # is widely enough distributed
     ['_', '}', '{'].each{|char|
-      @chars[char] = OpenStruct.new({latex: "\\" + char, char: "'#{char}'", math: false})
+      @chars[char.ord] = OpenStruct.new({latex: "\\" + char, math: false})
     }
-    @chars['\u00A0'] = OpenStruct.new({latex: ' ', char: "'\\u00A0'", math: false})
 
-    @chars.keys.each{|char|
+    @chars.keys.each{|charcode|
       { "\\textdollar"        => "\\$",
         "\\textquotedblleft"  => "``",
         "\\textquotedblright" => "''",
@@ -101,19 +98,19 @@ class UnicodeConverter
         "\\textquotesingle"   => "'",
         "\\space"             => ' '
       }.each_pair{|ist, soll|
-        @chars[char].latex = soll if @chars[char].latex == ist
+        @chars[charcode].latex = soll if @chars[charcode].latex == ist
       }
 
-      if @chars[char].latex == '~' || @chars[char].latex == ' ' || @chars[char].char =~ /u00A0'/
-        @chars[char].latex = ' '
-        @chars[char].math = false
+      if @chars[charcode].latex == '~' || @chars[charcode].latex == ' '
+        @chars[charcode].latex = ' '
+        @chars[charcode].math = false
       end
     }
   end
 
   def expand
-    @chars.keys.each{|char|
-      latex = [@chars[char].latex]
+    @chars.keys.each{|charcode|
+      latex = [@chars[charcode].latex]
 
       case latex[0]
         when /^(\\[a-z][^\s]*)\s$/i, /^(\\[^a-z])\s$/i  # '\ss ', '\& ' => '{\\s}', '{\&}'
@@ -142,7 +139,7 @@ class UnicodeConverter
           a <=> b
         end
       }
-      @chars[char].latex = latex
+      @chars[charcode].latex = latex
     }
   end
 
@@ -169,46 +166,9 @@ class UnicodeConverter
       latex = "{\\#{$1}#{$2}}" if latex =~ /^\\(["^`\.'~]){([^}]+)}$/
       latex = "{\\#{$1} #{$2}}" if latex =~ /^\\([cuHv]){([^}]+)}$/
 
-      id = char['id']
-      if id =~ /^u0/i
-        id = [id.upcase.sub(/^u0/i, '')]
-      else
-        cp = id.sub(/^u/i, '').to_i(16) - 0x10000
-        id = [(cp >> 10) + 0xD800, (@lowmask & cp) + 0xDC00].collect{|n| n.to_s(16)}
-      end
-      id = "'" + id.collect{|cp| "\\u" + cp}.join('') + "'"
-
-      @chars[chr] = OpenStruct.new({latex: latex , char: id, math: math})
+      @chars[char['id'].sub(/^u/i, '').to_i(16)] = OpenStruct.new({latex: latex , math: math})
     }
   end
-
-#  u2l = {
-#    unicode: OpenStruct.new({ math: [], map: {} }),
-#    ascii: OpenStruct.new({ math: [], map: {} })
-#  }
-#
-#  l2u = { }
-#
-#  mapping.each_pair{|key, repl|
-#    # need to figure something out for this. This has the form X<combining char>, which needs to be transformed to
-#    # \combinecommand{X}
-#    #raise value if value =~ /LECO/
-#
-#    if key =~ /^[\x20-\x7E]$/ # an ascii character that needs translation? Probably a TeX special character
-#      u2l[:unicode].map[key] = latex[0]
-#      u2l[:unicode].math << key if repl.math
-#    end
-#
-#    u2l[:ascii].map[key] = latex[0]
-#    u2l[:ascii].math << key if repl.math
-#
-#    latex.each{|ltx|
-#      l2u[ltx] = key if ltx =~ /\\/
-#    }
-#  }
-#
-#  l2u["\\url"] = '';
-#  l2u["\\href"] = '';
 end
 
 if __FILE__ == $0
