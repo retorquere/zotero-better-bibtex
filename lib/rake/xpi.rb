4680c2b16b386b955704a99e5f7fe7219c7e3890
retorquere <emiliano.heyns@iris-advies.com>
2016-03-27 19:18:20 +0200

    bintray

--- lib/rake/xpi.rb
+++ lib/rake/xpi.rb
@@ -137,8 +137,43 @@ module Rake
       end
 
       def publish
-        return if publish_s3
-        #return if publish_github
+        STDERR.puts "Publishing #{self.versioned_xpi}"
+        publish_bintray
+      end
+
+      def publish_bintray
+        return unless @config.bintray && @config.bintray.secret
+        #RestClient.log = 'stdout'
+
+        secret = ENV[@config.bintray.secret]
+
+        client = RestClient::Resource.new('https://api.bintray.com', @config.bintray.user, secret)
+        content = client['content'][@config.bintray.user][@config.bintray.repo]
+
+        begin
+          client['packages'][@config.bintray.user][@config.bintray.repo][@config.bintray.package]['versions']['latest'].delete
+        rescue RestClient::ResourceNotFound
+          # that's OK
+        end
+
+        content[@config.bintray.package][self.version][self.versioned_xpi].put(File.new(self.xpi), {
+          content_type: 'application/x-xpinstall',
+          x_bintray_package: @config.bintray.package,
+          x_bintray_version: self.version,
+          x_bintray_publish: '1'
+        })
+
+        if release_build?
+          update_rdf("https://bintray.com/artifact/download/#{@config.bintray.user}/#{@config.bintray.repo}/#{@config.bintray.package}/#{self.version}/#{self.versioned_xpi}"){|update|
+            content[@config.repo.package]['update.rdf'].put(File.new(update), {
+              content_type: 'application/rdf+xml',
+              x_bintray_package: @config.repo.package,
+              x_bintray_version: 'latest',
+              x_bintray_publish: '1',
+              x_bintray_override: '1'
+            })
+          }
+        end
       end
 
       def publish_s3
