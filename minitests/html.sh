cb3a5ff838915fb033683746a2adee2bcbaef282
retorquere <emiliano.heyns@iris-advies.com>
2016-08-20 12:40:49 +0200

    adjust more tests for #541

--- minitests/html.sh
+++ minitests/html.sh
@@ -9,7 +9,7 @@ cd ..
 OFFLINE=true
 rm -f minitests/test.js
 echo "var Translator = { debug: function(msg) { console.log(msg); }}, LaTeX = {};" >> minitests/test.js
-for src in resource/translators/xregexp-all.js resource/translators/simple-titlecaser.js resource/translators/titlecaser.js resource/translators/markupparser.js resource/translators/latex_unicode_mapping.js resource/translators/unicode_translator.js minitests/html.js ; do
+for src in resource/translators/xregexp-all.js resource/translators/simple-titlecaser.js resource/translators/titlecaser.js resource/translators/markupparser.js resource/translators/latex_unicode_mapping.js resource/translators/unicode_translator.js minitests/html-reconstruct.js minitests/html.js ; do
   rake $src
   cp $src wiki/retorquere/latex
   cat $src >> minitests/test.js
