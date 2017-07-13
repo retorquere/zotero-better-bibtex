7356c9c18460dd41884d88ba8cd8b4db49ed66ab
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2017-07-13 17:54:27 +0200

    debug-bridge

---
+++ test/fixtures/debug-bridge/chrome/content/debug-bridge.js
@@ -0,0 +1,25 @@
+if (!Zotero.DebugBridge) {
+  (function() {
+    var GeneratorFunction = Object.getPrototypeOf(function*(){}).constructor;
+
+    Zotero.DebugBridge = {};
+    Zotero.DebugBridge.Execute = function() {};
+    Zotero.DebugBridge.prototype = {
+      supportedMethods: ["POST"],
+      supportedDataTypes: '*',
+      permitBookmarklet: false,
+  
+      init: Zotero.Promise.coroutine(function* (options) {
+        let action = Zotero.Promise.coroutine(new GeneratorFunction(options.data));
+        let response;
+        try {
+          response = yield action();
+        } catch (err) {
+          return [500, "application/text", '' + err];
+        }
+        return [201, "application/json", JSON.stringify(response)];
+      })
+    };
+    Zotero.Server.Endpoints["/debug-bridge/execute"] = Zotero.Server.Connector.Import;
+  })();
+}
