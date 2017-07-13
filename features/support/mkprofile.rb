edbc69eec59cbf49111472a936487a9bcc9bced1
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2017-07-13 14:19:41 +0200

    profiles

--- features/support/mkprofile.rb
+++ features/support/mkprofile.rb
@@ -7,7 +7,9 @@ require 'fileutils'
 require 'selenium/webdriver'
 
 if OS.linux?
-  profiles_dir = File.expand_path('~/.zotero/zotero/')
+  profiles_dir = File.expand_path('~/.zotero/zotero')
+elsif OS.mac?
+  profiles_dir = File.expand_path('~/Library/Application Support/Zotero')
 else
   puts OS.report
   exit 1
@@ -31,6 +33,8 @@ class IniFile
 end
 
 profiles = IniFile.load(File.join(profiles_dir, 'profiles.ini'))
+
+profiles['General']['StartWithLastProfile'] = 1
 
 profile_name = 'BBTZ5TEST'
 profile_dir = File.expand_path("~/.#{profile_name}.profile")
@@ -61,7 +65,8 @@ end
 
 profiles.write_compact
 
-profile = Selenium::WebDriver::Firefox::Profile.new(File.expand_path('test/fixtures/profile/profile'))
+fixtures = File.expand_path(File.join(File.dirname(__FILE__), '../../test/fixtures/profile'))
+profile = Selenium::WebDriver::Firefox::Profile.new(File.join(fixtures, 'profile'))
 profile.log_file = File.expand_path(File.join(File.dirname(__FILE__), "#{ENV['LOGS'] || '.'}/firefox-console.log"))
 
 puts "Installing plugins..."
@@ -73,5 +78,5 @@ profile['extensions.checkCompatibility.5.0'] = false
 FileUtils.rm_rf(profile_dir)
 FileUtils.cp_r(profile.layout_on_disk, profile_dir)
 FileUtils.rm_rf(data_dir)
-FileUtils.cp_r(File.expand_path('test/fixtures/profile/data'), data_dir)
+FileUtils.cp_r(File.join(fixtures, 'data'), data_dir)
 puts profile_dir
