c92fa6ddc3a11f9e3c9a7eb5ce984cf7aa645b91
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2017-07-13 10:15:15 +0200

    auto-install

--- zotero5_installer.sh
+++ zotero5_installer.sh
@@ -20,52 +20,53 @@
 # along with qnotero.  If not, see <http://www.gnu.org/licenses/>.
 
 VERSION="5.0.2"
-if [ `uname -m` = "x86_64" ]; then
-	ARCH="x86_64"
-else
-	ARCH="i686"
-fi
+ARCH="x86_64"
 TMP="/tmp/zotero.tar.bz2"
 DEST_FOLDER=zotero
 EXEC=zotero
 
-echo ">>> This script will download and install Zotero standalone on your system."
-echo ">>> Do you want to continue?"
-echo ">>> y/n (default=y)"
-read INPUT
-if [ "$INPUT" = "n" ]; then
-	echo ">>> Aborting installation"
-	exit 0
-fi
-
-echo ">>> Do you want to install Zotero globally (g) or locally (l)."
-echo ">>> Root access is required for a global installation."
-echo ">>> g/l (default=l)"
-read INPUT
-if [ "$INPUT" != "g" ]; then
-	echo ">>> Installing locally"
-	DEST="$HOME/bin"
-	MENU_PATH="$HOME/.local/share/applications/zotero.desktop"
-	MENU_DIR="$HOME/.local/share/applications"
+if [ -z "$ZOTERO_INSTALL" ]; then
+  echo ">>> This script will download and install Zotero standalone on your system."
+  echo ">>> Do you want to continue?"
+  echo ">>> y/n (default=y)"
+  read INPUT
 else
-	echo ">>> Installing globally"
-	DEST="/opt"
-	MENU_PATH="/usr/share/applications/zotero.desktop"
-	MENU_DIR="/usr/share/applications"
+  INPUT=$ZOTERO_INSTALL
+fi
+if [ "$INPUT" = "n" ]; then
+  echo ">>> Aborting installation"
+  exit 0
 fi
 
-echo ">>> Please input the version of Zotero."
-echo ">>> (default=$VERSION)"
-read INPUT
-if [ "$INPUT" != "" ]; then
-	VERSION=$INPUT
+if [ -z "$ZOTERO_INSTALL_DEST" ]; then
+  echo ">>> Do you want to install Zotero globally (g) or locally (l)."
+  echo ">>> Root access is required for a global installation."
+  echo ">>> g/l (default=l)"
+  read INPUT
+else
+  INPUT=$ZOTERO_INSTALL_DEST
+fi
+if [ "$INPUT" != "g" ]; then
+  echo ">>> Installing locally"
+  DEST="$HOME/bin"
+  MENU_PATH="$HOME/.local/share/applications/zotero.desktop"
+  MENU_DIR="$HOME/.local/share/applications"
+else
+  echo ">>> Installing globally"
+  DEST="/opt"
+  MENU_PATH="/usr/share/applications/zotero.desktop"
+  MENU_DIR="/usr/share/applications"
 fi
 
-echo ">>> Please input your systems architecture (i686 or x86_64)."
-echo ">>> (default=$ARCH)"
-read INPUT
+if [ -z "$ZOTERO_INSTALL_VERSION" ]; then
+  echo ">>> Please input the version of Zotero."
+  echo ">>> (default=$VERSION)"
+  read INPUT
+else
+  INPUT=$ZOTERO_INSTALL_VERSION
+fi
 if [ "$INPUT" != "" ]; then
-	ARCH=$INPUT
+  VERSION=$INPUT
 fi
 
 URL="https://www.zotero.org/download/client/dl?channel=release&platform=linux-$ARCH&version=$VERSION"
@@ -75,27 +76,31 @@ echo ">>> URL: $URL"
 
 wget $URL -O $TMP
 if [ $? -ne 0 ]; then
-	echo ">>> Failed to download Zotero"
-	echo ">>> Aborting installation, sorry!"
-	exit 1
+  echo ">>> Failed to download Zotero"
+  echo ">>> Aborting installation, sorry!"
+  exit 1
 fi
 
 if [ -d $DEST/$DEST_FOLDER ]; then
-	echo ">>> The destination folder ($DEST/$DEST_FOLDER) exists. Do you want to remove it?"
-	echo ">>> y/n (default=n)"
-	read INPUT
-	if [ "$INPUT" = "y" ]; then
-		echo ">>> Removing old Zotero installation"
-		rm -Rf $DEST/$DEST_FOLDER
-		if [ $? -ne 0 ]; then
-			echo ">>> Failed to remove old Zotero installation"
-			echo ">>> Aborting installation, sorry!"
-			exit 1
-		fi
-	else
-		echo ">>> Aborting installation"
-		exit 0
-	fi
+  if [ -z "$ZOTERO_INSTALL_NUKE_DEST" ]; then
+    echo ">>> The destination folder ($DEST/$DEST_FOLDER) exists. Do you want to remove it?"
+    echo ">>> y/n (default=n)"
+    read INPUT
+  else
+    INPUT=$ZOTERO_INSTALL_NUKE_DEST
+  fi
+  if [ "$INPUT" = "y" ]; then
+    echo ">>> Removing old Zotero installation"
+    rm -Rf $DEST/$DEST_FOLDER
+    if [ $? -ne 0 ]; then
+      echo ">>> Failed to remove old Zotero installation"
+      echo ">>> Aborting installation, sorry!"
+      exit 1
+    fi
+  else
+    echo ">>> Aborting installation"
+    exit 0
+  fi
 fi
 
 echo ">>> Extracting Zotero"
@@ -103,21 +108,21 @@ echo ">>> Target folder: $DEST/$DEST_FOLDER"
 
 tar -xpf $TMP -C $DEST
 if [ $? -ne 0 ]; then
-	echo ">>> Failed to extract Zotero"
-	echo ">>> Aborting installation, sorry!"
-	exit 1
+  echo ">>> Failed to extract Zotero"
+  echo ">>> Aborting installation, sorry!"
+  exit 1
 fi
 
 mv $DEST/Zotero_linux-$ARCH $DEST/$DEST_FOLDER
 if [ $? -ne 0 ]; then
-	echo ">>> Failed to move Zotero to $DEST/$DEST_FOLDER"
-	echo ">>> Aborting installation, sorry!"
-	exit 1
+  echo ">>> Failed to move Zotero to $DEST/$DEST_FOLDER"
+  echo ">>> Aborting installation, sorry!"
+  exit 1
 fi
 
 if [ -f $MENU_DIR ]; then
-	echo ">>> Creating $MENU_DIR"
-	mkdir $MENU_DIR
+  echo ">>> Creating $MENU_DIR"
+  mkdir $MENU_DIR
 fi
 
 echo ">>> Creating menu entry"
@@ -129,9 +134,9 @@ Icon=$DEST/$DEST_FOLDER/chrome/icons/default/default48.png
 Type=Application
 StartupNotify=true" > $MENU_PATH
 if [ $? -ne 0 ]; then
-	echo ">>> Failed to create menu entry"
-	echo ">>> Aborting installation, sorry!"
-	exit 1
+  echo ">>> Failed to create menu entry"
+  echo ">>> Aborting installation, sorry!"
+  exit 1
 fi
 
 echo ">>> Done!"
