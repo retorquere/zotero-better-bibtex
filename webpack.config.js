70ab929ac74f8c2924de489943220760eba1dc46
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2017-07-14 11:45:12 +0200

    coffee-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,58 @@
 const path = require('path');
 
+const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const InstallRDFPlugin = require('./webpack/install-rdf');
 const PreferencesPlugin = require('./webpack/preferences');
 const ZipPlugin = require('zip-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CommonsPlugin = new webpack.optimize.CommonsChunkPlugin({ name: 'common', filename: 'common.bundle.js' })
 
 const version = require('./webpack/version');
 
-module.exports = {
-  // context: path.join(__dirname, 'app'), // ??
-  entry: './package.json',
-  output: {
-    path: path.join(__dirname, 'build'),
-    filename: 'install.rdf'
+module.exports = [
+  // must be first because of CleanWebpackPlugin
+  {
+    plugins: [
+      new CleanWebpackPlugin(['build']),
+      CommonsPlugin
+    ],
+    context: path.resolve(__dirname, './content'),
+    entry: {
+      betterbibtex: './better-bibtex.coffee'
+    },
+    output: {
+      path: path.resolve(__dirname, './build/content'),
+      filename: '[name].js'
+    },
+    module: {
+      rules: [
+        { test: /\.coffee$/, use: [ 'coffee-loader' ] },
+      ]
+    }
   },
-  plugins: [
-    new CleanWebpackPlugin(['build']),
-    new InstallRDFPlugin(),
-    new PreferencesPlugin(),
-    new CopyWebpackPlugin(
-      [
-        { from: 'content/**/*' },
-        { from: 'chrome.manifest' },
-      ],
-      { ignore: [ '*.coffee' ], copyUnmodified: true }
-    ),
-    new ZipPlugin({
-      filename: `zotero-better-bibtex-${version}`,
-      extension: 'xpi',
-    }),
-  ]
-};
+
+  // must be last because of ZipPlugin
+  {
+    entry: './package.json',
+    output: {
+      path: path.join(__dirname, 'build'),
+      filename: 'install.rdf'
+    },
+    plugins: [
+      new InstallRDFPlugin(),
+      new PreferencesPlugin(),
+      new CopyWebpackPlugin(
+        [
+          { from: 'content/**/*' },
+          { from: 'chrome.manifest' },
+        ],
+        { ignore: [ '*.coffee' ], copyUnmodified: true }
+      ),
+      new ZipPlugin({
+        filename: `zotero-better-bibtex-${version}`,
+        extension: 'xpi',
+      }),
+    ]
+  }
+];
