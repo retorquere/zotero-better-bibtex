0c0d44aea531eb2cdbddd951adcb2a32ac3be9c5
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2017-07-15 19:56:25 +0200

    clean shutdown

--- features/support/mkprofile.rb
+++ features/support/mkprofile.rb
@@ -9,17 +9,6 @@ require 'httparty'
 require 'shellwords'
 require 'benchmark'
 
-if OS.linux?
-  profiles_dir = File.expand_path('~/.zotero/zotero')
-  zotero_bin = File.expand_path('~/bin/zotero/zotero')
-elsif OS.mac?
-  profiles_dir = File.expand_path('~/Library/Application Support/Zotero')
-  zotero_bin = File.expand_path('/Applications/Zotero.app/Contents/MacOS/zotero')
-else
-  puts OS.report
-  exit 1
-end
-
 class IniFile
   def write_compact( opts = {} )
     filename = opts.fetch(:filename, @filename)
@@ -37,102 +26,107 @@ class IniFile
   end
 end
 
-FileUtils.mkdir_p(profiles_dir)
-profiles = File.join(profiles_dir, 'profiles.ini')
-File.open(profiles, "w") {} unless File.file?(profiles)
-profiles = IniFile.load(profiles)
-
-if !profiles.has_section?('General')
-  profiles['General'] = { 'StartWithLastProfile' => 1 }
-else
-  profiles['General']['StartWithLastProfile'] = 1
-end
-
-profile_name = 'BBTZ5TEST'
-profile_dir = File.expand_path("~/.#{profile_name}.profile")
-data_dir = File.expand_path("~/.#{profile_name}.data")
-
-profile = nil
-profiles.each{|section, param, val|
-  profile = section if param == 'Name' && val ==  profile_name
-}
-if profile.nil?
-  free = 0
-  while true do
-    profile = "Profile#{free}"
-    break unless profiles.has_section?(profile)
-    free += 1
+module BBT
+  if OS.linux?
+    profiles = File.expand_path('~/.zotero/zotero')
+    zotero = File.expand_path('~/bin/zotero/zotero')
+  elsif OS.mac?
+    profiles = File.expand_path('~/Library/Application Support/Zotero')
+    zotero = File.expand_path('/Applications/Zotero.app/Contents/MacOS/zotero')
+  else
+    raise OS.report
   end
-
-  profiles[profile]['Name'] = profile_name
-end
-profiles[profile]['IsRelative'] = 0
-profiles[profile]['Path'] = profile_dir
-profiles[profile]['Default'] = nil
-
-profiles.each_section do |section|
-  puts section
-  puts profiles[section].to_yaml
-end
-
-profiles.write_compact
-
-fixtures = File.expand_path(File.join(File.dirname(__FILE__), '../../test/fixtures'))
-profile = Selenium::WebDriver::Firefox::Profile.new(File.join(fixtures, 'profile/profile'))
-profile.log_file = File.expand_path(File.join(File.dirname(__FILE__), "#{ENV['LOGS'] || '.'}/firefox-console.log"))
-
-puts "Installing plugins..."
-plugins = File.expand_path(File.join(File.dirname(__FILE__), '../../xpi/*.xpi'))
-Dir[plugins].each{|plugin| profile.add_extension(plugin) }
-
-profile['extensions.zotero.dataDir'] = data_dir
-#profile['extensions.checkCompatibility.5.0'] = false
-
-profile['extensions.checkCompatibility.5.0'] = false
-profile['extensions.zotero.dataDir'] = data_dir
-profile['extensions.zotero.debug.log'] = true
-profile['extensions.zotero.debug.store'] = true
-profile['extensions.zotero.debug.time'] = true
-profile['extensions.zotero.firstRun2'] = false
-profile['extensions.zotero.firstRunGuidance'] = false
-profile['extensions.zotero.reportTranslationFailure'] = false
-
-FileUtils.rm_rf(profile_dir)
-FileUtils.cp_r(profile.layout_on_disk, profile_dir)
-FileUtils.rm_rf(data_dir)
-FileUtils.cp_r(File.join(fixtures, 'profile/data'), data_dir)
-puts profile_dir
-
-#job1 = fork do
-  #exec "#{zotero_bin} -P BBTZ5TEST -ZoteroDebugText -ZoteroSkipBundledFiles > #{File.expand_path('~/.BBTZ5TEST.log').shellescape} 2>&1"
-#end
-#Process.detach(job1)
-pid = Process.fork{ system("#{zotero_bin} -P BBTZ5TEST -ZoteroDebugText -ZoteroSkipBundledFiles > #{File.expand_path('~/.BBTZ5TEST.log').shellescape} 2>&1") }
-at_exit { Process.kill("HUP", pid) }
-
-puts Benchmark.measure {
-  attempts = 0
-  while true
-    begin
-      sleep(1)
-      result = HTTParty.post("http://127.0.0.1:23119/debug-bridge/execute", headers: { 'Content-Type' => 'text/plain' }, body: """
-        Zotero.debug('BBT: waiting for Zotero ready...');
-        yield Zotero.Schema.schemaUpdatePromise;
-        Zotero.debug('BBT: Zotero ready');
-        return true;
-      """)
-      puts result
-      break
-    rescue Errno::ECONNREFUSED => e
-      print '.'
-      attempts += 1
-      raise "Could not connect to Zotero after #{attempts} attempts" if attempts >= 60
+  
+  FileUtils.mkdir_p(profiles)
+  profiles_ini = File.join(profiles, 'profiles.ini')
+  File.open(profiles_ini, "w") {} unless File.file?(profiles_ini)
+  profiles_ini = IniFile.load(profiles_ini)
+  
+  if !profiles_ini.has_section?('General')
+    profiles_ini['General'] = { 'StartWithLastProfile' => 1 }
+  else
+    profiles_ini['General']['StartWithLastProfile'] = 1
+  end
+  
+  profile_name = 'BBTZ5TEST'
+  profile_tgt = File.expand_path("~/.#{profile_name}.profile")
+  data_tgt = File.expand_path("~/.#{profile_name}.data")
+  
+  profile_id = nil
+  profiles_ini.each{|section, param, val|
+    profile_id = section if param == 'Name' && val ==  profile_name
+  }
+  if profile_id.nil?
+    free = 0
+    while true do
+      profile_id = "Profile#{free}"
+      break unless profiles_ini.has_section?(profile_id)
+      free += 1
     end
+  
+    profiles_ini[profile_id]['Name'] = profile_name
   end
-}
-
-result = HTTParty.post("http://127.0.0.1:23119/debug-bridge/execute", headers: { 'Content-Type' => 'text/plain' }, body: """
-  var appStartup = Components.classes['@mozilla.org/toolkit/app-startup;1'].getService(Components.interfaces.nsIAppStartup);
-  appStartup.quit(Components.interfaces.nsIAppStartup.eAttemptQuit);
-""")
-puts result
+  profiles_ini[profile_id]['IsRelative'] = 0
+  profiles_ini[profile_id]['Path'] = profile_tgt
+  profiles_ini[profile_id]['Default'] = nil
+  profiles_ini.write_compact
+  
+  fixtures = File.expand_path(File.join(File.dirname(__FILE__), '../../test/fixtures'))
+  profile = Selenium::WebDriver::Firefox::Profile.new(File.join(fixtures, 'profile/profile'))
+  #profile.log_file = File.expand_path(File.join(File.dirname(__FILE__), "#{ENV['LOGS'] || '.'}/firefox-console.log"))
+  
+  plugins = File.expand_path(File.join(File.dirname(__FILE__), '../../xpi/*.xpi'))
+  Dir[plugins].each{|plugin|
+    puts "Installing #{plugin}"
+    profile.add_extension(plugin)
+  }
+  
+  profile['extensions.checkCompatibility.5.0'] = false
+  profile['extensions.zotero.dataDir'] = data_tgt
+  profile['extensions.zotero.debug.log'] = true
+  profile['extensions.zotero.debug.store'] = true
+  profile['extensions.zotero.debug.time'] = true
+  profile['extensions.zotero.firstRun2'] = false
+  profile['extensions.zotero.firstRunGuidance'] = false
+  profile['extensions.zotero.reportTranslationFailure'] = false
+  
+  FileUtils.rm_rf(profile_tgt)
+  FileUtils.cp_r(profile.layout_on_disk, profile_tgt)
+  FileUtils.rm_rf(data_tgt)
+  FileUtils.cp_r(File.join(fixtures, 'profile/data'), data_tgt)
+  
+  pid = Process.fork{ system("#{zotero} -P BBTZ5TEST -ZoteroDebugText > #{File.expand_path('~/.BBTZ5TEST.log').shellescape} 2>&1") }
+  
+  puts Benchmark.measure {
+    print "Starting Zotero."
+    attempts = 0
+    while true
+      begin
+        sleep(1)
+        result = HTTParty.post("http://127.0.0.1:23119/debug-bridge/execute", headers: { 'Content-Type' => 'text/plain' }, body: """
+          Zotero.debug('BBT: waiting for Zotero ready...');
+          yield Zotero.Schema.schemaUpdatePromise;
+          Zotero.debug('BBT: Zotero ready');
+          return true;
+        """)
+        if result.body.to_s.strip == 'true'
+          puts "Zotero Running"
+          break
+        end
+      rescue Errno::ECONNREFUSED => e
+        print '.'
+        attempts += 1
+        raise "Could not connect to Zotero after #{attempts} attempts" if attempts >= 60
+      end
+    end
+  }
+  
+  at_exit {
+    result = HTTParty.post("http://127.0.0.1:23119/debug-bridge/execute", headers: { 'Content-Type' => 'text/plain' }, body: """
+      var appStartup = Components.classes['@mozilla.org/toolkit/app-startup;1'].getService(Components.interfaces.nsIAppStartup);
+      appStartup.quit(Components.interfaces.nsIAppStartup.eAttemptQuit);
+    """)
+    sleep(5)
+    Process.kill("HUP", pid)
+  }
+end
