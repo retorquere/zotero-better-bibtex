98e5bc7667b161b5ce928ae486e3451be0e3ebad
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2017-07-20 02:01:26 +0200

    0 is no longer the user lib

--- features/basics.feature
+++ features/basics.feature
@@ -1,5 +1,5 @@
 Feature: Basics
 
 Scenario: Better BibTeX debug import
-  When I import 3 references from 'export/(non-)dropping particle handling #313.json'
-  Then the library should have 3 items
+  When I import 53 references from 'export/(non-)dropping particle handling #313.json'
+  Then the library should have 53 references
--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -6,13 +6,18 @@ When /^I import (\d+) references from (['"])([^\2]+)\2$/ do |n, quote, json|
       var file = Components.classes['@mozilla.org/file/local;1'].createInstance(Components.interfaces.nsILocalFile);
       file.initWithPath(args.filename);
 
+      var items = yield Zotero.Items.getAll(Zotero.Libraries.userLibraryID, false, false, true);
+      var before = items.length;
+
       Zotero.debug('{better-bibtex} starting import at ' + new Date());
       yield Zotero_File_Interface.importFile(file, false);
       Zotero.debug('{better-bibtex} import finished at ' + new Date());
 
-      var items = yield Zotero.Items.getAll(0, false, false, true);
-      Zotero.debug('{better-bibtex} found ' + items.length + ' items');
-      return items.length;
+      var items = yield Zotero.Items.getAll(Zotero.Libraries.userLibraryID, false, false, true);
+      var after = items.length;
+
+      Zotero.debug('{better-bibtex} found ' + (after - before) + ' items');
+      return (after - before);
     """
   )
   expect(imported).to eq(Integer(n))
@@ -20,7 +25,7 @@ end
 
 Then /^the library should have (\d+) references/ do |n|
   library = execute("""
-      var items = yield Zotero.Items.getAll(0, false, false, true);
+      var items = yield Zotero.Items.getAll(Zotero.Libraries.userLibraryID, false, false, true);
       return items.length;
     """
   )
