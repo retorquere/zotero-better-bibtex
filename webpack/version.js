6c6d3806a5169d5e8bfef343f661eed33e9f45f7
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2017-07-29 20:55:16 +0200

    circle

--- webpack/version.js
+++ webpack/version.js
@@ -1,4 +1,7 @@
 var version = require('../package.json').version;
 
+if (process.env.CIRCLE_BUILD_NUM) {
+  version += 'circle-' + process.env.CIRCLE_BUILD_NUM;
+}
 // do stuff for circle/local.release builds here
 module.exports = version;
