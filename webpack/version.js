048eb02ab360255ca601992ee940042469300dd4
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2017-08-07 01:07:30 +0200

    build-version

--- webpack/version.js
+++ webpack/version.js
@@ -1,15 +1,11 @@
 var version = require('../package.json').version;
 var os = require("os");
 
-console.log('version: base=', version);
-console.log('version: CIRCLE_BUILD_NUM=', process.env.CIRCLE_BUILD_NUM)
-console.log('version: CIRCLE_TAG=', process.env.CIRCLE_TAG)
-if (process.env.CIRCLE_BUILD_NUM && !!process.env.CIRCLE_TAG) {
+if (process.env.CIRCLE_BUILD_NUM && !process.env.CIRCLE_TAG) {
   version += '.circle.' + process.env.CIRCLE_BUILD_NUM;
 } else if (process.env.CIRCLECI != 'true') {
   var hostname = os.hostname();
   var username = os.userInfo().username
   version += '.' + hostname + '.' + username;
 }
-console.log('version: final=', version);
 module.exports = version;
