fe45d18d9d05dc62ffd8a2f75edcdb3fdabfd927
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2017-08-08 17:38:51 +0200

    branch

--- webpack/version.js
+++ webpack/version.js
@@ -2,7 +2,7 @@ var version = require('../package.json').version;
 var os = require("os");
 
 if (process.env.CIRCLE_BUILD_NUM && !process.env.CIRCLE_TAG) {
-  version += '.circle.' + process.env.CIRCLE_BUILD_NUM;
+  version += '.' + process.env.CIRCLE_BRANCH + '.' + process.env.CIRCLE_BUILD_NUM;
 } else if (process.env.CIRCLECI != 'true') {
   var hostname = os.hostname();
   var username = os.userInfo().username
