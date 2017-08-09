80ecb9ae7f8af9bfca0e4a1e1d0ddf9fdf935187
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2017-08-09 23:25:07 +0200

    sensible ordering of debug builds

--- webpack/version.js
+++ webpack/version.js
@@ -2,10 +2,8 @@ var version = require('../package.json').version;
 var os = require("os");
 
 if (process.env.CIRCLE_BUILD_NUM && !process.env.CIRCLE_TAG) {
-  version += '.' + process.env.CIRCLE_BRANCH + '.' + process.env.CIRCLE_BUILD_NUM;
+  version += [ '', process.env.CIRCLE_BUILD_NUM, process.env.CIRCLE_BRANCH].join('.')
 } else if (process.env.CIRCLECI != 'true') {
-  var hostname = os.hostname();
-  var username = os.userInfo().username
-  version += '.' + hostname + '.' + username;
+  version += [ '', os.userInfo().username, os.hostname() ].join('.')
 }
 module.exports = version;
