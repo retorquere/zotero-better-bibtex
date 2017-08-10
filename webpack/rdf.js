2c34074fadb9fdfd59fc945f89e5b26fad09a8eb
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2017-08-10 10:35:34 +0200

    prep for tools release

--- webpack/rdf.js
+++ webpack/rdf.js
@@ -3,18 +3,28 @@
 const fs = require('fs');
 const pug = require('pug');
 const path = require('path');
-const version = require('./version')
+const uriTemplate = require('uri-templates')
+const pkg = Object.assign({}, require('../package.json'))
+
+if (!pkg.id) pkg.id = (pkg.name.replace(/^zotero-/, '') + '@' + pkg.author.email.replace(/.*@/, '')).toLowerCase()
+if (pkg.xpi) Object.assign(pkg, pkg.xpi)
+
+pkg.version = require('./version')
+
+if (pkg.updateLink) pkg.updateLink = uriTemplate(pkg.updateLink).fill({version: pkg.version})
 
 module.exports = function() {
   var template;
 
+  var options_and_vars = Object.assign(pkg, { pretty: true})
+
   console.log('generating install.rdf')
   template = fs.readFileSync(path.join(__dirname, 'install.rdf.pug'), 'utf8')
-  template = pug.render(template, { pretty: true, version: version })
+  template = pug.render(template, options_and_vars)
   fs.writeFileSync(path.join(__dirname, '../build/install.rdf'), template, { encoding: 'utf8' })
 
   console.log('generating update.rdf')
   template = fs.readFileSync(path.join(__dirname, 'update.rdf.pug'), 'utf8')
-  template = pug.render(template, { pretty: true, version: version })
+  template = pug.render(template, options_and_vars)
   fs.writeFileSync(path.join(__dirname, '../gen/update.rdf'), template, { encoding: 'utf8' })
 }
