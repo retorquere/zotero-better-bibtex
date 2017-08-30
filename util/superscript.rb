a8f8060b274becbd3b9e5a8b7db58577b7791d88
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2017-08-30 08:52:51 +0200

    superscript

---
+++ util/superscript.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+require 'open-uri'
+require 'json'
+
+open('http://unicode.org/reports/tr30/datafiles/SuperscriptFolding.txt').readlines.each{|line|
+  next unless line =~ /^[0-9A-F]{4};/
+  line = line.split
+  line[0].sub!(/;/, '')
+
+  next if line[0] > '3192'
+  case line[0]
+    when '00AA', '00BA', '06E5', '06E6'
+      next
+  end
+
+  if line[1] >= '0020' && line[1] <= '007E'
+    chr = line[1].to_i(16).chr
+    chr = chr.to_json unless chr =~ /[a-z0-9]/i
+  else
+    chr = "'\\u" + line[1] + "'"
+  end
+
+  puts "    #{chr}: '\\u#{line[0]}'"
+  puts "    \"-\": '\\u#{line[0]}'" if chr == "'\\u2212'"
+}
