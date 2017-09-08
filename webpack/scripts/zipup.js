1462266698ec7dd27e1ac25d3f26dc692bd6ec9b
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2017-09-08 12:17:33 +0200

    cleanup

--- webpack/scripts/zipup.js
+++ webpack/scripts/zipup.js
@@ -2,11 +2,11 @@ const fs = require('fs');
 const archiver = require('archiver');
 const version = require('../version');
 
-const xpi = `${__dirname}/../xpi/${process.argv[3]}-${version}.xpi`;
+const xpi = `${__dirname}/../../xpi/${process.argv[3]}-${version}.xpi`;
 console.log(`creating ${xpi}`);
 if (fs.existsSync(xpi)) { fs.unlinkSync(xpi); }
 
 const archive = archiver.create('zip', {});
 archive.pipe(fs.createWriteStream(xpi));
-archive.directory(`${__dirname}/../${process.argv[2]}`, false);
+archive.directory(`${__dirname}/../../${process.argv[2]}`, false);
 archive.finalize();
