9407620c3b803927dd38c90e588f0439db45eee9
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2017-09-20 12:29:38 +0200

    module loading sentinel #norelease

--- webpack/loaders/wrap.js
+++ webpack/loaders/wrap.js
@@ -1,9 +1,20 @@
 const utils = require('loader-utils')
+const indent = require('indent-string');
 
 module.exports = function (source) {
   // this.cacheable()
 
   var src = this.resourcePath.substring(process.cwd().length + 1);
   // const opts = utils.getOptions(this) || {}
-  return [].concat(`Zotero.debug('BBT: loading ${src}')`, source, `Zotero.debug('BBT: loaded ${src}')`).join('\n').trim()
+
+  var pre = '';
+  pre   += `Zotero.debug('BBT: loading ${src}')\n`;
+  pre   += 'try\n';
+
+  var post = '';
+  post  += `  Zotero.debug('BBT: loaded ${src}')\n`;
+  post  += 'catch err\n';
+  post  += `  Zotero.debug('Error: BBT: load of ${src} failed:' + err)\n`;
+
+  return pre + indent(source, 2) + post;
 }
