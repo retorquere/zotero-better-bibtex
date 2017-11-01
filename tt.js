ed490ceb5e4ac4c30b59693b73e4457b815b1cac
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2017-11-01 11:15:50 +0100

    @bcf-loader

--- tt.js
+++ tt.js
@@ -1,105 +1,145 @@
 const xpath = require('xpath')
 const dom = require('xmldom').DOMParser
 const fs = require('fs')
+const jsesc = require('jsesc');
+const _ = require('lodash');
+const ejs = require('ejs');
 
 var xml = fs.readFileSync('resource/bibtex/biblatex.qr.bcf', 'utf8')
 var doc = new dom().parseFromString(xml)
 var select = xpath.useNamespaces({"bcf": "https://sourceforge.net/projects/biblatex"});
 
-const EntryTypes = {}
-const EntryFields = {}
-var all = null
+var BCF = {
+  // combinations fo allowed fields
+  fieldSet: {},
 
+  // per type, array of fieldset names that make up the allowed fields for this type
+  allowed: {},
+
+  // combinations of required fields and the types they apply to
+  required: {}
+};
+
+// get all the possible entrytypes and apply the generic fields
 for (const type of select("//bcf:entrytypes/bcf:entrytype", doc)) {
-  EntryTypes[type.textContent] = { entryfields: null };
+  BCF.allowed[type.textContent] = ['optional']
 }
 
+// gather the fieldsets
 for (const node of select("//bcf:entryfields", doc)) {
-  var entrytypes = select('./bcf:entrytype', node).map(type => type.textContent)
+  var types = select('./bcf:entrytype', node).map(type => type.textContent).sort()
 
-  var name = entrytypes.join('-')
-  name = name || '*'
+  var setname = types.length == 0 ? 'optional' : 'optional_' + types.join('_');
+  if (BCF.fieldSet[setname]) throw new Error(`field set ${setname} exists`);
 
-  EntryFields[name] = select('./bcf:field', node).map(field => field.textContent)
+  // find all the field names allowed by this set
+  BCF.fieldSet[setname] = new Set(select('./bcf:field', node).map(field => field.textContent))
 
-  for (const entrytype of entrytypes) {
-    if EntryTypes
-  }
-
-  var isAll = true;
-  var allowed = select('./bcf:field', node).map(field => field.textContent)
-
-  for (const type of select('./bcf:entrytype', node)) {
-    schemas[type.textContent].properties = allowed.reduce((acc, field) => { acc[field] = { type: 'object' }; return acc }, {})
-    isAll = false;
-  }
-
-  if (isAll) {
-    all = allowed
-  }
-}
-
-for (const [type, allowed] of Object.entries(schemas)) {
-  for (const field of all) {
-    schemas[type].properties = schemas[type].properties || {}
-    schemas[type].properties[field] = { type: 'object' }
-  }
-}
-
-function addConstraint(typeSchema, constraint) {
-  var fields
-
-  if (constraint.localName == 'fieldxor') {
-    fields = Array.from(constraint.childNodes).filter(child => child.localName).map(field => field.textContent)
-    typeSchema.allOf = (typeSchema.allOf || []).concat({
-      oneOf: fields.map(field => { return { required: [ field ] } })
-    })
-  }
-
-  else if (constraint.localName == 'fieldor') {
-    fields = Array.from(constraint.childNodes).filter(child => child.localName).map(field => field.textContent)
-    typeSchema.allOf = (typeSchema.allOf || []).concat({
-      anyOf: fields.map(field => { return { required: [ field ] } })
-    })
-  }
-
-  else if (constraint.localName) {
-    fields = [constraint.textContent]
-    typeSchema.required = (typeSchema.required || []).concat(constraint.textContent);
-  }
-
-  else return
-
-  for (const field of fields) {
-    typeSchema.properties = typeSchema.properties || {}
-    typeSchema.properties[field] = { type: 'object' }
-  }
-}
-
-for (const constraints of select('.//bcf:constraints', doc)) {
-  for (var type of select('./bcf:entrytype', constraints)) {
-    type = type.textContent
-
-    for (var constraint of select('./bcf:constraint', constraints)) {
-      if (constraint.getAttribute('type') != 'mandatory') continue
-
-      for (const field of Array.from(constraint.childNodes)) {
-        addConstraint(schemas[type], field)
-      }
+  // assign the fieldset to the types it applies to
+  for (const type of types) {
+    if (!BCF.allowed[type]) {
+      throw new Error(`Unknown reference type ${type}`)
+    } else {
+      BCF.allowed[type] = _.uniq(BCF.allowed[type].concat(setname))
     }
   }
 }
 
-fs.writeFileSync('biblatex.schema.json', JSON.stringify(schemas, null, 2));
+for (const node of select('.//bcf:constraints', doc)) {
+  var mandatory = select(".//bcf:constraint[@type='mandatory']", node);
+  switch (mandatory.length) {
+    case 0:
+      continue
+    case 1:
+      mandatory = mandatory[0]
+      break
+    default:
+      throw new Error(`found ${mandatory.length} constraints, expected 1`)
+  }
 
-var Ajv = require('ajv'); // version >= 4.7.4
-var ajv = new Ajv({sourceCode: true}); // this option is required
-var pack = require('ajv-pack');
+  var types = select('./bcf:entrytype', node).map(type => type.textContent).sort()
+  if (!types.length) throw new Error(`constraint has no types`)
 
-for (const [type, schema] of Object.entries(schemas)) {
-  console.log(type)
-  var validate = ajv.compile(schema);
-  var moduleCode = pack(ajv, validate);
+  var setname = 'required_' + types.join('_')
+  if (BCF.fieldSet[setname] || BCF.required[setname]) throw new Error(`constraint set ${setname} exists`);
 
-  fs.writeFileSync(`biblatex.${type}.schema.js`, moduleCode);
+  // find all the field names allowed by this set
+  BCF.fieldSet[setname] = new Set(select('.//bcf:field', node).map(field => field.textContent))
+
+  // allow the fields that are required
+  for (const type of types) {
+    if (!BCF.allowed[type]) {
+      throw new Error(`Unknown reference type ${type}`)
+    } else {
+      BCF.allowed[type] = _.uniq(BCF.allowed[type].concat(setname))
+    }
+  }
+
+  BCF.required[setname] = { types, fields: []}
+
+  for (const constraint of Array.from(mandatory.childNodes)) {
+    switch (constraint.localName || '#text') {
+      case '#text':
+        break
+
+      case 'field':
+        BCF.required[setname].fields.push(constraint.textContent)
+        break
+
+      case 'fieldor':
+      case 'fieldxor':
+        BCF.required[setname].fields.push({
+          [constraint.localName.replace('field', '')]: select('./bcf:field', constraint).map(field => field.textContent)
+        })
+        break
+
+      default:
+        throw new Error(`Unexpected constraint type ${constraint.localName}`)
+    }
+  }
 }
+
+fs.writeFileSync('biblatex.schema.json', JSON.stringify(BCF, null, 2));
+
+var template = `
+const fieldSet = ${jsesc(BCF.fieldSet, { compact: false, indent: '  ' })};
+const allowed = {
+<%_ for (const [type, sets] of Object.entries(allowed)) { -%>
+  <%= type -%>: [ <%- sets.map(name => "fieldSet." + name).join(', ') -%> ],
+<%_ }; -%>
+};
+const required = [
+  <%_ for (const [, {types, fields}] of Object.entries(required)) { -%>
+    {
+      types: new Set(<%- JSON.stringify(types) -%>),
+      check: function(ref, report) {
+        <%_ for (const field of fields) { -%>
+          <%_ if (typeof field == 'string') { -%>
+            if (!ref.has.<%= field -%>) report.push("Missing required field '<%= field -%>'")
+          <%_ } else if (field.or) { -%>
+            if (!(<%- field.or.map(field => "this.has." + field).join(' || ') -%>)) report.push("At least one of <%- field.or.map(f => "'" + f + "'").join(' / ') -%> must be present")
+          <%_ } else if (field.xor) { -%>
+            if (!ref.has.<%= field.xor[0] -%> === !ref.has.<%= field.xor[1] -%>) report.push("Exactly one of <%- field.xor.map(f => "'" + f + "'").join(' / ') -%> must be present")
+          <%_ } -%>
+        <%_ } -%>
+      }
+    },
+  <%_ } -%>
+];
+
+module.exports = function(ref) {
+  var type = ref.referencetype.toLowerCase();
+
+  if (!allowed[type]) return ["I don't know how to type-check " + type];
+
+  var unexpected = Object.keys(ref.has).filter(field => !allowed[type].find(set => set.has(field)));
+  var report = unexpected.map(field => "Unexpected field '" + field + "'")
+
+  for (const test of required) {
+    if (test.types.has(type)) test.check(ref, report)
+  }
+
+  return report;
+}
+`;
+fs.writeFileSync('biblatex.schema.js', ejs.render(template, BCF));
