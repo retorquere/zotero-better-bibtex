13820bae25031d8f574dbfe39a8dcfc722adde47
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2017-11-06 09:30:58 +0100

    fixes #767

--- content/Preferences.ts
+++ content/Preferences.ts
@@ -13,27 +13,6 @@ import KeyManager = require('./keymanager.ts')
 import AutoExport = require('./auto-export.ts')
 import Translators = require('./translators.ts')
 
-function autoExportNameCollectionPath(id) {
-  if (!id) return ''
-  const coll = Zotero.Collections.get(id)
-  if (!coll) return ''
-
-  if (coll.parent) return `${autoExportNameCollectionPath(coll.parent)}/${coll.name}`
-  return coll.name
-}
-function autoExportName(ae, full = false) {
-  let name
-  switch (ae.type) {
-    case 'library':
-      name = Zotero.Libraries.getName(ae.id)
-      break
-    case 'collection':
-      name = full ? autoExportNameCollectionPath(ae.id) : Zotero.Collections.get(ae.id).name
-      break
-  }
-  return name || ae.path
-}
-
 class AutoExportPrefPane {
   protected AutoExport: AutoExportPrefPane // tslint:disable-line:variable-name
 
@@ -66,11 +45,17 @@ class AutoExportPrefPane {
       return
     }
 
-    const exportlist = document.getElementById('better-bibtex-export-list')
+    const exportlist = document.getElementById('better-bibtex-auto-exports')
     if (!exportlist) return
     while (exportlist.hasChildNodes()) {
       exportlist.removeChild(exportlist.firstChild)
     }
+
+    const columns = Array.from(
+      document.getElementById('better-bibtex-export-list').getElementsByTagName('treecol') as NodeList
+    ).map(
+      col => (col as Element).getAttribute('id').replace(/^better-bibtex-preferences-auto-export-/, '')
+    )
 
     for (const ae of AutoExport.db.chain().simplesort('path').data()) {
       const treeitem = exportlist.appendChild(document.createElement('treeitem'))
@@ -79,23 +64,64 @@ class AutoExportPrefPane {
       const treerow = treeitem.appendChild(document.createElement('treerow'))
       // TODO: https://github.com/Microsoft/TypeScript/issues/19186
       // TODO: https://github.com/Microsoft/TypeScript/issues/1260
-      const cells: Array<{label: string, tooltip?: string}> = [
-        { label: `${ae.type}: ${autoExportName(ae)}` },
-        { label: ae.status + (ae.updated ? ` (${ae.updated})` : ''), tooltip: ae.error },
-        { label: ae.path.replace(/.*[\\\/]/, ''), tooltip: ae.path },
-        { label: (Translators.byId[ae.translatorID] ? Translators.byId[ae.translatorID].label : undefined) || '??' },
-        { label: `${ae.useJournalAbbreviation}` },
-        { label: `${ae.exportNotes}` },
-      ]
-      for (const cell of cells) {
-        debug('Preferences.AutoExport.refresh:', cell)
+
+      for (const column of columns) {
+        debug('Preferences.AutoExport.refresh:', column)
         const treecell = treerow.appendChild(document.createElement('treecell'))
         treecell.setAttribute('editable', 'false')
-        treecell.setAttribute('label', cell.label)
-        if (cell.tooltip) treecell.setAttribute('tooltiptext', cell.tooltip)
+
+        switch (column) {
+          case 'collection':
+            treecell.setAttribute('label', `${ae.type}: ${this.autoExportName(ae)}`)
+            break
+
+          case 'status':
+            treecell.setAttribute('label', (ae.status + (ae.updated ? ` (${ae.updated})` : '')) + (ae.error ? `: ${ae.error}` : ''))
+            break
+
+          case 'target':
+            treecell.setAttribute('label', ae.path)
+            break
+
+          case 'translator':
+            treecell.setAttribute('label', Translators.byId[ae.translatorID] ? Translators.byId[ae.translatorID].label : '??')
+            break
+
+          case 'auto-abbrev':
+            treecell.setAttribute('label', `${ae.useJournalAbbreviation}`)
+            break
+
+          case 'notes':
+            treecell.setAttribute('label', `${ae.exportNotes}`)
+            break
+
+          default:
+            throw new Error(`Unexpected auto-export column ${column}`)
+        }
       }
     }
+  }
 
+  private autoExportNameCollectionPath(id) {
+    if (!id) return ''
+    const coll = Zotero.Collections.get(id)
+    if (!coll) return ''
+
+    if (coll.parent) return `${this.autoExportNameCollectionPath(coll.parent)}/${coll.name}`
+    return coll.name
+  }
+
+  private autoExportName(ae) {
+    let name
+    switch (ae.type) {
+      case 'library':
+        name = Zotero.Libraries.getName(ae.id)
+        break
+      case 'collection':
+        name = this.autoExportNameCollectionPath(ae.id)
+        break
+    }
+    return name || ae.path
   }
 }
 
@@ -160,7 +186,7 @@ export = new class PrefPane {
     debug('manual key rescan done')
   }
 
-  public update() {
+  private update() {
     this.checkCitekeyFormat()
 
     if (ZoteroConfig.isJurisM) {
