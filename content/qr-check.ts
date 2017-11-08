de080c89fe3c1ad1d06c2a9e9ed2b38c2a986eb5
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2017-11-08 13:32:58 +0100

    value-qr

--- content/qr-check.ts
+++ content/qr-check.ts
@@ -1,20 +1,37 @@
+import ISBN = require('isbnjs')
+import ISSN = require('issn')
+import EAN = require('barcoder')
+import DateParser = require('./dateparser.ts')
+
+const isnm_prefix = '9790'
+
 export = (value, test, params = null) => {
   switch (test) {
     case 'isbn':
-      return ''
+      const isbn = ISBN.parse(value)
+      return isbn.isIsbn10() || isbn.isIsbn13() ? '' : 'not a valid ISBN'
 
     case 'issn':
-      return ''
+      return ISSN(value) ? '' : 'not a valid ISSN'
 
     case 'ismn':
-      return ''
+      value = value.replace(/[ -]/g, '')
+
+      if (value.length === 10) { // tslint:disable-line:no-magic-numbers
+        if (value[0] !== 'M') return 'not a valid ISMN'
+        value = isnm_prefix + value.substring(1)
+      }
+
+      // tslint:disable-next-line:no-magic-numbers
+      if (value.length !== 13 || !value.startsWith(isnm_prefix)) return 'not a valid ISMN'
+
+      return EAN.validate(value) ? '' : 'not a valid ISMN'
 
     case 'date':
-      return ''
+      return DateParser.isEDTF(value) ? '' : 'not a valid ETDF date'
 
     case 'pattern':
-      if (new RegExp(`^${params}$`, 'i').test(value)) return ''
-      return 'not a valid value'
+      return (new RegExp(`^${params}$`, 'i').test(value)) ? '' : 'not a valid value'
 
     default:
       throw new Error(`I don't know how to test for ${test}`)
