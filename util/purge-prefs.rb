f9508e51dd496ca663f4762cc0fe3aef61419355
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2017-11-10 00:53:38 +0100

    test cleanup

--- util/purge-prefs.rb
+++ util/purge-prefs.rb
@@ -5,12 +5,11 @@ require 'json'
 root = File.join(File.dirname(__FILE__), '..')
 supported = JSON.parse(File.read(File.join(root, 'defaults/preferences/defaults.json'))).keys
 
-Dir[File.join(root, 'test/fixtures/export/*.json')].each{|lib|
+Dir[File.join(root, 'test/fixtures/*/*.json')].each{|lib|
   next if lib =~ /\.csl.json$/
 
   data = JSON.parse(File.read(lib))
   next unless data.is_a?(Hash) && data['config'] && data['config']['preferences']
-
 
   data['config']['preferences'].keys.each{|key|
     data['config']['preferences'].delete(key) unless supported.include?(key)
