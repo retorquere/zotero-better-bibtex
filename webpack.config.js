0325a0c367b37695ef3e9a0abb3bbc14bececedb
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2017-11-10 00:37:49 +0100

    submodules

--- webpack.config.js
+++ webpack.config.js
@@ -37,11 +37,7 @@ for (let label of Object.keys(translators)) {
 }
 fs.writeFileSync(path.join(__dirname, 'gen/translators.json'), JSON.stringify(tr, null, 2));
 
-for (const submodule of ['citeproc-js/docs', 'citeproc-js/locale', 'citeproc-js']) {
-  console.log(`update ${submodule}`);
-  if (shell.exec(`cd ${submodule} && git checkout master && git pull`).code != 0) throw `${submodule} update failed`;
-  if (submodule.indexOf('/') === -1 && shell.exec(`git submodule update --depth 1 --recursive -- ${submodule}`).code != 0) throw `${submodule} update failed`;
-}
+if (shell.exec(`git submodule update --init --recursive`).code != 0) throw 'submodule update failed';
 
 dateparser(path.join(__dirname, 'citeproc-js/locale'), path.join(__dirname, 'gen/dateparser-data.json'));
 rdf();
