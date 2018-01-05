c8020f94eee14583256b00646bab1e39e195865d
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2018-01-05 22:06:14 +0100

    fixes #868

--- content/dateparser.ts
+++ content/dateparser.ts
@@ -197,6 +197,19 @@ function parse(value, toplevel = true) {
     }
   }
 
+  if (!parsed) {
+    if (m = /^([0-9]+)\s([^0-9]+)\s+([0-9]+)$/.exec(value.normalize('NFC').replace(months_re, _ => months[_.toLowerCase()] || _))) {
+      const [ , year, month, day ] = m
+      if (months[month]) {
+        try {
+          parsed = normalize_edtf(EDTF.parse(edtfy(`${day} ${month} ${year}`)))
+        } catch (err) {
+          parsed = null
+        }
+      }
+    }
+  }
+
   if (toplevel && !parsed) {
     for (const sep of ['--', '-', '/', '_', '–']) {
       const split = value.split(sep)
