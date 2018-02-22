a47b9ca5d981a498f458146d12ec27e2ff70fd6f
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2018-02-22 09:12:38 +0100

    offline

--- setup/submodules.ts
+++ setup/submodules.ts
@@ -1,6 +1,23 @@
 // tslint:disable:no-console
 
 import * as shell from 'shelljs'
+import rp = require('request-promise')
 
-console.log('update submodules')
-if (shell.exec('git submodule update --init --recursive --remote').code !== 0) throw new Error('submodule update failed')
+async function main() {
+  let online = true
+  try {
+    await rp('https://github.com')
+  } catch (err) {
+    console.log(err)
+    online = false
+  }
+
+  if (online) {
+    console.log('update submodules')
+    if (shell.exec('git submodule update --init --recursive --remote').code !== 0) throw new Error('submodule update failed')
+  } else {
+    console.log('offline, skipping submodules update')
+  }
+}
+
+main()
