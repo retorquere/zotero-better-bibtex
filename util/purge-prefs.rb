af12e5eba491a039378af2c741c7e5a0f15daf24
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2018-07-04 16:26:49 +0200

    fixes #1020

--- util/purge-prefs.rb
+++ util/purge-prefs.rb
@@ -9,18 +9,28 @@ Dir[File.join(root, 'test/fixtures/*/*.json')].each{|lib|
   next if lib =~ /\.csl.json$/
 
   data = JSON.parse(File.read(lib))
-  next unless data.is_a?(Hash) && data['config'] && data['config']['preferences']
+  next unless data.is_a?(Hash) && data['config']
 
-  data['config']['preferences'].keys.each{|key|
-    data['config']['preferences'].delete(key) unless supported.include?(key)
-  }
+  resave = false
+
+  if data['config']['preferences']
+    data['config']['preferences'].keys.each{|key|
+      next if supported.include?(key)
+      data['config']['preferences'].delete(key) 
+      resave = true
+    }
+  end
 
   if data['items'] && lib =~ /\/import\//
     data['items'].each{|item|
       next unless item['extra']
-      item['extra'].sub!(/(^|\n)bibtex:/, "\\1Citation Key:")
+      resave = true
+      item['extra'].sub!(/\nbibtex:/, "\nCitation Key:")
+      item['extra'].sub!(/^bibtex:/, "Citation Key:")
     }
   end
 
-  File.open(lib, 'w'){|f| f.puts(JSON.pretty_generate(data)) }
+  puts lib if resave
+
+  File.open(lib, 'w'){|f| f.puts(JSON.pretty_generate(data)) } if resave
 }
