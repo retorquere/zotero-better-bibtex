dd76835fa3344584fdf91a05fb67f3fbbd1860fd
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2018-07-15 00:53:55 +0200

    not liking the new differences of the new exporter formats

--- util/fix-test-fixtures.rb
+++ util/fix-test-fixtures.rb
@@ -3,19 +3,16 @@
 require 'json'
 
 root = File.join(File.dirname(__FILE__), '..')
-supported = JSON.parse(File.read(File.join(root, 'gen/preferences.json'))).keys
+$supported = JSON.parse(File.read(File.join(root, 'gen/preferences.json'))).keys
 
-Dir[File.join(root, 'test/fixtures/*/*.json')].each{|lib|
-  next if lib =~ /\.csl.json$/
-
-  data = JSON.parse(File.read(lib))
-  next unless data.is_a?(Hash)
+def fixBBTJSON(lib, data)
+  return unless data.is_a?(Hash)
 
   resave = false
 
   if data['config'] && data['config']['preferences']
     data['config']['preferences'].keys.each{|key|
-      next if supported.include?(key)
+      next if $supported.include?(key)
       data['config']['preferences'].delete(key) 
       resave = true
     }
@@ -45,25 +42,21 @@ Dir[File.join(root, 'test/fixtures/*/*.json')].each{|lib|
           end
         }
       }
-
-      (item['creators'] || []).each{|creator|
-        if creator['name']
-          creator['lastName'] = creator['name']
-          creator['fieldMode'] = 1
-          creator.delete('name')
-          resave = true
-        end
-
-        if creator['fieldMode'] == 1 && creator.key?('firstName')
-          creator.delete('firstName')
-          resave = true
-        end
-      }
     }
   end
 
   if data['items']
     data['items'].each{|item|
+      (item['creators'] || []).each{|creator|
+        if creator['fieldMode'] == 1
+          creator['name'] = creator['lastName']
+          creator.delete('fieldMode')
+          creator.delete('firstName')
+          creator.delete('lastName')
+          resave = true
+        end
+      }
+
       if item['relations'] || item['collections']
         resave = true
         item.delete('relations')
@@ -79,7 +72,47 @@ Dir[File.join(root, 'test/fixtures/*/*.json')].each{|lib|
     }
   end
 
-  puts lib if resave
+  return unless resave
 
-  File.open(lib, 'w'){|f| f.puts(JSON.pretty_generate(data)) } if resave
+  puts lib
+
+  File.open(lib, 'w'){|f| f.puts(JSON.pretty_generate(data)) }
+end
+
+def fixCSL(lib, data)
+  return unless data.is_a?(Array)
+
+  resave = false
+
+  data.each{|item|
+
+    %w{director editor author}.each{|kind|
+      next unless item[kind]
+
+      item[kind].each{|creator|
+        if creator['isInstitution'] == 1
+          creator['literal'] ||= creator['family']
+          creator.delete('isInstitution')
+          creator.delete('family')
+          creator.delete('given')
+          resave = true
+        end
+      }
+    }
+  }
+
+  return unless resave
+
+  puts lib
+
+  File.open(lib, 'w'){|f| f.puts(JSON.pretty_generate(data)) }
+end
+
+Dir[File.join(root, 'test/fixtures/*/*.json')].each{|lib|
+  data = JSON.parse(File.read(lib))
+  if lib =~ /\.csl.json$/
+    fixCSL(lib, data)
+  else
+    fixBBTJSON(lib, data)
+  end
 }
