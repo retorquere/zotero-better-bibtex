47dd112d40949cf1e18f0fec1869ee6e030cfcf2
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2018-07-17 15:56:13 +0200

    fixes #1026

--- translators/lib/exporter.ts
+++ translators/lib/exporter.ts
@@ -68,7 +68,7 @@ export let Exporter = new class { // tslint:disable-line:variable-name
     debug('sorted:', { prefs: Translator.preferences, bbt: Translator.BetterBibTeX, bbl: Translator.BetterBibLaTeX })
     if (Translator.preferences.sorted && (Translator.BetterBibTeX || Translator.BetterBibLaTeX)) {
       Translator.references.sort((a, b) => a.citekey.localeCompare(b.citekey, 'en'))
-      Zotero.write(Translator.references.join(''))
+      Zotero.write(Translator.references.map(ref => ref.reference).join(''))
     }
 
     debug('Exporter.complete: write JabRef groups')
