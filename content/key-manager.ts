47e039b03f88b471709df9037ebdb5c331695b36
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2018-07-19 15:35:14 +0200

    request for non-existent key is an error

--- content/key-manager.ts
+++ content/key-manager.ts
@@ -315,7 +315,7 @@ export let KeyManager = new class { // tslint:disable-line:variable-name
     const key = this.keys.findOne({ itemID })
     if (key) return key
 
-    log.debug('KeyManager.get called for non-existent itemID', itemID, new Error('non-existing item'))
+    log.error('KeyManager.get called for non-existent itemID', itemID, new Error('non-existing item'))
     return { citekey: '', pinned: false }
   }
 
