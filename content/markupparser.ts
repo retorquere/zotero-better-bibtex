67478537cfc291470d5bd7b4e8f2dae829c34280
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2018-07-22 01:57:23 +0200

    replace pre with dummy text

--- content/markupparser.ts
+++ content/markupparser.ts
@@ -210,6 +210,7 @@ export let HTMLParser = new class { // tslint:disable-line:variable-name
         // the CSL title caser would consider last words in a title that actually have a following <pre> block the last
         // word and would capitalize it. The prevents that behavior by adding the contents of the <pre> block, but it
         // will be ignored by the BBT title caser, which only title-cases #text blocks
+        this.titleCased += ''.padStart(node.value.length, 'latex')
         break
 
       default:
