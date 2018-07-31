3698da54e859d0fe1f6dd1299e21439fec22d98c
Emiliano Heyns <emiliano.heyns@han.nl>
2018-07-31 21:55:41 +0200

    build on MacOS

--- setup/kuromoji.ts
+++ setup/kuromoji.ts
@@ -2,18 +2,20 @@
 
 import * as shell from 'shelljs'
 import * as fs from 'fs-extra'
+import * as path from 'path'
 
 function exec(cmd) {
   if (shell.exec(cmd).code !== 0) throw new Error(`failed: ${cmd}`)
 }
 
-const dicts = 'node_modules/kuromoji/dict'
-const unzipped = 'build/resource/kuromoji'
+const root = path.normalize(path.dirname(path.dirname(__filename)))
+const dicts = `${root}/node_modules/kuromoji/dict`
+const unzipped = `${root}/build/resource/kuromoji`
 
 exec(`mkdir -p ${unzipped}`)
 console.log('copying kuromoji dicts...')
 for (const dict of fs.readdirSync(dicts)) {
   console.log('  ', dict)
   const dat = dict.replace('.gz', '')
-  exec(`zcat ${dicts}/${dict} > ${unzipped}/${dat}`)
+  exec(`gunzip -d ${dicts}/${dict} > ${unzipped}/${dat}`)
 }
