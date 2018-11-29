05f45296d5caa9c28d28ac7394c608646f671cea
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2018-11-29 11:01:44 +0100

    translator header order matters

--- setup/plugins/translator-header.ts
+++ setup/plugins/translator-header.ts
@@ -7,6 +7,8 @@ import * as crypto from 'crypto'
 import version from 'zotero-plugin/version'
 import root from 'zotero-plugin/root'
 
+import stringify = require('json-stable-stringify')
+
 const prefs = {
   overrides: require(path.join(root, 'gen/preferences/auto-export-overrides.json')),
   defaults: require(path.join(root, 'gen/preferences/defaults.json')),
@@ -34,14 +36,44 @@ class TranslatorHeaderPlugin {
         {overrides, header, version}
       )
 
+      delete header.description
       header.configOptions = header.configOptions || {}
       header.configOptions.hash = crypto.createHash('md5').update(translator).digest('hex')
 
+      // because Zotero doesn't allow headers that have a object at the last key, so put lastUpdated at the end as a safeguard
+      const header_order = [
+        'translatorID',
+        'translatorType',
+        'label',
+        'creator',
+        'target',
+        'minVersion',
+        'maxVersion',
+        'priority',
+        'inRepository',
+        'configOptions',
+        'displayOptions',
+        'exportCharset',
+        'exportNotes',
+        'exportFileData',
+        'useJournalAbbreviation',
+      ]
+      const json_header = stringify(header, {
+        space: 2,
+        cmp: (a, b) => {
+          // lastUpdated always at the end
+          if (a.key === 'lastUpdated') return 1
+          if (b.key === 'lastUpdated') return -1
+
+          a.pos = (header_order.indexOf(a.key) + 1) || header_order.length + 1
+          b.pos = (header_order.indexOf(b.key) + 1) || header_order.length + 1
+          if (a.pos !== b.pos) return a.pos - b.pos
+          return a.key.localeCompare(b.key) // can only happen if they're both not in the order
+        },
+      }) + '\n\n'
+
       const asset = this.translator + '.js'
-      compilation.assets[asset] = new ConcatSource(
-        [JSON.stringify(header, null, 2), translator].join('\n\n'),
-        compilation.assets[asset]
-      )
+      compilation.assets[asset] = new ConcatSource(json_header + translator, compilation.assets[asset])
     })
   }
 }
