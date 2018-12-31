dc0b51c31e6445e892856f424fccf4084a58770d
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2018-12-31 19:56:57 +0100

    regex.exec keeps state

--- content/arXiv.ts
+++ content/arXiv.ts
@@ -1,27 +1,27 @@
 // export singleton: https://k94n.com/es6-modules-single-instance-pattern
+
 export let arXiv = new class {
   // new-style IDs
   // arXiv:0707.3168 [hep-th]
   // arXiv:YYMM.NNNNv# [category]
-  private post2007Id = /(?:^|\s|\/)((?:arXiv:\s*)?\d{4}\.\d{4,5}(?:v\d+)?)(?:\s\[(.*?)\])?(?=$|\s)/gi
+  private post2007 = /(?:^|\s|\/)(?:arXiv:\s*)?(\d{4}\.\d{4,5}(?:v\d+)?)(?:\s\[(.*?)\])?(?=$|\s)/i
 
   // arXiv:arch-ive/YYMMNNNv# or arXiv:arch-ive/YYMMNNNv# [category]
-  private pre2007Id = /(?:^|\s|\/)((?:arXiv:\s*)?[a-z-]+(?:\.[A-Z]{2})?\/\d{2}(?:0[1-9]|1[012])\d{3}(?:v\d+)?(?=$|\s))/gi
-
-  private prefix = /^arxiv:/i
+  private pre2007 = /(?:^|\s|\/)(?:arXiv:\s*)?([a-z-]+(?:\.[A-Z]{2})?\/\d{2}(?:0[1-9]|1[012])\d{3}(?:v\d+)?(?=$|\s))/i
 
   public parse(id): { id: string, category?: string } {
     if (!id) return { id: null }
 
     let match
 
-    while (match = this.post2007Id.exec(id)) {
-      return { id: match[1].replace(this.prefix, ''), category: match[2] && match[2].trim() }
-    }
-    while (match = this.pre2007Id.exec(id)) {
-      return { id: match[1].replace(this.prefix, '') }
+    if (match = this.post2007.exec(id)) {
+      return { id: match[1], category: match[2] && match[2].trim() }
     }
 
-    if (!id) return { id: null }
+    if (match = this.pre2007.exec(id)) {
+      return { id: match[1] }
+    }
+
+    return { id: null }
   }
 }
