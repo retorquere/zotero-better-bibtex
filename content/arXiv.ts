1e45abff9ad887227360904327aae7621b3878d2
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2018-12-31 13:58:10 +0100

    strip arxiv: prefix

--- content/arXiv.ts
+++ content/arXiv.ts
@@ -8,16 +8,18 @@ export let arXiv = new class {
   // arXiv:arch-ive/YYMMNNNv# or arXiv:arch-ive/YYMMNNNv# [category]
   private pre2007Id = /(?:^|\s|\/)((?:arXiv:)?[a-z-]+(?:\.[A-Z]{2})?\/\d{2}(?:0[1-9]|1[012])\d{3}(?:v\d+)?(?=$|\s))/gi
 
+  private prefix = /^arxiv:/i
+
   public parse(id): { id: string, category?: string } {
     if (!id) return { id: null }
 
     let match
 
     while (match = this.post2007Id.exec(id)) {
-      return { id: match[1], category: match[2] && match[2].trim() }
+      return { id: match[1].replace(this.prefix, ''), category: match[2] && match[2].trim() }
     }
     while (match = this.pre2007Id.exec(id)) {
-      return { id: match[1] }
+      return { id: match[1].replace(this.prefix, '') }
     }
 
     if (!id) return { id: null }
