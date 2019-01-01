4aef78133b11f7c6ba9f0c42925689b36baf6f75
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2019-01-01 21:59:37 +0100

    iffy workaround for #1107

--- content/title-case.ts
+++ content/title-case.ts
@@ -2,6 +2,7 @@ declare const Zotero: any
 declare const Components: any
 
 import * as log from './debug'
+import { patch as $patch$ } from './monkey-patch'
 
 // import Citeproc = require('./citeproc')
 const citeproc = { CSL: null }
@@ -10,6 +11,24 @@ const citeproc = { CSL: null }
 Components.utils.import('resource://gre/modules/Services.jsm')
 declare const Services: any
 if (!citeproc.CSL) Services.scriptloader.loadSubScript('chrome://zotero-better-bibtex/content/citeproc.js', citeproc, 'UTF-8')
+
+$patch$(citeproc.CSL.Doppeler, 'split', original => function split(str) {
+  const res = original.apply(this, arguments)
+  let fixed = false
+
+  res.strings = res.strings.map(s => {
+    if (typeof s === 'number') {
+      if (!fixed) log.error('Zotero.Citeproc.CSL.Doppeler.split: expected string array, found number', { str, split: res })
+      fixed = true
+      return ''
+    }
+    return s
+  })
+
+  if (fixed && res.origStrings) res.origStrings = res.origStrings.map(s => typeof s === 'number' ? '' : s)
+
+  return res
+})
 
 const state = {
   opt: { lang: 'en' },
