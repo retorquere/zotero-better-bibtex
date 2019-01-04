b9f299bfe11f85bf2cd6259467da2fb4ccc51373
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2019-01-04 14:00:13 +0100

    spin off i386 fix to propachi

--- content/title-case.ts
+++ content/title-case.ts
@@ -1,16 +1,6 @@
-// declare const Zotero: any
-// declare const Components: any
+declare const Zotero: any
 
 import * as log from './debug'
-import * as CSL from 'citeproc'
-
-// const CiteProc = { CSL: null } // tslint:disable-line:variable-name
-// const CiteProc = { CSL: Zotero.CiteProc.CSL } // tslint:disable-line:variable-name
-const CiteProc = { CSL } // tslint:disable-line:variable-name
-
-// Components.utils.import('resource://gre/modules/Services.jsm')
-// declare const Services: any
-// if (!CiteProc.CSL) Services.scriptloader.loadSubScript('chrome://zotero-better-bibtex/content/citeproc.js', CiteProc, 'UTF-8')
 
 const state = {
   opt: { lang: 'en' },
@@ -18,15 +8,15 @@ const state = {
   locale: {
     en: {
       opts: {
-        'skip-words': CiteProc.CSL.SKIP_WORDS,
-        'skip-words-regexp': new RegExp( '(?:(?:[?!:]*\\s+|-|^)(?:' + CiteProc.CSL.SKIP_WORDS.join('|') + ')(?=[!?:]*\\s+|-|$))', 'g'), // tslint:disable-line:prefer-template
+        'skip-words': Zotero.CiteProc.CSL.SKIP_WORDS,
+        'skip-words-regexp': new RegExp( '(?:(?:[?!:]*\\s+|-|^)(?:' + Zotero.CiteProc.CSL.SKIP_WORDS.join('|') + ')(?=[!?:]*\\s+|-|$))', 'g'), // tslint:disable-line:prefer-template
       },
     },
   },
 }
 
 export function titleCase(text) {
-  const titleCased = CiteProc.CSL.Output.Formatters.title(state, text)
+  const titleCased = Zotero.CiteProc.CSL.Output.Formatters.title(state, text)
   log.debug('titlecase:', {text, titleCased})
   return titleCased
 }
