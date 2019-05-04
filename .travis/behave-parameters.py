b88c90ee9f4f067a0c367761eb2ca9c707fa6bf4
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2019-05-04 23:44:58 +0200

    cleanup

--- .travis/behave-parameters.py
+++ .travis/behave-parameters.py
@@ -6,9 +6,10 @@ import shlex
 nightly = os.environ.get('TRAVIS_EVENT_TYPE', '') == 'cron' or os.environ.get('TRAVIS_TAG', '') != '' or '#nightly' in os.environ.get('TRAVIS_COMMIT_MESSAGE', '')
 
 print(f'--define zotero={shlex.quote(os.environ["ZOTERO"])}', end=' ')
+
 if nightly:
   tags = os.environ['TAGS']
   print(f'--define timeout=300', end=' ')
 else:
-  tags = os.environ['TAGS'] + ' and not @nightly'
-print(f'--tags={shlex.quote(tags)}')
+  print('--tags "~@nightly"', end=' ')
+print(f'--tags={shlex.quote(os.environ["TAGS"])}')
