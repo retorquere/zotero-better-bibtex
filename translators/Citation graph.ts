7c794055f2d4b469b54d3cfcfdea038e742af84e
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2019-12-02 09:25:08 +0100

    citation graph

--- translators/Citation graph.ts
+++ translators/Citation graph.ts
@@ -9,25 +9,40 @@ function node(id, label, style = null) {
   if (style) Zotero.write(`    graphics [ outlineStyle "${style}" ]\n`)
   Zotero.write('  ]\n')
 }
-function edge(source, target) {
+function edge(source, target, bidi = false) {
   Zotero.write('  edge [\n')
   Zotero.write(`    source ${source}\n`)
   Zotero.write(`    target ${target}\n`)
+  if (bidi) {
+    Zotero.write('graphics [\n')
+    Zotero.write('  fill  "#000000"\n')
+    Zotero.write('  sourceArrow "standard"\n')
+    Zotero.write('  targetArrow "standard"\n')
+    Zotero.write(']\n')
+  }
   Zotero.write('  ]\n')
+}
+
+type Item = {
+  id: number
+  uri: string
+  cites: string[]
+  relations: string[]
 }
 
 Translator.doExport = () => {
   Zotero.write('Creator "Zotero Better BibTeX"\n')
   Zotero.write('Version "2.15"\n')
   Zotero.write('graph [\n')
-  Zotero.write('  hierarchic 1\n')
+  Zotero.write('  hierarchic 0\n')
   Zotero.write('  label ""\n')
   Zotero.write('  directed 1\n')
 
-  const items: { [key: string]: { id: number, cites: string[] } } = {}
+  const items: Record<string, Item> = {}
 
   let _item
   let id = -1
+
   while ((_item = Zotero.nextItem())) {
     if (['note', 'attachment'].includes(_item.itemType)) continue
 
@@ -56,8 +71,10 @@ Translator.doExport = () => {
     */
 
     node(id, _item.citekey)
-    items[_item.citekey] = {
+    items[_item.citekey] = items[_item.uri] = {
       id,
+      uri: _item.uri,
+      relations: _item.relations || [],
       cites: [].concat.apply([],
         (_item.extra || '')
           .split('\n')
@@ -69,20 +86,46 @@ Translator.doExport = () => {
     }
   }
 
+  const bidi: string[] = []
   for (const item of Object.values(items)) {
     for (const cited of item.cites) {
       if (!items[cited]) {
         id += 1
         items[cited] = {
           id,
+          uri: `http://${cited}`,
           cites: [],
+          relations: [],
         }
 
         node(id, cited, 'dashed')
       }
 
       edge(item.id, items[cited].id)
+
     }
+
+    for (const other of item.relations) {
+      if (!items[other]) {
+        id += 1
+        items[other] = {
+          id,
+          uri: other,
+          cites: [],
+          relations: [],
+        }
+
+        node(id, '??', 'dashed')
+      }
+
+      const rel = [item.uri, other].sort().join('\t')
+      if (!bidi.includes(rel)) bidi.push(rel)
+    }
+  }
+
+  for (const rel of bidi) {
+    const [from, to] = rel.split('\t')
+    edge(items[from].id, items[to].id, true)
   }
 
   Zotero.write(']\n')
