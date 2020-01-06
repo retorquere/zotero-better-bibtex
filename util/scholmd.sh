0b78cda2df61ab0cb4696ea11e7c045ba89777cb
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2020-01-06 12:20:06 +0100

    util

--- util/scholmd.sh
+++ util/scholmd.sh
@@ -2,4 +2,5 @@
 
 #curl http://localhost:23119/better-bibtex/json-rpc -X POST -H "Content-Type: application/json" -H "Accept: application/json" --data-binary '{"jsonrpc": "2.0", "method": "user.groups" }'
 curl http://localhost:23119/better-bibtex/json-rpc -X POST -H "Content-Type: application/json" -H "Accept: application/json" --data-binary '{"jsonrpc": "2.0", "method": "item.attachments", "params": ["bentley_academic_2011"] }'
+curl http://localhost:23119/better-bibtex/json-rpc -X POST -H "Content-Type: application/json" -H "Accept: application/json" --data-binary '{"jsonrpc": "2.0", "method": "item.attachments", "params": ["@bentley_academic_2011"] }'
 
