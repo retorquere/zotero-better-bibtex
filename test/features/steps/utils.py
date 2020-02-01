877d8b339c80d6ce1d85f9fe12396dc694c8605f
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2020-02-01 15:43:18 +0100

    what a mess rebase is

--- test/features/steps/utils.py
+++ test/features/steps/utils.py
@@ -64,11 +64,8 @@ def html2md(html):
   html = BeautifulSoup(html, features='lxml').prettify()
   return md(html).strip()
 
-def serialize(obj, sort_keys):
-  if sort_keys:
-    return json.dumps(obj, indent=2, sort_keys=True)
-  else:
-    return json.dumps(obj, indent=2)
+def serialize(obj):
+  return json.dumps(obj, indent=2, sort_keys=True)
 
 def running(id):
   if type(id) == int:
