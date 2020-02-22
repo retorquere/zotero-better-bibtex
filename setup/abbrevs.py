972560ed58d3e9da017bc350810fc9e65d9cb986
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2020-02-22 01:27:24 +0100

    abbrevs to json

---
+++ setup/abbrevs.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import markdown
+import os
+import lxml.html
+import re
+import csv
+import json
+
+print('generating (un)abbreviation lists')
+root = os.path.join(os.path.dirname(__file__), '..')
+journals = 'abbrv.jabref.org/journals'
+
+abbrev_lists = {}
+with open(os.path.join(root, journals, 'README.md')) as f:
+  md = f.read()
+  html = markdown.markdown(md)
+  doc = lxml.html.fromstring(html)
+for link in doc.xpath('//a'):
+  href = link.get('href')
+  if re.match(r'[-_a-z]+\.csv$', href):
+    abbrev_lists[href.replace('.csv', '.json')] = link.text
+with open(os.path.join(root, 'build/resource/abbrev.json'), 'w') as f:
+  json.dump(abbrev_lists, f, indent='  ')
+
+for a in os.listdir(os.path.join(root, journals)):
+  if not a.endswith('.csv'): continue
+  print(' ', a)
+  abbrev = {}
+  unabbrev = {}
+  with open(os.path.join(root, journals, a)) as f:
+    reader = csv.reader(f, delimiter=';')
+    for row in reader:
+      unabbrev[row[1]] = row[0]
+      abbrev[row[0]] = row[1]
+  with open(os.path.join(root, 'build/resource/abbrev', a.replace('.csv', '.json')), 'w') as f:
+    json.dump(abbrev, f, indent='  ')
+  with open(os.path.join(root, 'build/resource/unabbrev', a.replace('.csv', '.json')), 'w') as f:
+    json.dump(unabbrev, f, indent='  ')
