a97aab81597bcf1033eeddf9b4afa2ca01484d11
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2020-02-24 23:41:30 +0100

    holy crap these abbreviation lists are a mess

--- setup/abbrevs.py
+++ setup/abbrevs.py
@@ -9,6 +9,8 @@ import json
 import sqlite3
 from munch import Munch
 import argparse
+import inflect
+from pygtrie import CharTrie as Trie
 
 root = os.path.join(os.path.dirname(__file__), '..')
 
@@ -17,12 +19,12 @@ parser.add_argument('-c', '--case-sensitive', action='store_true')
 parser.add_argument('-r', '--rebuild', action='store_true')
 parser.add_argument('-v', '--verbose', action='store_true')
 args = parser.parse_args()
-if args.rebuild: args.verbose = True
 
 if args.case_sensitive:
   dbname = 'abbrev-cs.sqlite'
 else:
   dbname = 'abbrev-ci.sqlite'
+dbname = ':memory:'
 if os.path.exists(dbname) and (args.rebuild or os.stat(dbname).st_mtime < os.stat(__file__).st_mtime): os.remove(dbname)
 
 rebuild = not os.path.exists(dbname)
@@ -34,24 +36,46 @@ def munch_factory(cursor, row):
   return m
 db.row_factory = munch_factory
 
+number = inflect.engine()
+def _mismatch(abbr, full):
+  abbr = '.*?' + '.*?'.join(re.escape(c) for c in list(re.sub('[-,. ()]', '', abbr)))
+  if args.case_sensitive:
+    if re.match(abbr, full) is None: return 'mismatch'
+  else:
+    if re.match(abbr, full, re.IGNORECASE) is None: return 'mismatch'
+  return None
+def mismatch(abbr, full):
+  m = _mismatch(abbr, full)
+  if m is None: return m
+
+  m = _mismatch(re.sub('[0-9]+', lambda n: number.number_to_words(int(n.group())).replace('-', ''), abbr), full)
+  if m is None: return m
+
+  return _mismatch(re.sub('[0-9]+', lambda n: number.ordinal(int(n.group())).replace('-', ''), abbr), full)
+
+def unjunk(abbr, full): # holy crap what a mess these lists are
+  if abbr[-1] == '.': abbr = abbr[:-1]
+  if abbr.endswith(' j'):
+    prefix = abbr + 'ournal-'
+    if full.lower().startswith(prefix):
+      full = full[len(prefix):]
+      full = full[0].upper() + full[1:]
+
+    prefix = abbr + 'etin-'
+    if full.lower().startswith(prefix):
+      full = full[len(prefix):]
+      full = full[0].upper() + full[1:]
+  return full
+
 if rebuild:
-  db.execute('CREATE TABLE abbrev (abbr, full, keep, list)')
-  if args.verbose: print('generating (un)abbreviation lists')
+  db.execute('CREATE TABLE abbrev (abbr, full, discard, list)')
+  db.execute('CREATE TABLE clean (abbr, full)')
+
+  print('generating (un)abbreviation lists')
   journals = 'abbrv.jabref.org/journals'
 
-  def name(_csv):
-    return _csv.replace('journal_abbreviations_', '').replace('.csv', '.json')
-
-  def match(abbr, full):
-    if abbr.lower() == full.lower(): return 0
-    if not '.' in abbr and full.lower().startswith(abbr.lower()): return 0
-
-    abbr = '.*'.join(re.escape(c) for c in list(re.sub('[-,. ()]', '', abbr)))
-    if args.case_sensitive:
-      if re.match(abbr, full) is None: return 0
-    else:
-      if re.match(abbr, full, re.IGNORECASE) is None: return 0
-    return 1
+  def name(_csv, ext = '.json'):
+    return _csv.replace('journal_abbreviations_', '').replace('.csv', ext)
 
   abbrev_lists = {}
   with open(os.path.join(root, journals, 'README.md')) as f:
@@ -65,48 +89,98 @@ if rebuild:
   with open(os.path.join(root, 'build/resource/abbrev.json'), 'w') as f:
     json.dump(abbrev_lists, f, indent='  ')
 
-  for a in os.listdir(os.path.join(root, journals)):
-    if not a.endswith('.csv'): continue
-    if args.verbose: print(' ', a)
-    abbrev = {}
-    with open(os.path.join(root, journals, a)) as f:
+  for abbrev_csv in os.listdir(os.path.join(root, journals)):
+    if not abbrev_csv.endswith('.csv'): continue
+
+    print('  *', abbrev_csv)
+    with open(os.path.join(root, journals, abbrev_csv)) as f:
       reader = csv.reader(f, delimiter=';')
       for row in reader:
-        full, abbr = row[0:2]
-        abbr = abbr.replace(' & ', ' and ')
-        full = full.replace(' & ', ' and ')
-        keep = match(abbr, full)
+        full, abbr = [r.strip() for r in row[0:2]]
+        if full == '' or abbr == '': continue
+
+        if ' and ' in full and ' & ' in abbr: abbr = abbr.replace(' & ', ' and ')
+        if ' and ' in full and '&' in abbr: abbr = abbr.replace('&', ' and ')
+        if ' und ' in full and ' & ' in abbr: abbr = abbr.replace(' & ', ' und ')
+        if ' und ' in full and '&' in abbr: abbr = abbr.replace('&', ' und ')
+
+        abbr = re.sub('[{}$]', '', abbr)
+        full = re.sub('[{}$]', '', full)
         if not args.case_sensitive: abbr = abbr.lower()
-        db.execute('INSERT INTO abbrev (abbr, full, keep, list) VALUES (?, ?, ?, ?)', (abbr, full, keep, name(a).replace('.json', '')))
+        db.execute('INSERT INTO abbrev (abbr, full, discard, list) VALUES (?, ?, ?, ?)', (abbr, unjunk(abbr, full), mismatch(abbr, full), name(abbrev_csv, '')))
+
+  print('  removing abbr == full')
+  db.execute("UPDATE abbrev SET discard = 'same' WHERE LOWER(abbr) IN (SELECT LOWER(full) FROM abbrev)")
+
+  print('  removing & alternates')
+  db.execute('''
+    WITH amp_and AS (SELECT REPLACE(full, ' and ', ' & ') as full FROM abbrev WHERE full LIKE '% and %')
+    UPDATE abbrev SET discard = 'alternate'
+    WHERE discard IS NULL AND full LIKE '% & %' AND full IN (SELECT full FROM amp_and)
+  ''')
+
+  print('  removing prefix alternates')
+  trie = Trie()
+  prefixed = set([])
+  duplicate = []
+  for row in db.execute('SELECT rowid, abbr, full FROM abbrev WHERE discard IS NULL GROUP BY abbr, full ORDER BY abbr, full DESC'):
+    name = f'{row.abbr}\t{row.full.lower()}'
+    if trie.get(name): # strict duplicates
+      duplicate.append(str(row.rowid))
+      continue
+
+    try: # "aana j.": "AANA Journal" with better alternatives
+      longer = False
+      for prefix, rowid in trie.items(prefix=f'{row.abbr}\t'): # abbr already in there => longer because of full DESC
+        longer = True
+        break
+      if longer:
+        abbr = row.abbr
+        if abbr.endswith('.'): abbr = abbr[:-1]
+        if abbr.endswith(' j') and row.full.lower() == abbr + 'ournal':
+          prefixed.add(str(row.rowid))
+          continue
+        if abbr.endswith(' bull') and row.full.lower() == abbr + 'etin':
+          prefixed.add(str(row.rowid))
+          continue
+    except:
+      pass
+
+    try:
+      for prefix, rowid in trie.items(prefix=name):
+        prefixed.add(str(rowid))
+    except KeyError: # prefix doesn't exist
+      pass 
+    trie[name] = row.rowid
+  if len(duplicate) > 0: db.execute("DELETE FROM abbrev WHERE rowid IN (" + ', '.join(list(duplicate)) + ')')
+  if len(prefixed) > 0: db.execute("UPDATE abbrev SET discard='prefix' WHERE rowid IN (" + ', '.join(list(prefixed)) + ')')
+    
+  print('  removing duplicates')
+  db.execute('''
+    WITH
+    distinct_abbr_full AS (SELECT DISTINCT abbr, full FROM abbrev WHERE discard IS NULL),
+    abbr_count AS (SELECT abbr, COUNT(*) AS n FROM distinct_abbr_full GROUP BY abbr)
+
+    UPDATE abbrev SET discard = 'duplicate' WHERE abbr IN (SELECT abbr FROM abbr_count WHERE n > 1)
+  ''')
+
   db.commit()
-
-if args.verbose:
-  if args.case_sensitive:
-    print('## Case sensitive\n')
-  else:
-    print('## Case insensitive\n')
-
-  for row in db.execute('SELECT COUNT(*) as n FROM abbrev WHERE keep = 0'):
-    print(f'{row.n} abbrevs do not seem to match the full name\n')
-
-  print('Combined lists:\n')
-  for row in db.execute('SELECT COUNT(*) as n FROM (SELECT abbr, COUNT(*) FROM abbrev WHERE keep = 1 GROUP BY abbr HAVING COUNT(*) > 1)'):
-    print(f'* {row.n} have more than one match')
-  for row in db.execute('SELECT COUNT(*) as n FROM (SELECT abbr, COUNT(*) FROM abbrev WHERE keep = 1 GROUP BY abbr HAVING COUNT(*) = 1)'):
-    print(f'* {row.n} unabbreviations available')
-
-  print('\nSeperate lists:\n')
-  for row in db.execute('SELECT SUM(n) as n FROM (SELECT list, COUNT(*) as n FROM (SELECT abbr, list, COUNT(*) FROM abbrev WHERE keep = 1 GROUP BY abbr, list HAVING COUNT(*) > 1) GROUP BY list)'):
-    print(f'* {row.n} have more than one match')
-  for row in db.execute('SELECT SUM(n) as n FROM (SELECT list, COUNT(*) as n FROM (SELECT abbr, list, COUNT(*) FROM abbrev WHERE keep = 1 GROUP BY abbr, list HAVING COUNT(*) = 1) GROUP BY list)'):
-    print(f'* {row.n} unabbreviations available')
 
 with open(os.path.join(root, 'build/resource/unabbrev.json'), 'w') as f:
   unabbrev = {}
-  for row in db.execute('SELECT abbr, full FROM abbrev WHERE abbr IN (SELECT abbr FROM (SELECT DISTINCT abbr, full FROM abbrev WHERE keep = 1) GROUP BY abbr HAVING COUNT(*) = 1)'):
+  for row in db.execute('SELECT DISTINCT abbr, full FROM abbrev WHERE discard IS NULL'):
     unabbrev[row.abbr] = row.full
   json.dump(unabbrev, f, indent='  ')
 
-with open('discarded.csv', 'w') as f:
-  for row in db.execute('SELECT DISTINCT abbr, full FROM abbrev WHERE keep = 0 ORDER BY abbr, full'):
-    print(f'{row.abbr};{row.full}', file=f)
+for row in db.execute('SELECT COUNT(*) AS n FROM abbrev WHERE discard IS NULL'):
+  print('  abbreviations:', row.n)
+
+if args.verbose:
+  for row in db.execute('SELECT discard, COUNT(*) AS n FROM abbrev WHERE discard IS NOT NULL GROUP BY discard'):
+    print(f'  {row.discard}: {row.n}')
+
+  with open('discarded.csv', 'w') as f:
+    writer = csv.writer(f, delimiter=';')
+    writer.writerow(['abbr', 'full', 'reason'])
+    for row in db.execute('SELECT DISTINCT abbr, full, MAX(discard) AS reason FROM abbrev WHERE discard IS NOT NULL GROUP BY abbr, full ORDER BY abbr, full'):
+      writer.writerow([row.abbr, row.full, row.reason])
