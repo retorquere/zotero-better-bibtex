cb9120a14a28f225cd643096b665ae6d15bbc7fc
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2020-02-24 13:20:34 +0100

    abbrev analysis

--- setup/abbrevs.py
+++ setup/abbrevs.py
@@ -8,10 +8,23 @@ import csv
 import json
 import sqlite3
 from munch import Munch
+import argparse
 
-db = sqlite3.connect(':memory:')
-db.execute('CREATE TABLE abbrev (abbr, full, list)')
+root = os.path.join(os.path.dirname(__file__), '..')
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--case-sensitive', action='store_true')
+parser.add_argument('-r', '--rebuild', action='store_true')
+args = parser.parse_args()
+
+if args.case_sensitive:
+  dbname = 'abbrev-cs.sqlite'
+else:
+  dbname = 'abbrev-ci.sqlite'
+if os.path.exists(dbname) and args.rebuild: os.remove(dbname)
+
+rebuild = not os.path.exists(dbname)
+db = sqlite3.connect(dbname)
 def munch_factory(cursor, row):
   m = Munch()
   for idx, col in enumerate(cursor.description):
@@ -19,45 +32,63 @@ def munch_factory(cursor, row):
   return m
 db.row_factory = munch_factory
 
-print('generating (un)abbreviation lists')
-root = os.path.join(os.path.dirname(__file__), '..')
-journals = 'abbrv.jabref.org/journals'
+if rebuild:
+  db.execute('CREATE TABLE abbrev (abbr, full, keep, list)')
+  print('generating (un)abbreviation lists')
+  journals = 'abbrv.jabref.org/journals'
 
-def name(_csv):
-  return _csv.replace('journal_abbreviations_', '').replace('.csv', '.json')
+  def name(_csv):
+    return _csv.replace('journal_abbreviations_', '').replace('.csv', '.json')
 
-def match(abbr, full):
-  #abbr = '^' + '.*'.join(re.escape(w) for w in re.split('[. ]+', abbr)) + '.*$'
-  abbr = '.*'.join(re.escape(c) for c in list(re.sub('[-,. ()]', '', abbr)))
-  if re.match(abbr, full, re.IGNORECASE) is None: return False
-  return True
+  def match(abbr, full):
+    abbr = '.*'.join(re.escape(c) for c in list(re.sub('[-,. ()]', '', abbr)))
+    if args.case_sensitive:
+      if re.match(abbr, full) is None: return 0
+    else:
+      if re.match(abbr, full, re.IGNORECASE) is None: return 0
+    return 1
 
-abbrev_lists = {}
-with open(os.path.join(root, journals, 'README.md')) as f:
-  md = f.read()
-  html = markdown.markdown(md)
-  doc = lxml.html.fromstring(html)
-for link in doc.xpath('//a'):
-  href = link.get('href')
-  if re.match(r'[-_a-z]+\.csv$', href):
-    abbrev_lists[name(href)] = link.text
-with open(os.path.join(root, 'build/resource/abbrev.json'), 'w') as f:
-  json.dump(abbrev_lists, f, indent='  ')
+  abbrev_lists = {}
+  with open(os.path.join(root, journals, 'README.md')) as f:
+    md = f.read()
+    html = markdown.markdown(md)
+    doc = lxml.html.fromstring(html)
+  for link in doc.xpath('//a'):
+    href = link.get('href')
+    if re.match(r'[-_a-z]+\.csv$', href):
+      abbrev_lists[name(href)] = link.text
+  with open(os.path.join(root, 'build/resource/abbrev.json'), 'w') as f:
+    json.dump(abbrev_lists, f, indent='  ')
 
-for a in os.listdir(os.path.join(root, journals)):
-  if not a.endswith('.csv'): continue
-  print(' ', a)
-  abbrev = {}
-  with open(os.path.join(root, journals, a)) as f:
-    reader = csv.reader(f, delimiter=';')
-    for row in reader:
-      full, abbr = row[0:2]
-      if not match(abbr, full):
-        #print(json.dumps(abbr), 'does not match', json.dumps(full))
-        continue
-      db.execute('INSERT INTO abbrev (abbr, full, list) VALUES (?, ?, ?)', (abbr, full, name(a)))
+  for a in os.listdir(os.path.join(root, journals)):
+    if not a.endswith('.csv'): continue
+    print(' ', a)
+    abbrev = {}
+    with open(os.path.join(root, journals, a)) as f:
+      reader = csv.reader(f, delimiter=';')
+      for row in reader:
+        full, abbr = row[0:2]
+        keep = match(abbr, full)
+        if not args.case_sensitive: abbr = abbr.lower()
+        db.execute('INSERT INTO abbrev (abbr, full, keep, list) VALUES (?, ?, ?, ?)', (abbr, full, keep, name(a).replace('.json', '')))
+  db.commit()
 
-for row in db.execute('SELECT COUNT(*) as n FROM (SELECT abbr, COUNT(*) FROM abbrev GROUP BY abbr HAVING COUNT(*) > 1)'):
-  print('Global dup:', row.n)
-for row in db.execute('SELECT SUM(n) as n FROM (SELECT list, COUNT(*) as n FROM (SELECT abbr, list, COUNT(*) FROM abbrev GROUP BY abbr, list HAVING COUNT(*) > 1) GROUP BY list)'):
-  print('per-list dup:', row.n)
+if args.case_sensitive:
+  print('## Case sensitive')
+else:
+  print('## Case insensitive')
+
+for row in db.execute('SELECT COUNT(*) as n FROM abbrev WHERE keep = 0'):
+  print(f'{row.n} abbrevs do not seem to match the full name')
+
+print('Combined lists:')
+for row in db.execute('SELECT COUNT(*) as n FROM (SELECT abbr, COUNT(*) FROM abbrev WHERE keep = 1 GROUP BY abbr HAVING COUNT(*) > 1)'):
+  print(f'* {row.n} have more than one match')
+for row in db.execute('SELECT COUNT(*) as n FROM (SELECT abbr, COUNT(*) FROM abbrev WHERE keep = 1 GROUP BY abbr HAVING COUNT(*) = 1)'):
+  print(f'* {row.n} unabbreviations available')
+
+print('Seperate lists:')
+for row in db.execute('SELECT SUM(n) as n FROM (SELECT list, COUNT(*) as n FROM (SELECT abbr, list, COUNT(*) FROM abbrev WHERE keep = 1 GROUP BY abbr, list HAVING COUNT(*) > 1) GROUP BY list)'):
+  print(f'* {row.n} have more than one match')
+for row in db.execute('SELECT SUM(n) as n FROM (SELECT list, COUNT(*) as n FROM (SELECT abbr, list, COUNT(*) FROM abbrev WHERE keep = 1 GROUP BY abbr, list HAVING COUNT(*) = 1) GROUP BY list)'):
+  print(f'* {row.n} unabbreviations available')
