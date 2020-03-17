c9df5f7aaa55db441c4d297cb7d67bfc970f7753
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2020-03-17 17:32:55 +0100

    knapsack solver only understands integer weights

--- util/balance.py
+++ util/balance.py
@@ -9,6 +9,11 @@ import re
 import math
 import sqlite3
 from ortools.algorithms import pywrapknapsack_solver
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', '--verbose', action='store_true')
+args = parser.parse_args()
 
 db = sqlite3.connect(':memory:')
 db.execute('CREATE TABLE tests(build, job, name, duration, state, PRIMARY KEY(build, name))')
@@ -48,19 +53,19 @@ for log in sorted(glob.glob(os.path.expanduser('~/pCloud Drive/travis/*.json')))
           status,
         ])
   if not has_tests:
-    print('-', log)
+    print('no tests:', log)
     os.remove(log)
     restart = True
 
 for build, n in db.execute('WITH builds AS (SELECT DISTINCT build, job FROM tests) SELECT build, count(*) FROM builds GROUP BY build HAVING COUNT(*) <> 2'):
   for log in glob.glob(os.path.expanduser(f'~/pCloud Drive/travis/*{build}*.json')):
-    print('-', log)
+    print('expected 2, got', n, log)
     os.remove(log)
     restart = True
 
 for build, state in db.execute("SELECT DISTINCT build, state FROM tests WHERE state = 'failed'"):
   for log in glob.glob(os.path.expanduser(f'~/pCloud Drive/travis/*{build}*.json')):
-    print('-', log)
+    print('failed', log)
     os.remove(log)
     restart = True
 
@@ -76,7 +81,7 @@ sql = '''
 '''
 for (build,) in db.execute(sql):
   for log in glob.glob(os.path.expanduser(f'~/pCloud Drive/travis/*{build}*.json')):
-    print('-', log)
+    print('slow-fast reshuffle', log)
     os.remove(log)
     restart = True
 
@@ -101,19 +106,26 @@ def balance(slow=False):
   sql = 'SELECT name, duration FROM durations'
   if not slow: sql += " WHERE state <> 'slow'"
 
+  factor = 100
   tests, durations = zip(*db.execute(sql))
+  durations = [int(d * factor) for d in durations]
+  if 0 in durations: raise ValueError(f'{factor} is too small')
 
   solver = pywrapknapsack_solver.KnapsackSolver(pywrapknapsack_solver.KnapsackSolver.KNAPSACK_MULTIDIMENSION_BRANCH_AND_BOUND_SOLVER, 'KnapsackExample')
-  solver.Init([1 for n in durations], [durations], [int(sum(durations)/2)])
-  #solver.Init(durations, [durations], [int(sum(durations)/2)])
-  computed_value = solver.Solve()
+  total = sum(durations)
+  solver.Init([1 for n in durations], [durations], [int(total/2)])
+  solver.Solve()
 
   clusters = {'1': [], '2': []}
+  clustertime = {'1': [], '2': []}
   for cluster in clusters.keys():
-    clusters[cluster] = sorted([test for i, test in enumerate(tests) if solver.BestSolutionContains(i) == (cluster == '1')])
-  print('slow' if slow else 'fast')
+    indexes = sorted([i for i in range(len(tests)) if solver.BestSolutionContains(i) == (cluster == '1')], key=lambda x: tests[x])
+    clusters[cluster] = [test for i, test in enumerate(tests) if i in indexes]
+    clustertime[cluster] = [dur / factor for i, dur in enumerate(durations) if i in indexes]
+
+  print('slow' if slow else 'fast', total)
   for cluster in clusters.keys():
-    print(' ', cluster, sum([dur for i, dur in enumerate(durations) if solver.BestSolutionContains(i) == (cluster == '1')]))
+    print(' ', cluster, sum(clustertime[cluster]), len(clustertime[cluster]), sum(clustertime[cluster]) / len(clustertime[cluster]))
   return clusters
 
 with open('balance.json', 'w') as f:
