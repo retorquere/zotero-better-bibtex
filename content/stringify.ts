e1b0aba38fd125df2a0aa8bd8fdd0d8faa55f308
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2020-04-07 10:29:58 +0200

    single stringifier

--- content/stringify.ts
+++ content/stringify.ts
@@ -1,5 +1,7 @@
 import safeStableStringify from 'safe-stable-stringify'
 
-export function stringify(obj, replacer?, indent?) {
-  return safeStableStringify(obj, replacer, indent).replace(/[\u007F-\uFFFF]/g, chr => '\\u' + ('0000' + chr.charCodeAt(0).toString(16)).substr(-4)) // tslint:disable-line:no-magic-numbers
+export function stringify(obj, replacer?, indent?, ucode?) {
+  const stringified = safeStableStringify(obj, replacer, indent)
+
+  return return ucode ? stringified.replace(/[\u007F-\uFFFF]/g, chr => '\\u' + ('0000' + chr.charCodeAt(0).toString(16)).substr(-4)) : stringified // tslint:disable-line:no-magic-numbers
 }
