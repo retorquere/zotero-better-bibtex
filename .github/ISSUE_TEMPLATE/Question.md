2afbc6548d98246d41f51ee060731b67b22cc388
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2020-05-15 10:31:41 +0200

    issue templates templates

--- .github/ISSUE_TEMPLATE/Question.md
+++ .github/ISSUE_TEMPLATE/Question.md
@@ -8,10 +8,11 @@ labels: question
 *Please verify **all** of the below before submitting*:
 
 1. you have picked the right category for the issue in the previous screen.
-2. in the Zotero addons screen you can see that you have the latest release of BBT (https://github.com/retorquere/zotero-better-bibtex/releases/latest) and Zotero installed
-3. you are posting a single question
-4. you have included a descriptive subject of the problem
-5. you are available for follow-up questions and testing
+2. You are on the latest release of Zotero
+3. in the Zotero addons screen you can see that you have the latest release of BBT (https://github.com/retorquere/zotero-better-bibtex/releases/latest)
+4 you are posting a single question.
+5. the issue has a subject that succinctly describes the problem or question.
+6. you are available for follow-up questions and testing.
 
 Picking the right issue category is really important. Each category (`Export`, `General error`, `Import`, `Key generation`, `Question`) has different instructions for gathering the data necessary required to resolve the issue you are experiencing
 
---
+++ .github/issue-templates.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+from mako.template import Template
+from munch import *
+
+templates = [
+  Munch(
+    name='Export',
+    title='Export bug/enhancement/question',
+    about='You appear to have found a bug in BBTs export, are proposing an enhancement to the exports, or have a question about exporting items.',
+    labels='bug',
+  ),
+  Munch(
+    name='General_error',
+    title='General error',
+    about='You are experiencing an error not covered by the other categories',
+    labels='bug'
+  ),
+  Munch(
+    name='Import',
+    title='Import bug/enhancement/question',
+    about='You appear to have found a bug in BBTs BibTeX import, are proposing an enhancement to the import, or have a question about importing BibTeX.',
+    labels='bug'
+  ),
+  Munch(
+    name='Key_generation',
+    title='Key generation bug/enhancement/question',
+    about='You appear to have found a bug in BBTs citation key generation, are proposing an enhancement to the key generation, or have a question about key generation.',
+    labels='bug'
+  ),
+  Munch(
+    name='Question',
+    title='Question on use, or feature suggestions, not covered by the other categories above.',
+    about='You want information on how to use BBT, or have an idea for improvement that is not one of the categories above.',
+    labels='question'
+  ),
+]
+
+md = Template(filename='issue-template.md.mako')
+for template in templates:
+  with open(f'ISSUE_TEMPLATE/{template.name}.md', 'w') as f:
+    print(md.render(templates=templates, template=template), file=f)
