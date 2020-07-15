9d1679d6cb987d0118ce80408b7fa6f2e69ed982
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2020-07-15 12:51:47 +0200

    citeproc update

--- content/title-case.ts
+++ content/title-case.ts
@@ -8,19 +8,21 @@ function makeRegExp(lst) {
 }
 // tslint:enable
 
-const state = new class {
+class State {
   public opt = { lang: 'en' }
   public locale: { [locale: string]: { opts: { 'skip-words'?: string[], 'skip-words-regexp'?: RegExp } } }
+  public tmp: any
 
   constructor() {
     this.locale = {}
     this.locale[this.opt.lang] = { opts: {} }
     this.locale[this.opt.lang].opts['skip-words'] = CSL.SKIP_WORDS
     this.locale[this.opt.lang].opts["skip-words-regexp"] = makeRegExp(this.locale[this.opt.lang].opts["skip-words"]) // tslint:disable-line:quotemark semicolon
+    this.tmp = {}
   }
 }
 
 export function titleCase(text) {
-  const titleCased = CSL.Output.Formatters.title(state, text)
+  const titleCased = CSL.Output.Formatters.title(new State, text)
   return titleCased
 }
