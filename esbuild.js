24fa291fcc7fa62306a5fb785f68950ebee8c94d
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2021-04-02 18:42:25 +0200

    prep for esbuild

--- esbuild.js
+++ esbuild.js
@@ -4,6 +4,8 @@ const { bibertool } = require('./setup/loaders/bibertool')
 const esbuild = require('esbuild')
 const pegjs = require('pegjs')
 const exec = require('child_process').exec
+const glob = require('glob-promise')
+const crypto = require('crypto')
 
 let resolveShims = {
   name: 'node-shims',
@@ -48,25 +50,21 @@ function execShellCommand(cmd) {
 }
 
 async function rebuild() {
-  for (const translator of process.argv.slice(2)) {
-    if (!translator.startsWith('translators/')) continue
-
-    tr = path.parse(translator)
-    console.log(tr.base)
-
-    const header = require('./' + path.join(tr.dir, tr.name + '.json'))
+  for (const translator of (await glob('translators/*.json')).map(tr => path.parse(tr))) {
+    console.log(translator.name)
+    const header = require('./' + path.join(translator.dir, translator.name + '.json'))
     const vars = ['Translator']
       .concat((header.translatorType & 1) ? ['detectImport', 'doImport'] : [])
       .concat((header.translatorType & 2) ? ['doExport'] : [])
 
-    const globalName = tr.name.replace(/ /g, '') + '__' + vars.join('__')
-    const outfile = path.join('build/dist', tr.name + '.js')
+    const globalName = translator.name.replace(/ /g, '') + '__' + vars.join('__')
+    const outfile = path.join('build/resource', translator.name + '.js')
 
     // https://esbuild.github.io/api/#write
     // https://esbuild.github.io/api/#outbase
     // https://esbuild.github.io/api/#working-directory
     await esbuild.build({
-      entryPoints: [translator],
+      entryPoints: [path.join(translator.dir, translator.name + '.ts')],
       format: 'iife',
       globalName,
       bundle: true,
@@ -79,8 +77,17 @@ async function rebuild() {
       // banner: { js: '// bloody insane speed', },
       target: ['firefox60'],
     })
+
+    const source = await fs.promises.readFile(outfile, 'utf-8')
+    const checksum = crypto.createHash('sha256')
+    checksum.update(source)
+    if (!header.configOptions) header.configOptions = {}
+    header.configOptions.hash = checksum.digest('hex')
+    header.lastUpdated = (new Date).toISOString().replace(/T.*/, '')
+    await fs.promises.writeFile(path.join('build/resource', translator.name + '.json'), JSON.stringify(header, null, 2))
   }
 
+  /*
   const entryPoints = process.argv.slice(2).filter(src => {
     if (!src.startsWith('content/')) return false
     src = path.parse(src)
@@ -96,11 +103,11 @@ async function rebuild() {
       splitting: true,
       outdir: 'build/dist/content',
       target: ['firefox60'],
+      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/globalThis
+      banner: "const Global = Function('return this')();\n\n",
     })
   }
-  for (const entryPoint of entryPoints ) {
-    await execShellCommand(`./node_modules/.bin/jscodeshift -t post.js 'build/dist/${entryPoint.replace('.ts', '.js')}'`)
-  }
+  */
 }
 
 rebuild().catch(err => console.log(err))
