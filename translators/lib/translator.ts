48d2c266f2b63526d031de96588c0b96e5b5a3dc
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2021-06-02 18:38:18 +0200

    find cache disabler [fast-test]

--- translators/lib/translator.ts
+++ translators/lib/translator.ts
@@ -20,7 +20,7 @@ const cacheDisabler = new class {
 
     // collections: jabref 4 stores collection info inside the reference, and collection info depends on which part of your library you're exporting
     if (property === 'collections') {
-      log.debug('cache-rate: not for', target)
+      log.debug('cache-rate: not for', target, (new Error).stack)
       target.$cacheable = false
     }
 
@@ -32,7 +32,7 @@ const cacheDisabler = new class {
   }
 
   set(target, property, value): boolean {
-    if (property === '$cacheable' && target.$cacheable && !value) log.debug('cache-rate: not for', target)
+    if (property === '$cacheable' && target.$cacheable && !value) log.debug('cache-rate: not for', target, (new Error).stack)
     target[property] = value
     return true
   }
