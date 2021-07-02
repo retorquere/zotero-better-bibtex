68f006764c0d2f6df3e49e5d07537a0c9088143f
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2021-07-02 09:36:34 +0200

    juris-m

--- test/features/steps/utils.py
+++ test/features/steps/utils.py
@@ -72,7 +72,7 @@ def serialize(obj):
   return json.dumps(obj, indent=2, ensure_ascii=True, sort_keys=True)
 
 def extra_lower(obj):
-  if type(obj) == dict and 'items' in obj:
+  if isinstance(obj, dict) and 'items' in obj:
     obj = copy.deepcopy(obj)
     for item in obj['items']:
       if 'extra' in item:
