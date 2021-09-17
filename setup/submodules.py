bf38b7ba81a3bd2120a78f36d825786d216ff408
Emiliano Heyns <emiliano.heyns@han.nl>
2021-09-18 01:48:39 +0200

    skip upgrades on CI

--- setup/submodules.py
+++ setup/submodules.py
@@ -3,22 +3,24 @@
 import subprocess
 import urllib.request
 import textwrap
+import os
 
-print('updating submodules')
-online = True
-try:
-  online = urllib.request.urlopen('https://github.com').getcode() == 200
-except:
-  online = False
+if os.environ.get('CI') != 'true':
+  print('updating submodules')
+  online = True
+  try:
+    online = urllib.request.urlopen('https://github.com').getcode() == 200
+  except:
+    online = False
 
-def run(cmd):
-  return textwrap.indent(subprocess.check_output(cmd.split(' ')).decode('utf-8'), '  ')
+  def run(cmd):
+    return textwrap.indent(subprocess.check_output(cmd.split(' ')).decode('utf-8'), '  ')
 
-if online:
-  submodules = run('git submodule update --init --recursive --remote')
-  if submodules.strip() == '': submodules = '  up to date'
-  print(submodules)
-  print(run('git submodule foreach git checkout'))
-  print(run('git submodule foreach git pull origin'))
-else:
-  print('  GitHub offline -- you may not have network access -- skipping submodule update')
+  if online:
+    submodules = run('git submodule update --init --recursive --remote')
+    if submodules.strip() == '': submodules = '  up to date'
+    print(submodules)
+    print(run('git submodule foreach git checkout'))
+    print(run('git submodule foreach git pull origin'))
+  else:
+    print('  GitHub offline -- you may not have network access -- skipping submodule update')
