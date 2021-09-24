6b8d54775d434926bbfab7d8b8f87344c8aac199
Emiliano Heyns <emiliano.heyns@han.nl>
2021-09-24 17:00:19 +0200

    log both name and message

--- content/logger.ts
+++ content/logger.ts
@@ -57,7 +57,7 @@ class Logger {
   }
 
   private formatError(e, indent='') {
-    let msg = `${e.message || e.name || ''}`
+    let msg = [e.name, e.message].filter(s => s).join(': ')
     if (e.filename || e.fileName) msg += ` in ${e.filename || e.fileName}`
     if (e.lineno || e.lineNumber) {
       msg += ` line ${e.lineno}`
