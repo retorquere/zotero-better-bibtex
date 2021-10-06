fae069ddbb0624faa89a0857c3aa22e34785d765
Emiliano Heyns <emiliano.heyns@han.nl>
2021-10-06 23:01:30 +0200

    upgrades

--- setup/submodules.py
+++ setup/submodules.py
@@ -4,6 +4,18 @@ import subprocess
 import urllib.request
 import textwrap
 import os
+import configparser
+from contextlib import contextmanager
+from pathlib import Path
+
+@contextmanager
+def chdir(path):
+  origin = Path().absolute()
+  try:
+    os.chdir(str(Path(path).absolute()))
+    yield
+  finally:
+    os.chdir(origin)
 
 if os.environ.get('CI') != 'true':
   print('updating submodules')
@@ -13,14 +25,24 @@ if os.environ.get('CI') != 'true':
   except:
     online = False
 
-  def run(cmd):
-    return textwrap.indent(subprocess.check_output(cmd.split(' ')).decode('utf-8'), '  ')
+  def run(path, cmd):
+    with chdir(path):
+      return textwrap.indent(subprocess.check_output(cmd.split(' ')).decode('utf-8'), '  ')
 
   if online:
-    submodules = run('git submodule update --init --recursive --remote')
+    root = str(Path(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')).absolute())
+
+    submodules = run(root, 'git submodule update --init --recursive --remote')
     if submodules.strip() == '': submodules = '  up to date'
     print(submodules)
-    print(run('git submodule foreach git checkout'))
-    print(run('git submodule foreach git pull'))
+
+    gitmodules = configparser.ConfigParser()
+    gitmodules.read(os.path.join(root, '.gitmodules'))
+    for section, module in gitmodules.items():
+      if 'submodule ' in section:
+        print(section)
+        print(' ', run(os.path.join(root, module['path']), 'git checkout ' + module['branch']))
+        print(' ', run(os.path.join(root, module['path']), 'git pull'))
+
   else:
     print('  GitHub offline -- you may not have network access -- skipping submodule update')
