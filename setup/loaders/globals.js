307e442f413f99d8bf79890bbb1d671b1d05e80a
Emiliano Heyns <emiliano.heyns@han.nl>
2021-11-16 09:32:29 +0100

    globals

--- setup/loaders/globals.js
+++ setup/loaders/globals.js
@@ -1,1 +1,3 @@
-export let process = require('process/browser.js')
+export let process = require('process/browser.js');
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/globalThis
+export var global = Function('return this')();
