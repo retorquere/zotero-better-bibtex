510fef82ba01fe9cd2f1ecdb2b6c2d286a3deae3
Emiliano Heyns <emiliano.heyns@han.nl>
2021-12-01 13:23:56 +0100

    stringifier not supported by FF

--- content/stringify.ts
+++ content/stringify.ts
@@ -1,5 +1,4 @@
-// import _stringify from 'fast-safe-stringify'
-import _stringify from 'safe-stable-stringify'
+import _stringify from 'fast-safe-stringify'
 
 export function asciify(str: string): string {
   return str.replace(/[\u007F-\uFFFF]/g, chr => `\\u${(`0000${chr.charCodeAt(0).toString(16)}`).substr(-4)}`) // eslint-disable-line no-magic-numbers
@@ -7,7 +6,7 @@ export function asciify(str: string): string {
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export function stringify(obj: any, replacer?: any, indent?: string | number, ucode?: boolean): string {
-  const stringified = _stringify(obj, replacer, indent)
+  const stringified: string = _stringify.stable(obj, replacer, indent)
 
   return ucode ? asciify(stringified) : stringified
 }
