0125af0899fa0f968e18ddc7653ac6f9cdc8ba2c
Emiliano Heyns <emiliano.heyns@han.nl>
2021-12-18 11:08:35 +0100

    explicitly set variable

--- content/better-bibtex.ts
+++ content/better-bibtex.ts
@@ -107,6 +107,7 @@ $patch$(Zotero.Utilities.Item?.itemToCSLJSON ? Zotero.Utilities.Item : Zotero.Ut
         if (! (cslItem.note?.match(/(^|\n)citation key:/i))) {
           log.debug('patching CSL-JSON:', citekey.citekey)
           cslItem.note = `${cslItem.note || ''}\nCitation Key: ${citekey.citekey}`.trim()
+          cslItem['citation-key'] = citekey.citekey
         }
       }
       else {
