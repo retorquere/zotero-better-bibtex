15f9b4151b1f2620585c6bc5648c239f23e49552
Emiliano Heyns <emiliano.heyns@han.nl>
2022-02-06 00:28:25 +0100

    same-value test only works for primitives

--- content/environment.ts
+++ content/environment.ts
@@ -1,11 +1,10 @@
 // copied from emscripten
-export const environment: { node: boolean, web: boolean, worker: boolean, zotero: boolean, shell: boolean, name: string } = {
-  node: typeof process === 'object' && typeof require === 'function',
-  web: typeof window === 'object',
+export const environment: { node: boolean, worker: boolean, zotero: boolean, name: string } = {
+  // require is a function in firefox after importScripts("resource://gre/modules/workers/require.js")
+  node: typeof process === 'object' && typeof require === 'function' && typeof importScripts !== 'function',
+  // web: typeof window === 'object',
   worker: typeof importScripts === 'function',
   zotero: typeof Components !== 'undefined',
-  shell: false,
   name: '',
 }
-environment.shell = !environment.web && !environment.node && !environment.worker
 environment.name = Object.entries(environment).map(([name, on]) => on ? name : '').filter(name => name).join('/')
