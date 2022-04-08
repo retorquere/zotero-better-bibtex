4900d62c592c192e992f679d95efd2ca2159af78
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2022-04-08 13:27:37 +0200

    basics ready

--- content/key-manager/jsformatter.ts
+++ content/key-manager/jsformatter.ts
@@ -3,16 +3,19 @@
 import * as recast from 'recast'
 import api from '../../gen/api/key-formatter.json'
 const methodnames: Record<string, string> = Object.keys(api).reduce((acc, name) => { acc[name.toLowerCase()]=name; return acc }, {} as Record<string, string>)
-console.log(methodnames)
 function findMethod(fname: string): string {
   const uscore = fname.replace(/[a-z][A-Z]/g, chr => `${chr[0]}_${chr[1]}`).toLowerCase()
   const duscore = fname.replace(/[a-z][A-Z]/g, chr => `${chr[0]}__${chr[1]}`).toLowerCase()
-  console.log('find', uscore, duscore, methodnames[uscore] || methodnames[duscore])
-  return methodnames[uscore] || methodnames[duscore]
+  for (const prefix of ['', '$', '_']) {
+    for (let name of [uscore, duscore]) {
+      if (name = methodnames[prefix + name]) return name
+    }
+  }
+  return ''
 }
 
 type AST = any
-type Creator = { fname: string, onlyEditors?: boolean, scrub?: boolean }
+type Creator = { fname?: string, onlyEditors?: boolean, scrub?: boolean, joiner?: string }
 
 const Ajv = require('ajv')
 const ajv = new Ajv()
@@ -26,6 +29,8 @@ for (const fname in api) {
   if (fname[0] !== '_' && fname[0] !== '$') throw new Error(`Unexpected fname ${fname}`)
 }
 
+console.log('started', new Date)
+
 export class PatternParser {
   public code: string
   private finder: AST
@@ -34,6 +39,7 @@ export class PatternParser {
   constructor(source: string) {
     this.finder = recast.parse('[].find(pattern => { try { return pattern() } catch (err) { if (err.next) return ""; throw err } })')
     this.addpattern(recast.parse(source).program.body[0].expression)
+    this.insert(recast.parse('"zotero-" + this.item.id').program.body[0].expression, false)
     this.code = recast.prettyPrint(this.finder, {tabWidth: 2}).code
   }
 
@@ -53,64 +59,78 @@ export class PatternParser {
     const function_name = `${prefix}${rest}`
     const onlyEditors = !!editor
     const scrub = !!prefix.match(/^[ae]/)
-    console.log('creator.name', name, function_name)
+    const joiner = ''
 
     let fname = ''
     for (let [author, editor] of [['authors', 'editors'], ['author', 'editor'], ['authAuth', 'edtrEdtr'], [ 'auth', 'edtr' ]]) {
       fname = function_name.startsWith(editor) ? fname = function_name.replace(editor, author) : function_name
-      console.log('creator.fname', function_name, findMethod(`$${fname}`))
-      if (fname = findMethod(`$${fname}`)) break
+      if (fname = findMethod(fname)) break
 
       author = author[0].toUpperCase() + author.slice(1)
       editor = editor[0].toUpperCase() + editor.slice(1)
       fname = function_name.startsWith(editor) ? fname = function_name.replace(editor, author) : function_name
-      console.log('creator.fname', function_name, findMethod(`$${fname}`))
-      if (fname = findMethod(`$${fname}`)) break
+      if (fname = findMethod(fname)) break
     }
 
     const method = api[fname]
     if (!method) return null
 
-    const auth = { fname, scrub, onlyEditors, withInitials: false }
+    const auth = { fname: fname.substr(1), joiner, scrub, onlyEditors, withInitials: false }
     for (const field of Object.keys(auth)) {
-      if (!method.parameters.includes(field)) delete auth[field]
+      if (!method.parameters.includes(field) && field !== 'fname') delete auth[field]
     }
-    console.log('creator.auth', name, fname, auth)
     return auth
   }
 
-  private resolveArguments(fname: string, args: AST[]): AST[] {
+  private resolveArguments(fname: string, args: AST[], extra: Record<string, number | string | boolean> = {}): AST[] {
     const method = api[findMethod(fname)] // transitional before rename in formatter.ts
     const kind = {$: 'function', _: 'filter'}[fname[0]]
     fname = fname.slice(1)
     const me = `${kind} ${JSON.stringify(fname)}`
     if (!method) throw new Error(`No such ${me}`)
 
-    const disarray = args.find((arg, i) => arg.named_argument && args[i+1] && !args[i+1].named_argument)
-    if (disarray) throw new Error(`${me}: named argument ${disarray.named_argument} is followed by positional arguments`)
+    let named = false
+    args.forEach((arg, i) => {
+      if (!arg.named_argument) {
+        if (named) throw new Error(`${me}: positional argument ${i+1} after named argument`)
+        arg.named_argument = method.parameters[i]
+      }
+      else {
+        if (method.rest) throw new Error(`${me}: named argument not supported in rest function`)
+        named = true
+      }
+    })
 
-    const parameters = {}
+    for (const [named_argument, value] of Object.entries(extra)) {
+      if (method.rest) throw new Error(`${me}: unexpected extra arguments for rest method`)
+      if (typeof value !== 'undefined' && !args.find(arg => arg.named_argument === named_argument)) {
+        args.push({ type: 'Literal', value, named_argument })
+      }
+    }
+
+    let parameters = {}
+    args = args.map((arg, i) => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      let { named_argument, loc, ...argc } = arg
+      if (typeof parameters[named_argument] === 'undefined') {
+        parameters[named_argument] = argc
+      }
+      else {
+        throw new Error(`${me}: duplicate argument ${JSON.stringify(named_argument)}`)
+      }
+      return argc
+    })
+
     if (method.rest) {
       if (method.parameters.length !== 1) throw new Error(`${me}: ...rest method may have only one parameter, got ${method.parameters.join(', ')}`)
-      if (args.find(arg => arg.named_argument)) throw new Error(`${me}: named argument not supported in rest function`)
-      args = args.map(({ _loc, ...arg }) => arg) // eslint-disable-line @typescript-eslint/no-unsafe-return
-      parameters[method.rest] = { type: 'ArrayExpression', elements: args }
+      parameters = { [method.rest]: { type: 'ArrayExpression', elements: args } }
     }
     else {
       if (method.parameters.length < args.length) throw new Error(`${me}: expected ${method.parameters.length} arguments, got ${args.length}`)
 
-      const names = []
-      console.log(me, args, parameters)
-      args = args.map((arg, i) => {
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const { named_argument, _loc, ...argc } = arg
-        const name = named_argument || method.parameters[i]
-        if (names.includes(name)) throw new Error(`${me}: duplicate parameter ${name}`)
-        names.push(name)
-        parameters[name] = argc
-        return argc
-      })
       args = method.parameters.map((param: string) => parameters[param] as AST || { type: 'Identifier', name: 'undefined' } as AST)
+      let arg
+      while (args.length && (arg = args[args.length - 1]).type === 'Identifier' && arg.name === 'undefined') args.pop()
     }
 
     if (!method.validate(parameters)) {
@@ -126,6 +146,7 @@ export class PatternParser {
 
   protected Identifier(expr: AST, context: any): AST {
     let author: Creator
+    let fname: string
     if (context.arguments) {
       return { type: 'Literal', value: expr.name }
     }
@@ -133,19 +154,14 @@ export class PatternParser {
       return expr
     }
     else if (author = this.creator(expr.name)) {
-      console.log('ident', author)
-      const method = api[`$${author.fname}`]
-      if (!method) throw new Error(`No such function ${author.fname}`)
+      ({ fname, ...author } = author)
+      const method = api[`$${fname}`]
+      if (!method) throw new Error(`No such function ${fname}`)
 
-      const args = [ { type: 'Literal', value: author.scrub, named_argument: 'scrub' } ]
-      for (const field of Object.keys(author)) {
-        if (field !== 'name') {
-          args.push({ type: 'Literal', value: author[field], named_argument: field })
-        }
-      }
+      const args = Object.entries(author).map(([named_argument, value]) => ({ type: 'Literal', value, named_argument }))
       return {
         type: 'CallExpression',
-        callee: { type: 'Identifier', name: author.fname },
+        callee: { type: 'Identifier', name: fname },
         arguments: args,
       } as AST
     }
@@ -227,7 +243,7 @@ export class PatternParser {
 
   private resolve(expr: AST) {
     let fname: string
-    let author: Creator
+    let author: Partial<Creator>
 
     switch (expr.type) {
       case 'CallExpression':
@@ -240,26 +256,24 @@ export class PatternParser {
         }
 
         if (expr.callee.type === 'Identifier') {
-          console.log('callex ident', expr.callee.name)
           fname = expr.callee.name = `${fname}${expr.callee.name}`
         }
         else if (expr.callee.type === 'MemberExpression' && expr.callee.property.type === 'Identifier') {
-          console.log('callex member', expr.callee.property.name)
           fname = expr.callee.property.name = `${fname}${expr.callee.property.name}`
         }
         else {
           throw expr
         }
 
-        console.log('callexpr', fname)
         if (author = this.creator(fname)) {
-          console.log('callexpr auth', author)
-          fname = `$${author.fname}`
-          expr.arguments.push({ type: 'Literal', value: author.scrub, named_argument: 'scrub' })
-          if (typeof author.onlyEditors !== 'undefined') expr.arguments.push({ type: 'Literal', value: author.onlyEditors, named_argument: 'onlyEditors' })
+          ({ fname, ...author } = author)
+          fname = `$${fname}`
+        }
+        else {
+          author = {}
         }
 
-        expr.arguments = this.resolveArguments(fname, expr.arguments)
+        expr.arguments = this.resolveArguments(fname, expr.arguments, author)
         this.resolve(expr.callee)
         break
 
@@ -284,11 +298,13 @@ export class PatternParser {
     }
   }
 
-  private insert(expr: AST) {
-    expr = this.convert(expr, {})
-    this.ftype = '$'
-    this.resolve(expr)
-    expr = this.addThis(expr, { coerce: true })
+  private insert(expr: AST, convert=true) {
+    if (convert) {
+      expr = this.convert(expr, {})
+      this.ftype = '$'
+      this.resolve(expr)
+      expr = this.addThis(expr, { coerce: true })
+    }
 
     this.finder.program.body[0].expression.callee.object.elements.push({ type: 'ArrowFunctionExpression', params: [], body: expr, expression: true })
   }
@@ -302,4 +318,8 @@ export class PatternParser {
       this.insert(expr)
     }
   }
+}
+
+export function parse(pattern: string): string {
+  return (new PatternParser(pattern)).code
 }
