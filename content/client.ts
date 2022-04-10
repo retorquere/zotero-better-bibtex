8a14e2a49b36eee17d0dd5d86f7a6e3e1a5bcb7e
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2022-04-10 13:06:44 +0200

    closer to migration

--- content/client.ts
+++ content/client.ts
@@ -4,5 +4,5 @@ declare const ZOTERO_CONFIG: any
 // we may be running in a translator, which will have it pre-loaded
 if (typeof Components !== 'undefined') Components.utils.import('resource://zotero/config.js')
 
-// for standalone tests'
-export const client = ZOTERO_CONFIG.GUID.replace(/@.*/, '').replace('-', '')
+// check for process for node testing
+export const client = (typeof process === 'undefined') ? ZOTERO_CONFIG.GUID.replace(/@.*/, '').replace('-', '') : 'zotero'
