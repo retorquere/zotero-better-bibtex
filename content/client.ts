c8d3752baf2c9bfa6a498023b3046bbe042794f4
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2022-04-10 21:18:44 +0200

    client process checking

--- content/client.ts
+++ content/client.ts
@@ -4,5 +4,6 @@ declare const ZOTERO_CONFIG: any
 // we may be running in a translator, which will have it pre-loaded
 if (typeof Components !== 'undefined') Components.utils.import('resource://zotero/config.js')
 
-// check for process for node testing
-export const client = (typeof process === 'undefined') ? ZOTERO_CONFIG.GUID.replace(/@.*/, '').replace('-', '') : 'zotero'
+// check for process.version for node testing
+export const client = (process.version === '') ? ZOTERO_CONFIG.GUID.replace(/@.*/, '').replace('-', '') : 'zotero'
+
