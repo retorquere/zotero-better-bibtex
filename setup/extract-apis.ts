2ba9bc24abb2f1432329f68d54072fa5052d4499
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2022-04-20 16:55:16 +0200

    transition

--- setup/extract-apis.ts
+++ setup/extract-apis.ts
@@ -19,7 +19,6 @@ class FormatterAPI {
       if (!kind) continue
 
       const key = name.toLowerCase()
-      if (key === '$text') continue
 
       if (this.signature[key]) throw new Error(`duplicate ${kind} ${key}`)
       this.signature[name.toLowerCase()] = _.cloneDeep({
@@ -43,7 +42,7 @@ class FormatterAPI {
         + ')'
       }
 
-      this.doc[kind][quoted] = method.doc
+      if (key !== '$text') this.doc[kind][quoted] = method.doc
     }
 
     /* re-enable this after the formatter migration
