f7e3d8d24f8f97e496844d8f8cf87f5078c6272d
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2022-07-19 21:58:02 +0200

    queue without setTimeout -- why is setTimeout so flaky?

--- content/translators.ts
+++ content/translators.ts
@@ -19,7 +19,7 @@ import { flash } from './flash'
 import { $and, Query } from './db/loki'
 import { Events } from './events'
 import { Pinger } from './ping'
-import PQueue from 'p-queue'
+import Puqeue from 'puqeue'
 
 import * as translatorMetadata from '../gen/translators.json'
 
@@ -41,7 +41,7 @@ export const Translators = new class { // eslint-disable-line @typescript-eslint
   public byName: Record<string, Translator.Header>
   public byLabel: Record<string, Translator.Header>
   public itemType: { note: number, attachment: number, annotation: number }
-  private queue: PQueue
+  private queue = new Puqeue
   public worker: ChromeWorker
 
   public workers: { total: number, running: Set<number>, startup: number } = {
@@ -52,11 +52,6 @@ export const Translators = new class { // eslint-disable-line @typescript-eslint
 
   constructor() {
     Object.assign(this, translatorMetadata)
-    this.queue = new PQueue({
-      concurrency: 1,
-      timeout: 1000 * 60 * 60, // eslint-disable-line no-magic-numbers
-      throwOnTimeout: true,
-    })
   }
 
   public async init() {
