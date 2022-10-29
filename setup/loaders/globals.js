4429a83ee7a609987e4578c97a89b23c5b6b380a
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2022-10-29 15:13:13 +0200

    ncu

--- setup/loaders/globals.js
+++ setup/loaders/globals.js
@@ -1,3 +1,4 @@
 export let process = require('process/browser.js');
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/globalThis
 export var global = Function('return this')();
+export var globalThis = global;
