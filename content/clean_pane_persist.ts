3e97df30c729571705b27983360c90e94776fd95
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2023-05-28 00:16:08 +0200

    almost there

--- content/clean_pane_persist.ts
+++ content/clean_pane_persist.ts
@@ -4,6 +4,7 @@ export function clean_pane_persist(): void {
     try {
       persisted = JSON.parse(persisted)
       delete persisted['zotero-items-column-citekey']
+      delete persisted['zotero-items-column-citationKey']
       Zotero.Prefs.set('pane.persist', JSON.stringify(persisted))
     }
     catch (err) {
