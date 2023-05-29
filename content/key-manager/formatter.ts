c254a947d8e1defb399b2b8fba12db47c63fbd99
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2023-05-29 18:34:31 +0200

    page_range_splitter

--- content/key-manager/formatter.ts
+++ content/key-manager/formatter.ts
@@ -773,14 +773,14 @@ class PatternFormatter {
   public $firstpage() {
     const pages: string = this.item.getField('pages') as string
     if (!pages) return this.$text('')
-    return this.$text(pages.split(/page_range_splitter/)[0] || '')
+    return this.$text(pages.split(page_range_splitter)[0] || '')
   }
 
   /** The number of the last page of the publication (See the remark on `firstpage`) */
   public $lastpage() {
     const pages: string = this.item.getField('pages') as string
     if (!pages) return this.$text('')
-    return this.$text(pages.split(/page_range_splitter/)[0] || '')
+    return this.$text(pages.split(page_range_splitter)[0] || '')
   }
 
   /** Tag number `n`. Mostly for legacy compatibility -- order of tags is undefined */
