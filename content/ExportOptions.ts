606b8d156bebb89fa77b6bdf4ebb7f5367aac435
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2023-08-01 17:42:22 +0200

    export options translate + behavior

--- content/ExportOptions.ts
+++ content/ExportOptions.ts
@@ -9,80 +9,96 @@ let $window: XULWindow // eslint-disable-line no-var
 var Zotero_File_Interface_Export: any // eslint-disable-line no-var
 
 Events.on('window-loaded', ({ win, href }: {win: Window, href: string}) => {
-  if (href === 'chrome://zotero/content/exportOptions.xul') {
-    $window = win as XULWindow
-    Zotero_File_Interface_Export = $window.Zotero_File_Interface_Export
-    Zotero.BetterBibTeX.ExportOptions.load()
+  switch (href) {
+    case 'chrome://zotero/content/exportOptions.xul':
+    case 'chrome://zotero/content/exportOptions.xhtml':
+      $window = win as XULWindow
+      Zotero_File_Interface_Export = $window.Zotero_File_Interface_Export
+      Zotero.BetterBibTeX.ExportOptions.load()
+      break
   }
 })
 
 export class ExportOptions {
-  private DOM_OBSERVER: MutationObserver = null
-  private reset = true
   private patched: Trampoline[] = []
   private elements: Elements
 
   public load(): void {
-    const doc = $window.document
-    this.DOM_OBSERVER = new MutationObserver(this.addEventHandlers.bind(this))
-    this.DOM_OBSERVER.observe(doc.getElementById('translator-options'), { attributes: true, subtree: true, childList: true })
-    this.addEventHandlers()
     $window.addEventListener('unload', () => {
       this.unload()
     })
 
     this.elements = new Elements(document)
-    const translateOptions = doc.getElementById('translator-options')
-    translateOptions.parentNode.insertBefore(this.elements.create('description', {style: 'color: red', hidden: 'true', id: 'better-bibtex-reminder'}), translateOptions)
 
-    this.mutex()
-    this.warning()
-
-    $patch$(Zotero_File_Interface_Export, 'init', original => function(_options) {
-      for (const translator of $window.arguments[0].translators) {
-        if (translator.label === 'BetterBibTeX JSON') translator.label = 'BetterBibTeX debug JSON'
-      }
-      // eslint-disable-next-line prefer-rest-params
-      original.apply(this, arguments)
-    }, this.patched)
+    this.show()
 
     const self = this // eslint-disable-line @typescript-eslint/no-this-alias
     $patch$(Zotero_File_Interface_Export, 'updateOptions', original => function(_options) {
       // eslint-disable-next-line prefer-rest-params
       original.apply(this, arguments)
-      self.warning()
+      self.show()
     }, this.patched)
   }
 
-  public warning(): void {
+  private selected(): any {
     const index = ($window.document.getElementById('format-menu') as HTMLSelectElement).selectedIndex
-    const translator = (index >= 0) ? $window.arguments[0].translators[index].translatorID : null
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return (index >= 0) ? $window.arguments[0].translators[index].translatorID : null
+  }
 
-    let hidden = 'false'
-    let textContent = ''
-    switch (translator) {
+  public show(): void {
+    const doc = $window.document
+
+    const selected = this.selected()
+    let reminder = doc.getElementById('better-bibtex-reminder')
+    if (!selected) {
+      if (reminder) reminder.hidden = true
+      return
+    }
+
+    if (!reminder) {
+      const translateOptions = doc.getElementById('translator-options')
+      translateOptions.parentNode.insertBefore(reminder = this.elements.create('description', {style: 'color: red', hidden: 'true', id: 'better-bibtex-reminder'}), translateOptions)
+    }
+
+    switch (selected.translatorID) {
       case 'b6e39b57-8942-4d11-8259-342c46ce395f':
-        textContent = l10n.localize('better-bibtex_export-options_reminder', { translator: 'Better BibLaTeX' })
+        reminder.hidden = false
+        reminder.textContent = l10n.localize('better-bibtex_export-options_reminder', { translator: 'Better BibLaTeX' })
         break
 
       case '9cb70025-a888-4a29-a210-93ec52da40d4':
-        textContent = l10n.localize('better-bibtex_export-options_reminder', { translator: 'Better BibTeX' })
+        reminder.textContent = l10n.localize('better-bibtex_export-options_reminder', { translator: 'Better BibTeX' })
+        reminder.hidden = false
         break
 
       default:
-        hidden = 'true'
+        reminder.hidden = true
         break
     }
 
-    const reminder = $window.document.getElementById('better-bibtex-reminder')
-    reminder.setAttribute('hidden', hidden)
-    reminder.textContent = textContent
+    for (const node of [...doc.querySelectorAll('#export-option-exportFileData, #export-option-keepUpdated, #export-option-worker')]) {
+      if (node.classList.contains('better-bibex-export-options')) continue
+      node.classList.add('better-bibex-export-options')
+      node.addEventListener('command', this.mutex.bind(this))
+
+      switch (node.getAttribute('id')) {
+        case 'export-option-keepUpdated':
+          node.setAttribute('label', l10n.localize('better-bibtex_export-options_keep-updated'))
+          break
+        case 'export-option-worker':
+          (node as any).checked = false
+          node.setAttribute('label', l10n.localize('better-bibtex_export-options_worker'))
+          break
+      }
+    }
+
+    this.mutex()
 
     $window.sizeToContent()
   }
 
   public unload(): void {
-    this.DOM_OBSERVER.disconnect()
     this.elements.remove()
     $unpatch$(this.patched)
   }
@@ -106,32 +122,5 @@ export class ExportOptions {
 
     keepUpdated.disabled = exportFileData.checked
     worker.disabled = keepUpdated.checked
-  }
-
-  addEventHandlers(): void {
-    for (const id of [ 'export-option-exportFileData', 'export-option-keepUpdated', 'export-option-worker' ]) {
-      const node = $window.document.getElementById(id) as HTMLInputElement
-      if (!node) {
-        Zotero.debug(`exportoptions: ${id} not found`)
-        break
-      }
-
-      if (id === 'export-option-keepUpdated') {
-        node.setAttribute('label', l10n.localize('better-bibtex_export-options_keep-updated'))
-        if (this.reset) {
-          node.checked = false
-          this.reset = false
-        }
-      }
-
-      if (id === 'export-option-worker') {
-        node.setAttribute('label', l10n.localize('better-bibtex_export-options_worker'))
-      }
-
-      if (node.getAttribute('better-bibtex')) return null
-
-      node.setAttribute('better-bibtex', 'true')
-      node.addEventListener('command', this.mutex.bind(this))
-    }
   }
 }
