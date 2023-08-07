12adb7a0e2fe8c30d4f55021657c3cefa8c1e0a9
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2023-08-07 02:53:50 +0200

    adjust tests for new sentence-caser

--- content/l10n.ts
+++ content/l10n.ts
@@ -1,18 +1,16 @@
 import format = require('string-template')
 import { log } from './logger'
-const dtdParser = require('./dtd-file.peggy')
 
-let dtd: Record<string, string>
-const url = 'chrome://zotero-better-bibtex/locale/zotero-better-bibtex.dtd'
+const src = 'chrome://zotero-better-bibtex/locale/zotero-better-bibtex.properties'
+const stringBundle = Services.strings.createBundle(src)
 
 export function localize(id: string, params: any = null): string {
   try {
-    dtd = dtd || dtdParser.parse(Zotero.File.getContentsFromURL(url))
-    const str: string = dtd[id] || id
+    const str: string = stringBundle.GetStringFromName(id)
     return params ? (format(str, params) as string) : str
   }
   catch (err) {
-    log.error('l10n.get error:', url, id, err)
+    log.error('l10n.get error:', src, id, err)
     return id
   }
 }
