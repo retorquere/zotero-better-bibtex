9a3fc84df8526b6575e276be16adde7ec31a3c95
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2023-08-16 09:41:21 +0200

    fluent for zotero 7

--- content/l10n.ts
+++ content/l10n.ts
@@ -1,16 +1,24 @@
 import format = require('string-template')
 import { log } from './logger'
+import { is7 } from './client'
 
-const src = 'chrome://zotero-better-bibtex/locale/zotero-better-bibtex.properties'
-const stringBundle = Services.strings.createBundle(src)
+declare const Localization: any
+
+const stringBundle = is7 ?  null : Services.strings.createBundle('chrome://zotero-better-bibtex/locale/zotero-better-bibtex.properties')
+const l10n = is7 ? new Localization(['better-bibtex.ftl'], true) : null
 
 export function localize(id: string, params: any = null): string {
   try {
-    const str: string = stringBundle.GetStringFromName(id)
-    return params ? (format(str, params) as string) : str
+    if (is7) {
+      return l10n.formatValueSync(id, params || {}) as string
+    }
+    else {
+      const str: string = stringBundle.GetStringFromName(id)
+      return params ? (format(str, params) as string) : str
+    }
   }
   catch (err) {
-    log.error('l10n.get error:', src, id, err)
+    log.error('l10n.get error:', id, err)
     return id
   }
 }
