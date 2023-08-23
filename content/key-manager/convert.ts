6318efdf7de6ceefd7f04fcd62c78f5b33d00aef
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2023-08-23 14:20:01 +0200

    nested-formula

--- content/key-manager/convert.ts
+++ content/key-manager/convert.ts
@@ -100,9 +100,26 @@ class Compiler {
     return parts
   }
 
+  wrap(formula: string): string {
+    return `sub(function() { return ${formula} })`
+  }
+
   $formula(ast: Node, wrap=false): string {
-    let formula = `this.finalize(this.reset() + ${this.split(ast, '+').map((term: Node) => this.$term(term)).join(' + ')})`
-    if (wrap) formula = `sub(function() { return ${formula} })`
+    let formula = ''
+    for (const term of this.split(ast, '+').map((t: Node) => this.$term(t))) {
+      if (term.startsWith('this._len')) { // convert function len to filter
+        formula = `this.$text(${this.wrap(formula)}).${term.replace(/^this[.]/, '')}`
+      }
+      else if (formula) {
+        formula += ` + ${term}`
+      }
+      else {
+        formula = term
+      }
+    }
+    formula = `this.finalize(this.reset() + ${formula})`
+    // let formula = `this.finalize(this.reset() + ${this.split(ast, '+').map((term: Node) => this.$term(term)).join(' + ')})`
+    if (wrap) formula = this.wrap(formula)
     return formula
   }
 
@@ -124,7 +141,12 @@ ${compiled}
   flatten(node: Node): Node[] {
     switch (node.type) {
       case 'MemberExpression':
-        return this.flatten(node.object).concat(this.flatten(node.property))
+        if (node.object.type === 'BinaryExpression') {
+          return [node.object].concat(this.flatten(node.property))
+        }
+        else {
+          return this.flatten(node.object).concat(this.flatten(node.property))
+        }
       case 'CallExpression':
         return this.flatten(node.callee).concat([node])
       case 'Identifier':
@@ -132,6 +154,9 @@ ${compiled}
         return [node]
       case 'LogicalExpression':
         if (node.operator !== '||') this.error(node, `unexpected ${node.type} operator ${node.operator}`)
+        return [node]
+      case 'BinaryExpression':
+        if (node.operator !== '+') this.error(node, `unexpected ${node.type} operator ${node.operator}`)
         return [node]
       case 'Literal':
         if (typeof node.value !== 'string') this.error(node, `expected string, got ${this.$typeof(node)}`)
@@ -164,7 +189,11 @@ ${compiled}
         const right = this.$formula(identifier.right, true)
         compiled += `.$text(${left} || ${right})`
       }
-      else if (prefix === '$' && identifier.name[0] === identifier.name[0].toUpperCase()) {
+      else if (identifier.type === 'BinaryExpression') {
+        const formula = this.$formula(identifier, true)
+        compiled += `.$text(${formula})`
+      }
+      else if (prefix === '$' && identifier.name[0] === identifier.name[0].toUpperCase()) { // direct property access
         if (flat[0] && flat[0].type === 'CallExpression') this.error(flat[0], `field "${identifier.name}" cannot be called as a function`)
         const name = items.name.field[identifier.name.toLowerCase()]
         if (!name) this.error(identifier, `Zotero items do not have a field named "${identifier.name}"`)
@@ -173,7 +202,7 @@ ${compiled}
       else {
         const name = `${prefix}${identifier.name.toLowerCase()}`
         const $name = alias[name] || name
-        const method = methods[$name]
+        const method = methods[$name === '$len' ? '_len' : $name] // fake out $len resolution by resolving it as _len
         if (!method) this.error(identifier, `Unknown ${prefix === '$' ? 'function' : 'filter'} ${identifier.name}`)
 
         compiled += `.${method.name}(`
