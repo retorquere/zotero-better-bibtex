ca969e58e0522e69825d655e8acf12431918dbf1
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2023-09-06 11:27:04 +0200

    convert.js

---
+++ convert.js
@@ -0,0 +1,197 @@
+const { parse, print, prettyPrint } = require('recast')
+const { visit, builders, namedTypes } = require('ast-types')
+const b = builders
+
+// const formula = '(auth + title).lower + year'
+// const formula = '(auth(creator="*").lower(1) + year).postfix(_) + (title > 0).lower | title.lower | year'
+const formula = '(x() ? y : z) + y() + len + ((auth(creator="*").lower(1) + year) > 0).postfix(_) + (title > 0).lower + other | x'
+
+function error(node, msg) {
+  throw new Error(`${print(node).code} ${msg}`)
+}
+function assert(cond, node, msg) {
+  if (!cond) error(node, msg)
+}
+
+const toArrayExpression = {
+  visitProgram(path) {
+    const body = path.node.body
+    if (!body.length) error(path.node, 'empty formula')
+
+    if (body.length > 1) { // multiple semi-colon separated statements
+      path.node.body = [
+        b.expressionStatement(
+          b.arrayExpression(
+            path.node.body.map(es => es.type === 'ExpressionStatement' ? b.arrowFunctionExpression([], es.expression) : error(es, 'is not an expression statement'))
+          )
+        )
+      ]
+    }
+
+    if (!body[0].type === 'ExpressionStatement') error(body[0], `Unexpected ${body[0].type}`)
+    const expression = body[0].expression
+
+    if (expression.type === 'BinaryExpression' && expression.operator === '|') {
+      let head = expression
+      const expressions = []
+      while (head.type === 'BinaryExpression' && head.operator === '|') {
+        expressions.unshift(head.right)
+        head = head.left
+      }
+      expressions.unshift(head)
+      path.node.body = [ b.expressionStatement(b.arrayExpression(expressions.map(ex => b.arrowFunctionExpression([], ex)))) ]
+    }
+    else if (expression.type === 'SequenceExpression') {
+      path.node.body = [ b.expressionStatement(b.arrayExpression(expression.expressions.map(ex => b.arrowFunctionExpression([], ex)))) ]
+    }
+    else {
+      path.node.body = [ b.expressionStatement(b.arrayExpression([b.arrowFunctionExpression([], expression)])) ]
+    }
+
+    this.traverse(path)
+  },
+
+  // change > n to .len('>', n)
+  visitBinaryExpression(path) {
+    if (path.node.operator.match(/^(<|<=|==|!=|>=|>)$/)) {
+      assert(path.node.right.type === 'Literal' && typeof path.node.right.value === 'number' && path.node.right.value >= 0, path.node, 'needs number value')
+      const object = path.node.left
+      const property = b.identifier('len')
+
+      const callee = b.memberExpression(object, property)
+      const args = [ b.literal(path.node.operator), path.node.right ]
+      return b.callExpression(callee, args)
+    }
+    else if (path.node.operator === '+') {
+      let len = path.node.right
+      if (len.type === 'CallExpression') len = len.callee
+      if (len.type === 'Identifier' && len.name === 'len') {
+        return b.memberExpression(path.node.left, len)
+      }
+    }
+
+    this.traverse(path)
+  },
+}
+
+function parents(path) {
+  let head = path
+  let history = []
+  while (head) {
+    history.push(head.node)
+    head = head.parent
+  }
+
+  return history.map((node, i) => {
+    if (node.type === 'BinaryExpression') {
+      if (node.left === history[i-1]) return `${node.operator}l`
+      if (node.right === history[i-1]) return `${node.operator}r`
+      return node.operator
+    }
+    else if (node.type === 'MemberExpression') {
+      if (node.object === history[i-1]) return '.o'
+      if (node.property === history[i-1]) return '.p'
+      return '.'
+    }
+    else {
+      return node.type
+    }
+  }).slice(1)
+}
+
+const markArgument = {
+  visitNode(path) {
+    path.node.inArgument = true
+    this.traverse(path)
+  }
+}
+
+const makeMethod = {
+  visitUnaryExpression(path) {
+    assert(path.node.operator === '-', path.node, 'unexpected unary expression')
+    assert(path.node.argument.type === 'Literal' && typeof path.node.argument.value === 'number', path.node, '- can only be applied to literal')
+    return b.literal(-1 * path.node.argument.value)
+  },
+
+  visitCallExpression(path) {
+    for (const arg of path.node.arguments) {
+      visit(arg, markArgument)
+    }
+    this.traverse(path)
+  },
+
+  visitIdentifier(path) {
+    const par = parents(path).join(' ')
+    if (path.node.inArgument || par.match(/^([.]p )?CallExpression/)) {
+      // pass
+    }
+    else if (par.match(/^([+.]|ConditionalExpression|ArrowFunctionExpression)/)) {
+      return b.callExpression(path.node, [], false)
+    }
+    else {
+      console.log('unhandled', path.node.name, par)
+    }
+
+    this.traverse(path)
+  }
+}
+
+function wrap(formula) {
+  const sub = b.callExpression(b.identifier('sub'), [
+    b.functionExpression(null, [], b.blockStatement([ b.returnStatement(formula) ]))
+  ])
+  const text = b.callExpression(b.memberExpression(b.thisExpression(), b.identifier('$text')), [sub])
+  return text
+}
+function finalize(formula) {
+  const finalized = parse(`this.finalize(this.reset() + ${print(formula).code})`)
+  return finalized.program.body[0].expression
+}
+
+const nameSpace = {
+  visitIdentifier(path) {
+    if (!path.node.inArgument) {
+      const par = parents(path).join(' ')
+      const prefix = par.match(/^CallExpression ([.]o|[+]|ArrowFunctionExpression)/) ? '$' : '_'
+      path.node.name = `${prefix}${path.node.name}`
+    }
+
+    this.traverse(path)
+  },
+
+  visitConditionalExpression(path) {
+    this.traverse(path)
+    for (const field of ['test', 'consequent', 'alternate']) {
+      path.node[field] = wrap(finalize(path.node[field]))
+    }
+  },
+
+  visitMemberExpression(path) {
+    this.traverse(path)
+    if (path.node.object.type === 'BinaryExpression') {
+      path.node.object = wrap(finalize(path.node.object))
+    }
+  },
+
+  visitCallExpression(path) {
+    path.node.arguments = path.node.arguments.map(a => a.type === 'Identifier' ? b.literal(a.name) : a)
+
+    this.traverse(path)
+
+    if (path.node.callee.type === 'Identifier' && path.node.callee.name[0] === '$') {
+      path.node.callee = b.memberExpression(b.thisExpression(), path.node.callee)
+    }
+  },
+
+  visitArrowFunctionExpression(path) {
+    this.traverse(path)
+    path.node.body = finalize(path.node.body)
+  }
+}
+
+console.log(formula)
+let ast = parse(formula)
+ast = visit(ast, toArrayExpression)
+ast = visit(ast, makeMethod)
+ast = visit(ast, nameSpace)
+console.log(prettyPrint(ast).code)
