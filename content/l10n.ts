ef31b05bf2974d261b7d6b7ef24c89bb06f1f28f
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2023-10-08 19:41:54 +0200

    l10n

--- content/l10n.ts
+++ content/l10n.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/indent */
+
 import format = require('string-template')
 import { log } from './logger'
 import { is7 } from './client'
@@ -9,7 +11,16 @@ const strings = is7
   : Services.strings.createBundle('chrome://zotero-better-bibtex/locale/zotero-better-bibtex.properties')
 
 export const localizev = is7
-  ? (id: string, params: any = null): string => strings.formatValueSync(id, params || {}) as string
+  ? (id_with_branch: string, params: any = null): string => {
+      if (id_with_branch.includes('.')) {
+        const [ id, branch ] = id_with_branch.split('.')
+        const messages = strings.formatMessagesSync([{ id, args: params || {} }])
+        return messages[0].attributes[0][branch] as string
+      }
+      else {
+        return strings.formatValueSync(id_with_branch, params || {}) as string
+      }
+    }
   : (id: string, params: any = null): string => {
     const str: string = strings.GetStringFromName(id)
     return params ? (format(str, params) as string) : str
