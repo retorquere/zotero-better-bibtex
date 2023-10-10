182a8253f96256de86b5bc893b9ace6881ac2868
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2023-10-10 23:26:50 +0200

    pref path for bins

--- content/path-search.ts
+++ content/path-search.ts
@@ -50,7 +50,7 @@ function expandWinVars(value: string): string {
 
 // https://searchfox.org/mozilla-central/source/toolkit/modules/subprocess/subprocess_win.jsm#135 doesn't seem to work on Windows.
 export async function findBinary(bin: string, installationDirectory: { mac?: string[], win?: string[] } = {}): Promise<string> {
-  const pref = `extensions.zotero.translators.better-bibtex.path.${bin}`
+  const pref = `translators.better-bibtex.path.${bin}`
   let location: string = Zotero.Prefs.get(pref)
   if (typeof location !== 'string') location = ''
   if (location === 'false') return ''
