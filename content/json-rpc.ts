e2d6b64cfbc094784bd7d1d6140b10aaf1004158
mgmeyers <matthew.gregory.meyers@gmail.com>
2024-01-09 18:30:33 -0800

    JSON-RPC: Update comment

--- content/json-rpc.ts
+++ content/json-rpc.ts
@@ -120,7 +120,7 @@ class NSItem {
    * - search([['ignore_feeds'], ['quicksearch-titleCreatorYear', 'contains', 'Zotero']]): quick search for 'Zotero' ignoring the Feeds
    * - search([['creator', 'contains', 'Johnny'], ['title', 'contains', 'Zotero']]): search for entries with Creator 'Johnny' AND Title 'Zotero'
    * - search([['joinMode', 'any'], ['creator', 'contains', 'Johnny'], ['title', 'contains', 'Zotero']]): search for entries with Creator 'Johnny' OR Title 'Zotero'
-   * - search([['blockStart'], ['creator', 'contains', 'Johnny'], ['title', 'contains', 'Zotero'], ['blockEnd'], ['creator', 'contains', 'Smith']]): search for entries with (Creator 'Johnny' OR Title 'Zotero') AND (Creator 'Smith')
+   * - search([['joinMode', 'any'], ['creator', 'contains', 'Johnny'], ['title', 'contains', 'Zotero'], ['creator', 'contains', 'Smith', true]]): search for entries with (Creator 'Johnny' OR Title 'Zotero') AND (Creator 'Smith')
    *
    * @param terms  Single string as typed into the search box in Zotero (search for Title Creator Year)
    *               Array of tuples similar as typed into the advanced search box in Zotero
