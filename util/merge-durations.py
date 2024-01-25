99ef9555055bc125a305e47db045ad81a5513fea
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2024-01-25 14:06:18 +0100

    did I mention these new artifact actions are pretty terrible?

--- util/merge-durations.py
+++ util/merge-durations.py
@@ -9,22 +9,13 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('-o', '--output', required=True)
 parser.add_argument('-c', '--client', required=True)
-parser.add_argument('-w', '--worker', action='store_true')
 parser.add_argument('durations', nargs='+')
 args = parser.parse_args()
 
 durations = {}
 for job in sorted(args.durations):
   print(os.path.basename(job))
-  dur, client, bin, worker = os.path.basename(job).replace('.json', '').split('--')
-  if worker == 'worker':
-    key = f'{client}-worker'
-  else:
-    key = client
-  if key not in durations:
-    durations[key] = {}
-  with open(job) as f:
-    durations[key] = durations[key] | json.load(f)
+  durations = durations | json.load(f)
 
 for key, dur in durations.items():
   print(key, timedelta(seconds=sum([test['seconds'] for test in dur.values()])))
@@ -32,9 +23,5 @@ for key, dur in durations.items():
 directory = os.path.dirname(args.output)
 if not os.path.exists(directory): os.makedirs(directory)
 
-if args.worker:
-  key = f'{args.client}-worker'
-else:
-  key = args.client
 with open(args.output, 'w') as f:
   json.dump(durations[key], f, indent='  ')
