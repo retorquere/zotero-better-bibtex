bebc26049a39b75acf3eabe6f35c477c008fd9a4
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2024-04-09 16:51:10 +0200

    nsiPromptService

--- content/prompt.ts
+++ content/prompt.ts
@@ -1,12 +1,15 @@
-const ps = Components.classes['@mozilla.org/embedcomp/prompt-service;1'].getService(Components.interfaces.nsIPromptService)
+import { is7 } from './client'
+export const PromptService = is7
+  ? Services.prompt
+  : Components.classes['@mozilla.org/embedcomp/prompt-service;1'].getService(Components.interfaces.nsIPromptService)
 
 export function alert({ title, text }: { title?: string, text: string }): void {
-  ps.alert(null, title || 'Alert', text)
+  PromptService.alert(null, title || 'Alert', text)
 }
 
 export function prompt({ title, text, value }: { title?: string, text: string, value?: string }): string {
   const wrap = { value: value || '' }
-  if (ps.prompt(null, title || 'Enter text', text, wrap, null, {})) {
+  if (PromptService.prompt(null, title || 'Enter text', text, wrap, null, {})) {
     return wrap.value
   }
   else {
