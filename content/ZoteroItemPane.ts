6af233487c349fb2ebd5616ec49bba7ee781a986
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2024-04-29 16:17:23 +0200

    section header

--- content/ZoteroItemPane.ts
+++ content/ZoteroItemPane.ts
@@ -50,11 +50,11 @@ export class ZoteroItemPane {
           paneID: 'betterbibtex-section-citationkey',
           pluginID: 'better-bibtex@iris-advies.com',
           header: {
-            l10nID: 'example-item-pane-header',
+            l10nID: 'betterbibtex-item-pane-header',
             icon: `${rootURI}content/skin/citation-key.png`,
           },
           sidenav: {
-            l10nID: 'example-item-pane-header',
+            l10nID: 'betterbibtex-item-pane-header',
             icon: `${rootURI}content/skin/citation-key.png`,
           },
           bodyXHTML: 'Citation Key <html:input type="text" id="better-bibtex-citation-key" readonly="true" style="position:relative;width:80%" xmlns:html="http://www.w3.org/1999/xhtml"/>',
