ccdaef48228b4dc6b096de4bf1ec2e0f859df8d7
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2024-05-05 15:04:48 +0200

    this shim...

--- content/os.ts
+++ content/os.ts
@@ -4,14 +4,16 @@ import { print } from './logger'
 import { OS as $OS } from '../gen/osfile'
 export const Shim: any = is7 ? $OS : undefined
 
-// no idea why it was decided the shim should not accept relative paths
-const Path = platform.windows ? { start: /\\/g, end: /\\$/ } : { start: /\//g, end: /\/$/ }
-Shim.Path.basename = (path: string) => path && (Shim.Path.normalize(path) as string).replace(Path.end, '').replace(Path.start, '')
+if (Shim) {
+  // no idea why it was decided the shim should not accept relative paths
+  const Path = platform.windows ? { start: /\\/g, end: /\\$/ } : { start: /\//g, end: /\/$/ }
+  Shim.Path.basename = (path: string) => path && (Shim.Path.normalize(path) as string).replace(Path.end, '').replace(Path.start, '')
 
-const exists = Shim.File.exists
-Shim.File.exists = (path: string) => {
-  print(`2867: testing ${JSON.stringify(path)}`)
-  return exists(path) as boolean
+  const exists = Shim.File.exists
+  Shim.File.exists = (path: string) => {
+    print(`2867: testing ${JSON.stringify(path)}`)
+    return exists(path) as boolean
+  }
 }
 
 /*
