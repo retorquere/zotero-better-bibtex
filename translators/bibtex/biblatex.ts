59ff16f5f47d9e10ee025058f71204c567de2e50
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2024-05-16 09:32:01 +0200

    probably fixes #2881, but I need a testcase

--- translators/bibtex/biblatex.ts
+++ translators/bibtex/biblatex.ts
@@ -350,7 +350,8 @@ export function generateBibLaTeX(translation: Translation): void {
     if (entry.entrytype === 'book' && item.numberOfVolumes) entry.entrytype = 'mvbook'
     if (entry.entrytype === 'report' && item.type?.toLowerCase().includes('manual')) entry.entrytype = 'manual'
     // if (item.itemType === 'preprint' && entry.entrytype === 'online' && item.ISSN && item.publicationTitle) entry.entrytype = 'article'
-    if (item.itemType === 'preprint') entry.add({ name: 'pubstate', value: 'preprint' })
+
+    this.add({ name: 'pubstate', value: item.itemType === 'preprint' ? 'preprint' : this.item.status })
 
     if (entry.entrytype === 'patent') {
       if (item.country && !patent.region(item)) entry.add({ name: 'location', value: item.country || item.extraFields.kv['publisher-place'] })
