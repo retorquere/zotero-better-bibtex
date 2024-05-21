6bbc292eb646ba2d9428470158eca902ff47af4d
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2024-05-21 21:41:38 +0200

    just busy-wait then

--- content/translators.ts
+++ content/translators.ts
@@ -564,8 +564,10 @@ export const Translators = new class { // eslint-disable-line @typescript-eslint
       }
     }
 
+    /* screw it. the busy-wait guarantees it has once been inited, and this just hangs for no reason for some people.
     log.debug('installing translators: now actually waiting for Zotero.Translators.init()')
     await Zotero.Translators.init()
+    */
 
     log.debug('installing translators: loading BBT translators')
     const reinit: { header: Translator.Header, code: string }[] = []
