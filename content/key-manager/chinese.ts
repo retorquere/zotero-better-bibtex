5a95fb4c5c97591d88838feab11868e18c210f82
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2024-07-30 21:02:26 +0200

    console stripping no worky

--- content/key-manager/chinese.ts
+++ content/key-manager/chinese.ts
@@ -1,6 +1,7 @@
 import { Preference } from '../prefs'
 import { Events } from '../events'
 // import { CJK } from '../text'
+import { discard } from '../logger'
 
 declare const ChromeUtils: any
 
@@ -14,6 +15,7 @@ import type { jieba as jiebaFunc, pinyin as pinyinFunc } from './chinese-optiona
 export const chinese = new class {
   public window: Window
   public document: Document
+  public console = discard
 
   public jieba: typeof jiebaFunc
   public pinyin: typeof pinyinFunc
