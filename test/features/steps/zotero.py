7bac985fcaed73e5008e61b55247fe2588be6c09
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2024-08-21 12:48:52 +0200

    skip crossref outage

--- test/features/steps/zotero.py
+++ test/features/steps/zotero.py
@@ -688,7 +688,7 @@ class Zotero:
     profile.firefox.set_preference('extensions.zotero.translators.better-bibtex.caching', self.caching)
     profile.firefox.set_preference('extensions.zotero.translators.better-bibtex.scrubDatabase', True)
     # don't nag about the Z7 beta for a day
-    profile.firefox.set_preference('extensions.zotero.hiddenNotices', json.dumps({ 'z7-beta-warning': time.time() + 86400 }))
+    profile.firefox.set_preference('extensions.zotero.hiddenNotices', json.dumps({ 'crossref-outage-2024-08-21': time.time() + 86400 }))
     profile.firefox.set_preference('intl.accept_languages', 'en-GB')
     profile.firefox.set_preference('intl.locale.requested', 'en-GB')
 
