ee5513d35ce42bfb715099799f8cff091e2f0596
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2025-02-21 16:01:08 +0100

    esm plugin

--- setup/api/type-doc-all-defaults.js
+++ setup/api/type-doc-all-defaults.js
@@ -1,39 +1,23 @@
-const { Converter, TypeScript } = require('typedoc')
+import { Converter, TypeScript } from 'typedoc'
 
-exports.load = function({ application }) {
-  /** @type {Map<Reflection, string>} */
+export function load({ application }) {
   const defaultValues = new Map()
 
-  const printer = TypeScript.createPrinter({
-    removeComments: true,
-    omitTrailingSemicolon: true,
-  })
+  const printer = TypeScript.createPrinter({ removeComments: true, omitTrailingSemicolon: true })
 
-  application.converter.on(
-    Converter.EVENT_CREATE_DECLARATION,
-    saveDefaultValues,
-  )
-  application.converter.on(
-    Converter.EVENT_CREATE_PARAMETER,
-    saveDefaultValues,
-  )
+  application.converter.on(Converter.EVENT_CREATE_DECLARATION, saveDefaultValues)
+  application.converter.on(Converter.EVENT_CREATE_PARAMETER, saveDefaultValues)
 
   function saveDefaultValues(_context, reflection) {
-    const node = reflection.project.getSymbolFromReflection(reflection)
-      ?.declarations?.[0]
+    const node = reflection.project.getSymbolFromReflection(reflection)?.declarations?.[0]
     if (!node || !node.initializer) return
 
-    // if ([TypeScript.SyntaxKind.ObjectLiteralExpression, TypeScript.SyntaxKind.ArrayLiteralExpression].includes(node.initializer.kind)) {
     // Unfortunately can't just set defaultValue right here, this happens before TD sets it.
-    defaultValues.set(
-      reflection,
-      printer.printNode(
-        TypeScript.EmitHint.Expression,
-        node.initializer,
-        node.getSourceFile(),
-      ),
-    )
-    // }
+    defaultValues.set(reflection, printer.printNode(
+      TypeScript.EmitHint.Expression,
+      node.initializer,
+      node.getSourceFile(),
+    ))
   }
 
   application.converter.on(Converter.EVENT_RESOLVE_BEGIN, () => {
