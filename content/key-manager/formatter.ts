d34ee0decf059aae45df45ca860d7e097c304b3d
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2025-03-21 08:19:41 +0100

    don't do more work than needed

--- content/key-manager/formatter.ts
+++ content/key-manager/formatter.ts
@@ -1537,12 +1537,14 @@ export class PatternFormatter {
       I: this.initials(creator),
       i: this.initials(creator, false),
     }
-    Object.assign(vars, {
-      f_zh: chinese.loaded?.splitName(name).familyName.name || vars.f,
-      f_zh_Latn: chinese.loaded?.splitName(name).familyName.transliteration || vars.f,
-      g_zh: chinese.loaded?.splitName(name).givenName.name || vars.g,
-      g_zh_Latn: chinese.loaded?.splitName(name).givenName.transliteration || vars.g,
-    })
+    if (template.includes('_zh') {
+      Object.assign(vars, {
+        f_zh: chinese.loaded?.splitName(name).familyName.name || vars.f,
+        f_zh_Latn: chinese.loaded?.splitName(name).familyName.transliteration || vars.f,
+        g_zh: chinese.loaded?.splitName(name).givenName.name || vars.g,
+        g_zh_Latn: chinese.loaded?.splitName(name).givenName.transliteration || vars.g,
+      })
+    }
     return sprintf(template, vars) as string
   }
 
