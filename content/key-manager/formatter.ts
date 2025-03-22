cce00e8db8e70684eb97ba5cb3b45c0611764c75
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2025-03-22 15:23:41 +0100

    cleanup

--- content/key-manager/formatter.ts
+++ content/key-manager/formatter.ts
@@ -1569,7 +1569,7 @@ export class PatternFormatter {
       i: this.initials(creator, false),
     }
     let isNameSplit = false
-    if (chinese.enabled && vars.g === '' && Preference.splitName
+    if (chinese.enabled && vars.g === '' && Preference.chineseSplitName
       && !(japanese.enabled && this.item.transliterateMode === 'japanese')) {
       const zh = chinese.splitName(name)
       if (zh.isName) {
