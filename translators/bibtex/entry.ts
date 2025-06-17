a98e2bbbed5470d7de54923857820117ed25933a
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2025-06-17 08:45:31 +0200

    pubmed

--- translators/bibtex/entry.ts
+++ translators/bibtex/entry.ts
@@ -383,9 +383,9 @@ export class Entry {
       this.add({ name: 'eprinttype', value: 'googlebooks' })
       this.add({ name: 'eprint', value: decodeURI(m[1]) })
     }
-    else if (m = this.item.url.match(/^https?:\/\/www.ncbi.nlm.nih.gov\/pubmed\/([\S]+)$/i)) {
+    else if (m = this.item.url.match(/^https?:\/\/www.ncbi.nlm.nih.gov\/pubmed\/(\d+)/i)) {
       this.add({ name: 'eprinttype', value: 'pubmed' })
-      this.add({ name: 'eprint', value: decodeURI(m[1]) })
+      this.add({ name: 'eprint', value: m[1] })
     }
     else if (m = this.item.url.match(/^https?:[/][/]hdl[.]handle[.]net[/]([^/]+[/].+)$/i)) {
       this.add({ name: 'eprinttype', value: 'hdl' })
