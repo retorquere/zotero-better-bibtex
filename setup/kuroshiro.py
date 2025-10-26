53258656022e27e1c1b0f0e315f3dd0761fb32dc
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2025-10-26 15:32:09 +0100

    kuro

--- setup/kuroshiro.py
+++ setup/kuroshiro.py
@@ -5,23 +5,28 @@ import glob
 import os
 import shutil
 import gzip
+import json
 
 root = os.path.join(os.path.dirname(__file__), '..')
 
-unzipped = os.path.join(root, 'build/content/resource/kuromoji')
-os.makedirs(unzipped, exist_ok=True)
+dicts_tgt = os.path.join(root, 'build/content/resource/kuromoji')
+os.makedirs(dicts_tgt, exist_ok=True)
 
 def run(cmd):
   print(' ', subprocess.check_output(cmd, shell=True).decode('utf-8').strip())
 
-print('copying kuromoji dicts...')
-for dic in sorted(glob.glob(os.path.join(root, 'node_modules/kuromoji/dict/*.gz'))):
-  base = os.path.basename(dic).replace('.gz', '')
-  print(' ', base)
-  dat = os.path.join(unzipped, base)
-  os.makedirs(os.path.dirname(dat), exist_ok=True)
-  with gzip.open(dic, 'rb') as f_in, open(dat, 'wb') as f_out:
-    shutil.copyfileobj(f_in, f_out)
+with open(os.path.join(root, 'node_modules/kuromoji/package.json')) as f:
+  pkg = json.load(f)
+print('copying kuromoji dicts', pkg['version'])
+
+for src in sorted(glob.glob(os.path.join(root, 'node_modules/kuromoji/dict/*.gz'))):
+  dict_name = os.path.basename(src)
+  print(' ', dict_name)
+  tgt = os.path.join(dicts_tgt, dict_name)
+  os.makedirs(os.path.dirname(tgt), exist_ok=True)
+  #with gzip.open(src, 'rb') as f_in, open(tgt.replace('.gz', ''), 'wb') as f_out:
+  #  shutil.copyfileobj(f_in, f_out)
+  shutil.copy(src, tgt)
 
 jieba = os.path.join(root, 'build/content/resource/jieba')
 os.makedirs(jieba, exist_ok=True)
