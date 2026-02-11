88b492956eb1d2595f71811cd300cb168e0b2d30
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2026-02-12 00:06:31 +0100

    unkey

---
+++ util/unkey.mjs
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+import fs from 'fs'
+
+for (const lib of process.argv.slice(2)) {
+  if (lib.endsWith('.json')) {
+    const data = JSON.parse(fs.readFileSync(lib, 'utf-8'))
+    if (data.config.id === '36a3b0b5-bad0-4a04-b79b-441c7cef77db') {
+      for (const item of data.items) {
+        delete item.citationKey
+      }
+      fs.writeFileSync(lib, JSON.stringify(data, null, 2))
+    }
+  }
+}
