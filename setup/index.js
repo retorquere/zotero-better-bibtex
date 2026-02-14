f2f06b855aac6255f13b24424c96575751315dde
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2026-02-14 23:51:44 +0100

    manifest

--- setup/index.js
+++ setup/index.js
@@ -14,6 +14,6 @@ import './apis.js'
 fs.copyFileSync('node_modules/@retorquere/bibtex-parser/dist/data/unabbrev.json', 'build/content/resource/bibtex/unabbrev.json')
 fs.copyFileSync('node_modules/@retorquere/bibtex-parser/dist/data/strings.bib', 'build/content/resource/bibtex/strings.bib')
 const manifest = JSON.parse(fs.readFileSync('build/manifest.json', 'utf-8'))
-manifest.applications.zotero.strict_min_version = '7.0.15'
-manifest.applications.zotero.strict_max_version = '8.*'
+manifest.applications.zotero.strict_min_version = '8.0.1'
+manifest.applications.zotero.strict_max_version = '9.*'
 fs.writeFileSync('build/manifest.json', JSON.stringify(manifest, null, 2))
