ae982a1756ad71fd8786f9b3529a089b2bfacd68
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2026-03-16 12:12:17 +0100

    deterministic sort

--- content/string-compare.ts
+++ content/string-compare.ts
@@ -1,7 +1,15 @@
 export const strcmp = (['base', 'accent', 'case', 'variant'] as const).reduce(
-  (acc, sensitivity) => ({
-    ...acc,
-    [sensitivity]: new Intl.Collator(undefined, { sensitivity }).compare, // eslint-disable-line @typescript-eslint/unbound-method
-  }),
+  (acc, sensitivity) => {
+    const collator = new Intl.Collator(undefined, { sensitivity })
+    return {
+      ...acc,
+      [sensitivity]: collator.compare.bind(collator),
+    }
+  },
   {} as Record<NonNullable<Intl.CollatorOptions['sensitivity']>, (a: string, b: string) => number>
 )
+
+for (const sensitivity of ['base', 'accent', 'case'] as const) {
+  const original = strcmp[sensitivity]
+  strcmp[sensitivity] = (a: string, b: string): number => original(a, b) || strcmp.variant(a, b)
+}
