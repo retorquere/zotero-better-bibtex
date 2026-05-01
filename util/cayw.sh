688ca49268180ab024d89e8302d069122c3923bf
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2026-05-01 08:38:18 +0200

    fixes #3371

--- util/cayw.sh
+++ util/cayw.sh
@@ -3,5 +3,5 @@
 #curl -v 'http://127.0.0.1:23119/better-bibtex/cayw?format=formatted-citation&selected=true'
 #curl -v 'http://127.0.0.1:23119/better-bibtex/cayw?format=formatted-citation'
 #curl -v 'http://127.0.0.1:23119/better-bibtex/cayw?format=jupyter&minimize=true'
-curl -v 'http://127.0.0.1:23119/better-bibtex/cayw?format=latex&minimize=true'
+curl -v 'http://127.0.0.1:23119/better-bibtex/cayw?format=pandoc&minimize=true'
 
